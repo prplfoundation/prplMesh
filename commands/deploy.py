fcd23772e57b171750284c0656d660a7872d574d
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2018-12-02 17:02:58 +0200

    WLANRTSYS-6198: multiap_tools: add support to deploy controller and agent (working)

--- commands/deploy.py
+++ commands/deploy.py
@@ -16,25 +16,70 @@ class mapdeploy(object):
     def __init__(self, args):
         if args.verbose: logger.setLevel(logging.DEBUG)
         modules = deploy_modules if 'all' in args.modules else [m for m in deploy_modules if m in args.modules]
-        modules_dir = os.path.realpath(args.map_path)
-        build_dir = os.path.realpath(modules_dir + '/build')
-        conf_file = args.conf if args.conf else os.path.realpath(modules_dir + '/deploy.yaml')
-        logger.debug("modules_dir={}, build_dir={}, conf={}".format(modules_dir, build_dir, conf_file))
-        with open(conf_file, 'r') as f: self.conf = yaml.load(f)
+        self.modules_dir = os.path.realpath(args.map_path)
+        self.build_dir = os.path.realpath(self.modules_dir + '/build')
+        conf_file = args.conf if args.conf else os.path.realpath(self.modules_dir + '/deploy.yaml')
+        logger.debug("modules_dir={}, build_dir={}, conf={}".format(self.modules_dir, self.build_dir, conf_file))
+        with open(conf_file, 'r') as f:
+            self.conf = yaml.load(f)
+            self.__connect__()
         logger.debug("deploy configuration: {}".format(self.conf))
         logger.info("deploy {}".format(modules))
 
         if args.file:
+            # HACK to copy single file
             logger.info("copy single file {} to target @{}".format(args.file, args.path))
             self.upload(args.file, args.path)
         else:
-            for name in modules:
-                logger.debug("deploy {}".format(name))
+            for m in modules:
+                logger.debug("deploy {}".format(m))
+                self.deploy(m)
 
-            self.upload("{}/controller/beerocks_v1.4_controller.tar.gz".format(build_dir), args.path)
+    def deploy(self, name):
+        if name in ['agent', 'controller']:
+            archive = "{}/{}/beerocks_v1.4_{}.tar.gz".format(self.build_dir, name, name)
+            softlink = "/opt/beerocks/update_beerocks_{}".format(name)
+            path = "/opt/beerocks/"
+            # copy archive to target
+            self.upload(archive, path)
+            # create soft link on target
+            target = self.conf['target']
+            command = 'sshpass -p {} ssh {} {}@{} "ln -s {} {}"'.format(target['pass'], self.sshoptions,
+                    target['user'], target['ip'], '{}/{}'.format(path, os.path.basename(archive)), softlink)
+            self.run([command])
+        else:
+            # deploy out/bin and out/lib in all other modules
+            logger.error("deploy {} not supported yet...".format(name))
 
+    def __connect__(self):
+        ''' connect sftp and ssh clients '''
+        proxy = self.conf['proxy']
+        target = self.conf['target']
+        
+        #default ssh options
+        self.sshoptions = "-oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no"
+        # start sftp connection
+        t = paramiko.Transport((proxy['ip'], 22))
+        t.connect(username=proxy['user'], password=proxy['pass'])
+        self.sftp = paramiko.SFTPClient.from_transport(t)
+        # start ssh connection
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.connect(proxy['ip'], port=22, username=proxy['user'], password=proxy['pass'])
+
+    def run(self, commands=[]):
+        ''' Run commands on target via proxy '''
+        proxy = self.conf['proxy']
+        target = self.conf['target']
+        for c in commands:
+            logger.debug("@{} --> {}".format(proxy['ip'], c))
+            stdin, stdout, stderr = self.client.exec_command(c)
+            if stdout.channel.recv_exit_status():
+                logger.error("Failed executing command: exit code - {}, stderr - {}".format(stdout.channel.recv_exit_status(), stderr.read()))
+        
     def upload(self, file, path):
-        ''' Uploads a file to the target vi proxy
+        ''' Uploads a file to the target via proxy
             file - local file
             path - path in target
         '''
@@ -42,34 +87,17 @@ class mapdeploy(object):
         target = self.conf['target']
         logger.info("Upload {} to {}:{} via {}".format(os.path.basename(file), target['ip'], path, proxy['ip']))
 
-        # start sftp connection
-        t = paramiko.Transport((proxy['ip'], 22))
-        t.connect(username=proxy['user'], password=proxy['pass'])
-        sftp = paramiko.SFTPClient.from_transport(t)
-
-        # start ssh connection
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(self.conf['proxy']['ip'], port=22, username=proxy['user'], password=proxy['pass'])
-    
         # copy to proxy via sftp
         proxy_file_path = '/tmp/%s' % (os.path.basename(file))
-        sftp.put(file, proxy_file_path)
+        self.sftp.put(file, proxy_file_path)
 
         # copy from proxy to target via ssh
-        sshoptions = "-oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no"
-        commands = ['sshpass -p {} ssh {} {}@{} "mkdir -p {}"'.format(target['pass'], sshoptions, target['user'], target['ip'], path),
-                    'sshpass -p admin scp -r {} {} admin@192.168.1.1:{}'.format(sshoptions, proxy_file_path, path)]
-        
-        for c in commands:
-            logger.debug("@{} --> {}".format(proxy['ip'], c))
-            stdin, stdout, stderr = client.exec_command(c)
-            if stdout.channel.recv_exit_status():
-                logger.error("Failed executing command: exit code - {}, stderr - {}".format(stdout.channel.recv_exit_status(), stderr.read()))
+        commands = ['sshpass -p {} ssh {} {}@{} "mkdir -p {}"'.format(target['pass'], self.sshoptions, target['user'], target['ip'], path),
+                    'sshpass -p admin scp -r {} {} admin@192.168.1.1:{}'.format(self.sshoptions, proxy_file_path, path)]
 
+        self.run(commands)
         # remove copied file from proxy
-        sftp.remove(proxy_file_path)
+        self.sftp.remove(proxy_file_path)
 
     @staticmethod
     def configure_parser(parser=argparse.ArgumentParser(prog='deploy')):
