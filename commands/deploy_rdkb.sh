aa3643cdd71379c3bf37eaef4a857cd22fe5a362
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2018-12-09 13:01:27 +0200

    multiap_tools - update maptools deploy to prepare a single tar file + deploy script

---
+++ commands/deploy_rdkb.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+archive=${1-/tmp/multiap/deploy/multiap_deploy.tar.gz}
+echo "$0: RDKB Deploy mAP - NOT IMPLEMENTED!"
---
+++ commands/deploy_ugw.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+archive=${1-/tmp/multiap/deploy/multiap_deploy.tar.gz}
+
+echo "$0: UGW Deploy mAP"
+echo "$0: Kill mAP controller & Agent"
+/opt/beerocks/beerocks_utils.sh killall_forced
+echo "$0: Stop mAP Framework"
+/etc/init.d/multiap_init.sh stop
+
+echo "$0: Deploy $archive ..."
+cd / && tar -xzvf $archive && cd -
+
+echo "$0: Start mAP Framework"
+/etc/init.d/multiap_init.sh start
+
+echo "$0: Start beerocks"
+/opt/beerocks/beerocks_utils.sh start
+
+echo "$0: Done"
