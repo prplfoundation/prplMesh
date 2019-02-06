7e74b642714c82501862e91c5b5a8346d21edc39
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-02-06 18:18:21 +0200

    update maptools.sh to support both icsl and elitpts10 servers

--- maptools.sh
+++ maptools.sh
@@ -1,3 +1,8 @@
-VIRTUAL_ENV_DISABLE_PROMPT=1 && source /opt/ugw/chdlabv3/.virtualenv/bin/activate
+if [ ! -d ${CHDLAB_PATH} ]; then
+    echo "Error: CHDLAB_PATH not set, aborting."
+    exit 1
+fi
+
+VIRTUAL_ENV_DISABLE_PROMPT=1 && source ${CHDLAB_PATH}/.virtualenv/bin/activate
 ./maptools.py $@
 deactivate
