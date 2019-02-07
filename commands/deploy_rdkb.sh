e8ab6e50463e3093c0081a95b593d020f66af028
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-02-07 13:52:27 +0200

    update deploy_rdkb.sh to restart wifi

--- commands/deploy_rdkb.sh
+++ commands/deploy_rdkb.sh
@@ -6,6 +6,13 @@ path=${2-/}
 echo "$0: RDKB Deploy APM"
 echo "$0: Stop APM"
 /opt/beerocks/apm_utils.sh stop
+
+echo "$0: WiFi Restart"
+wifi down #stops hostapd
+systemctl stop systemd-wave_init.service #rmmods ko’s
+systemctl start systemd-wave_init.service # insmod ko’s, runs recovery daemon
+wifi
+
 echo "$0: Deploy $archive to $path..."
 tar -C $path -xzvf $archive
 rm -rf $archive
