8430ebc9a87b09c5c7d8b55e8c1091f4c0db1eb2
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-02-07 10:09:52 +0200

    update deploy_rdkb.sh

--- commands/deploy_rdkb.sh
+++ commands/deploy_rdkb.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-archive=${1-/tmp/multiap/deploy/multiap_deploy.tar.gz}
-echo "$0: RDKB Deploy mAP - NOT IMPLEMENTED!"
+archive=${1-/tmp/multiap_deploy.tar.gz}
+path=${2-/}
+
+echo "$0: RDKB Deploy APM"
+echo "$0: Stop APM"
+/opt/beerocks/apm_utils.sh stop
+echo "$0: Deploy $archive to $path..."
+tar -C $path -xzvf $archive
+rm -rf $archive
+echo "$0: Start APM"
+/opt/beerocks/apm_utils.sh start
+
+echo "$0: Done"
