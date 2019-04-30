8734c8ee29b5aa12940246e94d581c82835dfe58
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-04-30 12:54:01 +0300

    change header license tepmplate

--- commands/package.py
+++ commands/package.py
@@ -15,7 +15,7 @@ license = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../LI
 header_template = '''
 /* SPDX-License-Identifier: BSD-2-Clause-Patent
  *
- * Copyright (c) 2018 Intel Corporation
+ * Copyright (c) 2016-2019 Intel Corporation
  *
  * This code is subject to the terms of the BSD+Patent license.
  * See LICENSE file for more details.
