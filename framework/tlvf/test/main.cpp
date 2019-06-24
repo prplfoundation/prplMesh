af8e099229e2bfae946ee8a7680646edde4def7c
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-06-24 18:11:33 +0200

    tlvf: tlvf_test: extend with tlvWscM2
    As a basis for testing encryption, add a basic test of tlvWscM2 to the
    tlvf_test.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- framework/tlvf/test/main.cpp
+++ framework/tlvf/test/main.cpp
@@ -15,6 +15,7 @@
 #include "tlvf/ieee_1905_1/tlvLinkMetricQuery.h"
 #include "tlvf/ieee_1905_1/tlvNon1905neighborDeviceList.h"
 #include "tlvf/ieee_1905_1/tlvVendorSpecific.h"
+#include "tlvf/ieee_1905_1/tlvWscM2.h"
 #include "tlvf/wfa_map/tlvApCapability.h"
 
 #include <mapf/common/logger.h>
@@ -34,7 +35,7 @@ int main(int argc, char *argv[])
 {
     int errors = 0;
     mapf::Logger::Instance().LoggerInit("TLVF example");
-    uint8_t tx_buffer[256];
+    uint8_t tx_buffer[1000];
 
     //START BUILDING THE MESSAGE HERE
     memset(tx_buffer, 0, sizeof(tx_buffer));
@@ -108,19 +109,31 @@ int main(int argc, char *argv[])
 
     MAPF_INFO("TLV LENGTH START: " << firstTlv->length());
     auto secondTlv = msg.addClass<tlvLinkMetricQuery>(); // another tlv for the example
-    auto metrics   = secondTlv->link_metrics();
-    metrics        = tlvLinkMetricQuery::eLinkMetricsType::RX_LINK_METRICS_ONLY;
+    secondTlv->link_metrics() = tlvLinkMetricQuery::eLinkMetricsType::RX_LINK_METRICS_ONLY;
 
-    // Remove "unused variable" warning
-    (void)metrics;
+    auto thirdTlv = msg.addClass<tlvWscM2>();
+    WSC::sM2& m2 = thirdTlv->M2Frame();
+    // All attributes have been initialized to default.
+    memcpy(m2.encrypted_settings_attr.ssid_attr.data, "test", 4);
 
     //MANDATORY - swaps to little indian.
     msg.finalize(true);
-    uint8_t recv_buffer[256];
-    memcpy(recv_buffer, tx_buffer, 256);
+    
+    // Temporary for checking correctness
+    std::cout << "TX: " << std::endl;
+    for (size_t i = 0; i < msg.getMessageLength(); i++) {
+        if (i % 16 == 0)
+            std::cout << std::endl;
+        std::cout << std::hex << std::setw(2) << std::setfill('0')
+                  << (unsigned)tx_buffer[i] << " ";
+    }
+    std::cout << std::endl;
+
+    uint8_t recv_buffer[sizeof(tx_buffer)];
+    memcpy(recv_buffer, tx_buffer, sizeof(recv_buffer));
 
     CmduMessageRx received_message;
-    received_message.parse(recv_buffer, 256, true);
+    received_message.parse(recv_buffer, sizeof(recv_buffer), true);
 
     eTlvType type;
     if (received_message.getNextTlvType(type) &&
@@ -167,6 +180,22 @@ int main(int argc, char *argv[])
 
     MAPF_INFO("size: " << received_message.getNextTlvLength());
 
+    auto tlv2 = received_message.addClass<tlvLinkMetricQuery>();
+    if (tlv2 != nullptr) {
+        MAPF_INFO("TLV2 LENGTH AFTER INIT: " << tlv2->length());
+    } else {
+        MAPF_ERR("TLV2 IS NULL");
+        errors++;
+    }
+    
+    auto tlv3 = received_message.addClass<tlvWscM2>();
+    if (tlv3 != nullptr) {
+        MAPF_INFO("TLV3 LENGTH AFTER INIT: " << tlv3->length());
+    } else {
+        MAPF_ERR("TLV3 IS NULL");
+        errors++;
+    }
+    
     int invalidBufferSize = 26;
     uint8_t invalidBuffer[invalidBufferSize];
     memcpy(invalidBuffer, recv_buffer, 26);
