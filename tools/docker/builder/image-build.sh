fda00b191be9bfcecd32395b13248993d6fc4ab6
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-06-25 13:25:16 +0200

    tools: set GID as well as UID
    Commit 5e83aec7ec made sure that the build is done as the current user
    instead of as root. However, it did not make sure that the group is also
    set to the user's group. Thus, the AutoGenerated files and the files in
    the build/ directory will belong to group 'root'.
    
    Similar to what we do for uid, add a variable gid and set it based on
    SUDO_GID, and use it in the USER directive.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/builder/image-build.sh
+++ tools/docker/builder/image-build.sh
@@ -9,7 +9,12 @@ scriptdir="$(cd "${0%/*}"; pwd)"
 topdir="${scriptdir%/*/*/*/*}"
 
 main() {
-    run docker image build --build-arg topdir=$topdir --build-arg uid=${SUDO_UID:-0} --tag prplmesh-build ${scriptdir}
+    run docker image build \
+        --build-arg topdir=$topdir \
+        --build-arg uid=${SUDO_UID:-0} \
+        --build-arg gid=${SUDO_GID:-0} \
+        --tag prplmesh-build \
+        ${scriptdir}
 }
 
 main $@
