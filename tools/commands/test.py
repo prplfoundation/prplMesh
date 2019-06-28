c8adf48b6543442ed76394c7595c93c29172c821
Ostap <slyusar.ostap@gmail.com>
2019-06-28 12:31:56 -0400

    fixup! WIP: added travis CI testing for prplmesh
    Signed-off-by: Ostap <slyusar.ostap@gmail.com>

--- tools/commands/test.py
+++ tools/commands/test.py
@@ -4,16 +4,7 @@ import os
 import subprocess
 
 logger = logging.getLogger("test")
-test_targets=['start', 'stop', 'restart', 'status', 'check']
-
-class prpltester(object):
-    def __init__(self, install_dir, verbose):
-        self.script_path = os.path.join(install_dir, 'scripts/prplmesh_utils.sh')
-        self.verbose = verbose
-
-    def run_cmd(self, param):
-        cmd = [self.script_path, param]
-        subprocess.check_call(cmd)
+test_targets=['start', 'stop', 'restart', 'status']
 
 class maptest(object):
     def __init__(self, args):
@@ -22,25 +13,26 @@ class maptest(object):
         logger.info("{} prpltest".format(args.commands))
 
         install_dir = os.path.realpath(os.path.join(args.map_path, '..', 'build/install'))
-        tester = prpltester(install_dir, args.verbose)
+        self.script_path = os.path.join(install_dir, 'scripts/prplmesh_utils.sh')
 
-        self.run_command(tester, args.commands)
+        if 'start' in args.commands:
+            self.run_command('start')
+        if 'stop' in args.commands:
+            self.run_command('stop')
+        if 'restart' in args.commands:
+            self.run_command('restart')
+        if 'status' in args.commands:
+            self.run_command('status')
 
-    def run_command(self, tester, commands):
-        if 'start' in commands:
-            tester.run_cmd('start')
-        if 'stop' in commands:
-            tester.run_cmd('stop')
-        if 'check' in commands:
-            tester.run_cmd('restart')
-            tester.run_cmd('check')
-            tester.run_cmd('stop')
+    def run_command(self, param):
+        cmd = [self.script_path, param]
+        subprocess.check_call(cmd)
 
     @staticmethod
     def configure_parser(parser=argparse.ArgumentParser(prog='test')):
         parser.help = "multiap_sw standalone test module"
         parser.add_argument("--verbose", "-v", action="store_true", help="verbosity on")
-        parser.add_argument('-c', '--commands', choices=test_targets, nargs='+', default=['check'], help="test command (default is check)")
+        parser.add_argument('-c', '--commands', choices=test_targets, nargs='+', default=['start'], help="test command (default is start)")
 
         return parser
 
