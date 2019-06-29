81e5027c0f7284ea4980924e5a989ace13fca698
tbeliyah <tomer.b.eliyahu@intel.com>
2019-06-29 17:30:55 +0300

    tools: docker builder build native os option
    In some cases we want to build using the same OS version as in the host.
    For example, consider the case that the host is ubuntu-19.04, where some
    of the packages which are installed are newer than the default image
    from which the builder derives (ubuntu-18.04).
    This means that we won't necessarilly be able to run the images built by
    the builder from the host.
    
    This is a major issue though since we can also run from docker using the
    runner docker, but adding an option to build the base image using the
    same OS version is nice, so this commit adds it.
    
    See issue #72
    
    Signed-off-by: tbeliyah <tomer.b.eliyahu@intel.com>

--- tools/docker/builder/image-build.sh
+++ tools/docker/builder/image-build.sh
@@ -1,20 +1,64 @@
 #!/bin/sh
 
+dbg() {
+    [ "$VERBOSE" = "true" ] && echo "$*"
+}
+
+err() {
+	echo -e '\033[1;31m'"$@"'\033[0m'
+}
+
 run() {
-    echo "$*"
+    dbg "$*"
     "$@" || exit $?
 }
 
 scriptdir="$(cd "${0%/*}"; pwd)"
 topdir="${scriptdir%/*/*/*/*}"
 
+usage() {
+    echo "usage: $(basename $0) [-hvn]"
+    echo "  options:"
+    echo "      -h|--help - show this help menu"
+    echo "      -v|--verbose - verbosity on"
+    echo "      -n|--native - Use the host OS's for the FROM image"
+}
+
 main() {
+    OPTS=`getopt -o 'hvn' --long verbose,help,native -n 'parse-options' -- "$@"`
+
+    if [ $? != 0 ] ; then err "Failed parsing options." >&2 ; usage; exit 1 ; fi
+
+    eval set -- "$OPTS"
+
+    while true; do
+        case "$1" in
+            -v | --verbose) VERBOSE=true; shift ;;
+            -h | --help)    usage; exit 0; shift ;;
+            -n | --native)  NATIVE=true; shift ;;
+            -- ) shift; break ;;
+            * ) err "unsupported argument $1"; usage; exit 1 ;;
+        esac
+    done
+
+    [ "$NATIVE" = "true" ] && {
+        . /etc/lsb-release
+        IMAGE=$(echo $DISTRIB_ID | awk '{print tolower($0)}'):$DISTRIB_RELEASE
+    }
+    dbg IMAGE=$IMAGE
+    dbg topdir=$topdir
+
     run docker image build \
+        --build-arg image=$IMAGE \
         --build-arg topdir=$topdir \
         --build-arg uid=${SUDO_UID:-0} \
         --build-arg gid=${SUDO_GID:-0} \
         --tag prplmesh-build \
         ${scriptdir}
 }
+
+VERBOSE=false
+NATIVE=false
+IMAGE=ubuntu:18.04
 
 main $@
