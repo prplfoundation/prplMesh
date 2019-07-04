be352182fd9755812044db830e33e59ecfc7dd69
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-07-04 20:28:35 +0300

    scripts: prplmesh_utils.sh platform_db support
    Add platform_db automatic creation by prplmesh_utils.sh according to the
    mode in which the script is invoked, and write that to
    /tmp/prplmesh_platform_db.
    
    While we're at it, change eth0 to sim-eth0 since if
    prplmesh_utils.sh runs on the host, it will mess up networking.
    Using a ~unique name for the dummy interface is good practice anyhow,
    and allows us to run the same script in both containers and hosts.
    Changing wlan0/2 to sim-wlan0/2 will be done in a separate commit.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- tools/docker/runner/entrypoint.sh
+++ tools/docker/runner/entrypoint.sh
@@ -8,10 +8,15 @@ run() {
 run ip link add br-lan type bridge
 run ip link add wlan0 type dummy up
 run ip link add wlan2 type dummy up
+run ip link add sim-eth0 type dummy up
+run ip link set sim-eth0 master br-lan
 run ip link set eth0 master br-lan
 run ip link set wlan0 master br-lan
 run ip link set wlan2 master br-lan
 run ifconfig eth0 0.0.0.0
+run ifconfig sim-eth0 up
+run ifconfig wlan0 up
+run ifconfig wlan2 up
 run ifconfig br-lan ${1} up
 
 shift
