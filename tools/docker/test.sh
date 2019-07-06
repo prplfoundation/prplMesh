5aea00cf8514a3891ddb825d1ecda84ef6cddeae
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-07-07 01:03:04 +0300

    tools: docker: add test.sh script
    Add test.sh, simple wrapper for prplmesh_utils.sh status which will be
    called by CI.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ tools/docker/test.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+dbg() {
+    [ "$VERBOSE" = "true" ] && echo "$*"
+}
+
+err() {
+	printf '\033[1;31m'"$@\n"'\033[0m'
+}
+
+run() {
+    echo "$*"
+    "$@" || exit $?
+}
+
+scriptdir="$(cd "${0%/*}"; pwd)"
+topdir="${scriptdir%/*/*/*}"
+
+usage() {
+    echo "usage: $(basename $0) [-hv] [-n name]"
+    echo "  options:"
+    echo "      -h|--help - show this help menu"
+    echo "      -v|--verbose - verbosity on"
+    echo "      -n|--name - container name for which to attach"
+}
+
+main() {
+    OPTS=`getopt -o 'hvn:' --long verbose,help,name -n 'parse-options' -- "$@"`
+
+    eval set -- "$OPTS"
+
+    while true; do
+        case "$1" in
+            -v | --verbose) VERBOSE=true; OPT="-v"; shift ;;
+            -h | --help)    usage; exit 0; shift ;;
+            -n | --name)    NAME="$2"; shift; shift ;;
+            -- ) shift; break ;;
+            * ) err "unsupported argument $1"; usage; exit 1 ;;
+        esac
+    done
+
+    run docker container exec -it ${NAME} ${topdir}/build/install/scripts/prplmesh_utils.sh status $OPT
+}
+
+VERBOSE=false
+NAME=prplMesh
+main $@
