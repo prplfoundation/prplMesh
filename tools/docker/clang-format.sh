827620b41258044b5b71599c6472fb82b826dbd9
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-07-07 16:50:42 +0300

    tools: docker: add clang-format.sh
    Add clang-format.sh which uses the prplmesh-runner image to start a
    container only to run clang-format.sh from the root of the sources
    directory.
    
    This will be used by the CI to verify that each PR adheres to the
    clang-format rules.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ tools/docker/clang-format.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+dbg() {
+    [ "$VERBOSE" = "true" ] && echo "$*"
+}
+
+err() {
+	printf '\033[1;31m'"$@\n"'\033[0m'
+}
+
+run() {
+    dbg "$*"
+    "$@" || exit $?
+}
+
+scriptdir="$(cd "${0%/*}"; pwd)"
+sourcesdir="${scriptdir%/*/*}"
+
+usage() {
+    echo "usage: $(basename $0) [-hvd] [-i ip] [-n name] [-N network]"
+    echo "  options:"
+    echo "      -h|--help - show this help menu"
+    echo "      -v|--verbose - verbosity on"
+    echo "      -t|--tag - prplmesh-runner tag to use (default is none)"
+}
+
+main() {
+    OPTS=`getopt -o 'hvt:' --long verbose,help,tag:,options: -n 'parse-options' -- "$@"`
+
+    if [ $? != 0 ] ; then err "Failed parsing options." >&2 ; usage; exit 1 ; fi
+
+    eval set -- "$OPTS"
+
+    while true; do
+        case "$1" in
+            -v | --verbose)     VERBOSE=true; shift ;;
+            -h | --help)        usage; exit 0; shift ;;
+            -t | --tag)         TAG=":$2"; shift; shift ;;
+            -- ) shift; break ;;
+            * ) err "unsupported argument $1"; usage; exit 1 ;;
+        esac
+    done
+
+    docker image inspect prplmesh-runner$TAG >/dev/null 2>&1 || {
+        [ -n "$TAG" ] && { err "image prplmesh-runner$TAG doesn't exist, aborting"; exit 1; }
+        dbg "Image prplmesh-runner$TAG does not exist, creating..."
+        run ${scriptdir}/image-build.sh
+    }
+
+    dbg "VERBOSE=${VERBOSE}"
+    dbg "IMAGE=prplmesh-runner$TAG"
+
+    DOCKEROPTS="-e USER=${SUDO_USER}
+                -e SOURCES_DIR=${sourcesdir}
+                -v ${sourcesdir}:${sourcesdir}
+                --name prplMesh-clang-format"
+
+    DOCKEROPTS="$DOCKEROPTS --interactive --tty --rm"
+    
+    run docker container run --entrypoint "${sourcesdir}/clang-format.sh" ${DOCKEROPTS} prplmesh-runner$TAG "$@"
+}
+
+VERBOSE=false
+
+main $@
