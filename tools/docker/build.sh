90418d013279b61f188bcb31007a7ac7ba265890
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-07-08 22:13:36 +0300

    license: add copywrite snippet to new files
    New scripts added lately are missing the copyright header, so add it.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- tools/docker/build.sh
+++ tools/docker/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+###############################################################
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# Copyright (c) 2019 Tomer Eliyahu (Intel)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+###############################################################
 
 run() {
     echo "$*"
