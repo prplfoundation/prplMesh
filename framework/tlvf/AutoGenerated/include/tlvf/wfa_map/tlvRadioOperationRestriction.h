ca604bd0f289a13a0d36dec0ebdc217ed85bf1cb
Moran Shoeg <moranx.shoeg@intel.com>
2019-07-10 16:18:36 +0300

    framework: tlvf: Add tlvRadioOperationRestriction Add tlvRadioOperationRestriction to tlvf_conf.yaml. This causes to the tlv factory python script to generate for it auto generated code, and to be compiled on maptools compilation.
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

---
+++ framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvRadioOperationRestriction.h
@@ -0,0 +1,96 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2016-2019 Intel Corporation
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_WFA_MAP_TLVRADIOOPERATIONRESTRICTION_H_
+#define _TLVF_WFA_MAP_TLVRADIOOPERATIONRESTRICTION_H_
+
+#include <cstddef>
+#include <stdint.h>
+#include <tlvf/swap.h>
+#include <string.h>
+#include <memory>
+#include <tlvf/BaseClass.h>
+#include "tlvf/wfa_map/eTlvTypeMap.h"
+#include "tlvf/common/sMacAddr.h"
+#include <tuple>
+#include <vector>
+
+namespace wfa_map {
+
+class cRestrictedOperatingClasses;
+
+class tlvRadioOperationRestriction : public BaseClass
+{
+    public:
+        tlvRadioOperationRestriction(uint8_t* buff, size_t buff_len, bool parse = false, bool swap_needed = false);
+        tlvRadioOperationRestriction(std::shared_ptr<BaseClass> base, bool parse = false, bool swap_needed = false);
+        ~tlvRadioOperationRestriction();
+
+        const eTlvTypeMap& type();
+        const uint16_t& length();
+        sMacAddr& radio_uid();
+        uint8_t& operating_classes_list_length();
+        std::tuple<bool, cRestrictedOperatingClasses&> operating_classes_list(size_t idx);
+        std::shared_ptr<cRestrictedOperatingClasses> create_operating_classes_list();
+        bool add_operating_classes_list(std::shared_ptr<cRestrictedOperatingClasses> ptr);
+        void class_swap();
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eTlvTypeMap* m_type = nullptr;
+        uint16_t* m_length = nullptr;
+        sMacAddr* m_radio_uid = nullptr;
+        uint8_t* m_operating_classes_list_length = nullptr;
+        cRestrictedOperatingClasses* m_operating_classes_list = nullptr;
+        size_t m_operating_classes_list_idx__ = 0;
+        std::vector<std::shared_ptr<cRestrictedOperatingClasses>> m_operating_classes_list_vector;
+        bool m_lock_allocation__ = false;
+};
+
+class cRestrictedOperatingClasses : public BaseClass
+{
+    public:
+        cRestrictedOperatingClasses(uint8_t* buff, size_t buff_len, bool parse = false, bool swap_needed = false);
+        cRestrictedOperatingClasses(std::shared_ptr<BaseClass> base, bool parse = false, bool swap_needed = false);
+        ~cRestrictedOperatingClasses();
+
+        typedef struct sChannelInfo {
+            uint8_t channel_number;
+            //The minimum frequency separation (in multiples of 10 MHz) that this radio would require when operating on
+            //the above channel number between the center frequency of that channel and the center operating frequency of
+            //another radio (operating simultaneous TX/RX) of the Multi-AP Agent.
+            uint8_t minimum_frequency_separation;
+            void struct_swap(){
+            }
+            void struct_init(){
+            }
+        } __attribute__((packed)) sChannelInfo;
+        
+        uint8_t& operating_class();
+        uint8_t& channel_list_length();
+        std::tuple<bool, sChannelInfo&> channel_list(size_t idx);
+        bool alloc_channel_list(size_t count = 1);
+        void class_swap();
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        uint8_t* m_operating_class = nullptr;
+        uint8_t* m_channel_list_length = nullptr;
+        sChannelInfo* m_channel_list = nullptr;
+        size_t m_channel_list_idx__ = 0;
+};
+
+}; // close namespace: wfa_map
+
+#endif //_TLVF/WFA_MAP_TLVRADIOOPERATIONRESTRICTION_H_
---
+++ framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvRadioOperationRestriction.cpp
@@ -0,0 +1,242 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2016-2019 Intel Corporation
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <tlvf/wfa_map/tlvRadioOperationRestriction.h>
+#include <tlvf/tlvflogging.h>
+
+using namespace wfa_map;
+
+tlvRadioOperationRestriction::tlvRadioOperationRestriction(uint8_t* buff, size_t buff_len, bool parse, bool swap_needed) :
+    BaseClass(buff, buff_len, parse, swap_needed) {
+    m_init_succeeded = init();
+}
+tlvRadioOperationRestriction::tlvRadioOperationRestriction(std::shared_ptr<BaseClass> base, bool parse, bool swap_needed) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse, swap_needed){
+    m_init_succeeded = init();
+}
+tlvRadioOperationRestriction::~tlvRadioOperationRestriction() {
+}
+const eTlvTypeMap& tlvRadioOperationRestriction::type() {
+    return (const eTlvTypeMap&)(*m_type);
+}
+
+const uint16_t& tlvRadioOperationRestriction::length() {
+    return (const uint16_t&)(*m_length);
+}
+
+sMacAddr& tlvRadioOperationRestriction::radio_uid() {
+    return (sMacAddr&)(*m_radio_uid);
+}
+
+uint8_t& tlvRadioOperationRestriction::operating_classes_list_length() {
+    return (uint8_t&)(*m_operating_classes_list_length);
+}
+
+std::tuple<bool, cRestrictedOperatingClasses&> tlvRadioOperationRestriction::operating_classes_list(size_t idx) {
+    bool ret_success = ( (m_operating_classes_list_idx__ > 0) && (m_operating_classes_list_idx__ > idx) );
+    size_t ret_idx = ret_success ? idx : 0;
+    if (!ret_success) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+    }
+    if (*m_operating_classes_list_length > 0) {
+        return std::forward_as_tuple(ret_success, *(m_operating_classes_list_vector[ret_idx]));
+    }
+    else {
+        return std::forward_as_tuple(ret_success, *(m_operating_classes_list));
+    }
+}
+
+std::shared_ptr<cRestrictedOperatingClasses> tlvRadioOperationRestriction::create_operating_classes_list() {
+    size_t len = cRestrictedOperatingClasses::get_initial_size();
+    if (m_lock_allocation__ || getBuffRemainingBytes() < len) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer";
+        return nullptr;
+    }
+    m_lock_allocation__ = true;
+    return std::make_shared<cRestrictedOperatingClasses>(getBuffPtr(), getBuffRemainingBytes(), m_parse__, m_swap__);
+}
+
+bool tlvRadioOperationRestriction::add_operating_classes_list(std::shared_ptr<cRestrictedOperatingClasses> ptr) {
+    if (ptr == nullptr) {
+        TLVF_LOG(ERROR) << "Received entry is nullptr";
+        return false;
+    }
+    if (m_lock_allocation__ == false) {
+        TLVF_LOG(ERROR) << "No call to create_operating_classes_list was called before add_operating_classes_list";
+        return false;
+    }
+    if (ptr->getStartBuffPtr() != getBuffPtr()) {
+        TLVF_LOG(ERROR) << "Received to entry pointer is different than expected (excepting the same pointer returned from add method)";
+        return false;
+    }
+    if (ptr->getLen() > getBuffRemainingBytes()) {;
+        TLVF_LOG(ERROR) << "Not enough available space on buffer";
+        return false;
+    }
+    if (!m_parse__) {
+        m_operating_classes_list_idx__++;
+        (*m_operating_classes_list_length)++;
+    }
+    size_t len = ptr->getLen();
+    m_operating_classes_list_vector.push_back(ptr);
+    m_buff_ptr__ += len;
+    if(m_length){ (*m_length) += len; }
+    m_lock_allocation__ = false;
+    return true;
+}
+
+void tlvRadioOperationRestriction::class_swap()
+{
+    tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
+    m_radio_uid->struct_swap();
+    for (size_t i = 0; i < (size_t)*m_operating_classes_list_length; i++){
+        std::get<1>(operating_classes_list(i)).class_swap();
+    }
+}
+
+size_t tlvRadioOperationRestriction::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(eTlvTypeMap); // type
+    class_size += sizeof(uint16_t); // length
+    class_size += sizeof(sMacAddr); // radio_uid
+    class_size += sizeof(uint8_t); // operating_classes_list_length
+    return class_size;
+}
+
+bool tlvRadioOperationRestriction::init()
+{
+    if (getBuffRemainingBytes() < kMinimumLength) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_type = (eTlvTypeMap*)m_buff_ptr__;
+    if (!m_parse__) *m_type = eTlvTypeMap::TLV_RADIO_OPERATION_RESTRICTION;
+    else {
+            if (*m_type != eTlvTypeMap::TLV_RADIO_OPERATION_RESTRICTION) {
+            TLVF_LOG(ERROR) << "TLV type mismatch. Expected value: " << int(eTlvTypeMap::TLV_RADIO_OPERATION_RESTRICTION) << ", received value: " << int(*m_type);
+            return false;
+        }
+    }
+    m_buff_ptr__ += sizeof(eTlvTypeMap) * 1;
+    m_length = (uint16_t*)m_buff_ptr__;
+    if (!m_parse__) *m_length = 0;
+    m_buff_ptr__ += sizeof(uint16_t) * 1;
+    m_radio_uid = (sMacAddr*)m_buff_ptr__;
+    m_buff_ptr__ += sizeof(sMacAddr) * 1;
+    if(m_length && !m_parse__){ (*m_length) += sizeof(sMacAddr); }
+    if (!m_parse__) { m_radio_uid->struct_init(); }
+    m_operating_classes_list_length = (uint8_t*)m_buff_ptr__;
+    if (!m_parse__) *m_operating_classes_list_length = 0;
+    m_buff_ptr__ += sizeof(uint8_t) * 1;
+    if(m_length && !m_parse__){ (*m_length) += sizeof(uint8_t); }
+    m_operating_classes_list = (cRestrictedOperatingClasses*)m_buff_ptr__;
+    m_operating_classes_list_idx__ = *m_operating_classes_list_length;
+    for (size_t i = 0; i < *m_operating_classes_list_length; i++) {
+        if (!add_operating_classes_list(create_operating_classes_list())) { 
+            TLVF_LOG(ERROR) << "Failed adding operating_classes_list entry.";
+            return false;
+        }
+    }
+    if (m_buff_ptr__ - m_buff__ > ssize_t(m_buff_len__)) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    if (m_parse__ && m_swap__) { class_swap(); }
+    return true;
+}
+
+cRestrictedOperatingClasses::cRestrictedOperatingClasses(uint8_t* buff, size_t buff_len, bool parse, bool swap_needed) :
+    BaseClass(buff, buff_len, parse, swap_needed) {
+    m_init_succeeded = init();
+}
+cRestrictedOperatingClasses::cRestrictedOperatingClasses(std::shared_ptr<BaseClass> base, bool parse, bool swap_needed) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse, swap_needed){
+    m_init_succeeded = init();
+}
+cRestrictedOperatingClasses::~cRestrictedOperatingClasses() {
+}
+uint8_t& cRestrictedOperatingClasses::operating_class() {
+    return (uint8_t&)(*m_operating_class);
+}
+
+uint8_t& cRestrictedOperatingClasses::channel_list_length() {
+    return (uint8_t&)(*m_channel_list_length);
+}
+
+std::tuple<bool, cRestrictedOperatingClasses::sChannelInfo&> cRestrictedOperatingClasses::channel_list(size_t idx) {
+    bool ret_success = ( (m_channel_list_idx__ > 0) && (m_channel_list_idx__ > idx) );
+    size_t ret_idx = ret_success ? idx : 0;
+    if (!ret_success) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+    }
+    return std::forward_as_tuple(ret_success, m_channel_list[ret_idx]);
+}
+
+bool cRestrictedOperatingClasses::alloc_channel_list(size_t count) {
+    if (count == 0) {
+        TLVF_LOG(WARNING) << "can't allocate 0 bytes";
+        return false;
+    }
+    size_t len = sizeof(sChannelInfo) * count;
+    if(getBuffRemainingBytes() < len )  {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer - can't allocate";
+        return false;
+    }
+//TLVF_TODO: enable call to memmove
+    m_channel_list_idx__ += count;
+    *m_channel_list_length += count;
+    m_buff_ptr__ += len;
+    if (!m_parse__) { 
+        for (size_t i = m_channel_list_idx__ - count; i < m_channel_list_idx__; i++) { m_channel_list[i].struct_init(); }
+    }
+    return true;
+}
+
+void cRestrictedOperatingClasses::class_swap()
+{
+    for (size_t i = 0; i < (size_t)*m_channel_list_length; i++){
+        m_channel_list[i].struct_swap();
+    }
+}
+
+size_t cRestrictedOperatingClasses::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(uint8_t); // operating_class
+    class_size += sizeof(uint8_t); // channel_list_length
+    return class_size;
+}
+
+bool cRestrictedOperatingClasses::init()
+{
+    if (getBuffRemainingBytes() < kMinimumLength) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_operating_class = (uint8_t*)m_buff_ptr__;
+    m_buff_ptr__ += sizeof(uint8_t) * 1;
+    m_channel_list_length = (uint8_t*)m_buff_ptr__;
+    if (!m_parse__) *m_channel_list_length = 0;
+    m_buff_ptr__ += sizeof(uint8_t) * 1;
+    m_channel_list = (sChannelInfo*)m_buff_ptr__;
+    m_channel_list_idx__ = *m_channel_list_length;
+    m_buff_ptr__ += sizeof(sChannelInfo)*(*m_channel_list_length);
+    if (m_buff_ptr__ - m_buff__ > ssize_t(m_buff_len__)) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    if (m_parse__ && m_swap__) { class_swap(); }
+    return true;
+}
+
+
