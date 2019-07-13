2b0f194d7cd5208beb76a219645445a891b6ba90
Lior Amram <lior.amram@intel.com>
2019-07-13 09:15:34 -0700

    framework: tlvf: Add sVendorOUI struct
    Add a struct to represent the 3 byte field of OUI in Vendor Specific
    tlv. Create a .h file for it and include it into tlvf_conf.yaml.
    
    This new struct provides the functionality needed to solve this issue:
    https://github.com/prplfoundation/prplMesh/issues/33
    
    Signed-off-by: Lior Amram <lior.amram@intel.com>

---
+++ framework/tlvf/src/include/tlvf/ieee_1905_1/sVendorOUI.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _SVENDOR_OUI_H_
+#define _SVENDOR_OUI_H_
+
+#include <stdint.h>
+#include <algorithm>
+
+typedef struct sVendorOUI {
+    uint8_t upper;
+    uint8_t middle;
+    uint8_t lower;
+    sVendorOUI(uint32_t val)
+    {
+        lower = val & 0xFF;
+        middle = (val >> 8 ) & 0xFF;
+        upper = (val >> 16 ) & 0xFF;
+    }
+
+    operator uint32_t() const
+    {
+        return (upper << 16) + (middle << 8) + lower;
+    }
+
+    void struct_swap()
+    {
+        std::swap(upper, lower);
+    }
+
+    void struct_init() { }
+} __attribute__((packed)) sVendorOUI;
+
+
+#endif //_SVENDOR_OUI_H_
