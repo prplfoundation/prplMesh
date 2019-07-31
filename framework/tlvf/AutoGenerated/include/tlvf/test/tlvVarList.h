22dea2899be6abb43a3082cec7731b3897291a98
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-07-31 14:10:51 +0200

    tlvf: tlvfTestVarList: use 1 byte for list lengths
    The tlvf currently doesn't support a list length of more than one byte,
    because it handles swapping incorrectly. Since we don't actually need
    list lengths with more than one byte, it will not be fixed now.
    Therefore, just change tlvfTestVarList do always use one byte for the
    list length.
    
    The problem lies in the class_swap function: it will first swap the
    length member and then use this length member to iterate over the
    elements. For parsing, this is indeed correct. For sending, however,
    after swapping the length would no longer be correct, so the length has
    to be swapped after iterating over the list instead of before. For
    1-byte length this is not a problem since it doesn't need to be swapped.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- framework/tlvf/AutoGenerated/include/tlvf/test/tlvVarList.h
+++ framework/tlvf/AutoGenerated/include/tlvf/test/tlvVarList.h
@@ -33,10 +33,10 @@ class tlvTestVarList : public BaseClass
         const uint8_t& type();
         const uint16_t& length();
         uint16_t& var0();
-        uint16_t& simple_list_length();
+        uint8_t& simple_list_length();
         std::tuple<bool, uint16_t&> simple_list(size_t idx);
         bool alloc_simple_list(size_t count = 1);
-        uint16_t& complex_list_length();
+        uint8_t& complex_list_length();
         std::tuple<bool, cInner&> complex_list(size_t idx);
         std::shared_ptr<cInner> create_complex_list();
         bool add_complex_list(std::shared_ptr<cInner> ptr);
@@ -54,11 +54,11 @@ class tlvTestVarList : public BaseClass
         uint8_t* m_type = nullptr;
         uint16_t* m_length = nullptr;
         uint16_t* m_var0 = nullptr;
-        uint16_t* m_simple_list_length = nullptr;
+        uint8_t* m_simple_list_length = nullptr;
         uint16_t* m_simple_list = nullptr;
         size_t m_simple_list_idx__ = 0;
         int m_lock_order_counter__ = 0;
-        uint16_t* m_complex_list_length = nullptr;
+        uint8_t* m_complex_list_length = nullptr;
         cInner* m_complex_list = nullptr;
         size_t m_complex_list_idx__ = 0;
         std::vector<std::shared_ptr<cInner>> m_complex_list_vector;
@@ -77,7 +77,7 @@ class cInner : public BaseClass
         cInner(std::shared_ptr<BaseClass> base, bool parse = false, bool swap_needed = false);
         ~cInner();
 
-        uint16_t& list_length();
+        uint8_t& list_length();
         std::tuple<bool, uint8_t&> list(size_t idx);
         bool alloc_list(size_t count = 1);
         uint32_t& var1();
@@ -86,7 +86,7 @@ class cInner : public BaseClass
 
     private:
         bool init();
-        uint16_t* m_list_length = nullptr;
+        uint8_t* m_list_length = nullptr;
         uint8_t* m_list = nullptr;
         size_t m_list_idx__ = 0;
         int m_lock_order_counter__ = 0;
