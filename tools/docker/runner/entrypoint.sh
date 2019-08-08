5687fb18bc49179139f7d2b3e3c2c8aad33b981f
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-08-08 18:07:37 +0200

    tools: docker: runner: visually align ip commands
    entrypoint.sh has many ip commands. Visually align them to make reading
    a little more comfortable.
    
    While we're at it, add the optional 'dev' keyword to the ip link set
    commands. Some of them had it already, and it makes things consistent
    (for ip address, the dev keyword is not optional).
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/runner/entrypoint.sh
+++ tools/docker/runner/entrypoint.sh
@@ -13,20 +13,20 @@ run() {
 
 bridge_ip="$1"; shift
 
-run ip link add br-lan type bridge
-run ip link add wlan0 type dummy
-run ip link add wlan2 type dummy
-run ip link add sim-eth0 type dummy
-run ip link set sim-eth0 master br-lan
-run ip link set eth0 master br-lan
-run ip link set wlan0 master br-lan
-run ip link set wlan2 master br-lan
+run ip link add          br-lan   type bridge
+run ip link add          wlan0    type dummy
+run ip link add          wlan2    type dummy
+run ip link add          sim-eth0 type dummy
+run ip link set      dev sim-eth0 master br-lan
+run ip link set      dev eth0     master br-lan
+run ip link set      dev wlan0    master br-lan
+run ip link set      dev wlan2    master br-lan
 run ip address flush dev eth0
-run ip link set dev sim-eth0 up
-run ip link set dev wlan0 up
-run ip link set dev wlan2 up
-run ip address add dev br-lan "$bridge_ip"
-run ip link set dev br-lan up
+run ip link set      dev sim-eth0 up
+run ip link set      dev wlan0    up
+run ip link set      dev wlan2    up
+run ip address add   dev br-lan "$bridge_ip"
+run ip link set      dev br-lan   up
 
 cd ${INSTALL_DIR}
 exec /bin/bash "$@"
