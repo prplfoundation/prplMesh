0c7dd020ef03528201099e4883e77bd53f785e71
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-08-08 14:24:00 +0200

    tools: docker: image-pull.sh: new script to pull from dockerhub
    Building the images takes a bit of time, which is annoying for CI -
    especially because it regularly fails due to connectivity issues.
    Therefore, we have preloaded images on dockerhub.
    
    Add a script that downloads (pulls) the images from dockerhub, and
    gives them a local tag.
    
    The interface to this command is identical to image-build.sh, but
    instead of building it pulls it from dockerhub.
    
    If the image doesn't exist on dockerhub, the pull command will fail.
    
    Note that if a container image with the same tag already existed, the
    name will be destroyed but the image itself will remain until it is
    cleaned up.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ tools/docker/image-pull.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+###############################################################
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# Copyright (c) 2019 Tomer Eliyahu (Intel), Arnout Vandecappelle (Essensium/Mind)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+###############################################################
+
+dbg() {
+    [ "$VERBOSE" = "true" ] && echo "$(basename $0): $*"
+}
+
+err() {
+	printf '\033[1;31m'"$(basename $0): $@"'\033[0m\n'
+}
+
+info() {
+	printf '\033[1;35m'"$(basename $0): $@"'\033[0m\n'
+}
+
+run() {
+    dbg "$*"
+    "$@" || exit $?
+}
+
+scriptdir="$(cd "${0%/*}"; pwd)"
+topdir="${scriptdir%/*/*/*}"
+
+usage() {
+    echo "usage: $(basename $0) [-hvbt]"
+    echo "  options:"
+    echo "      -h|--help - show this help menu"
+    echo "      -v|--verbose - verbosity on"
+    echo "      -b|--base-image - Base OS image to use (Dockerfile 'FROM')"
+    echo "      -n|--native - Use the same base OS image as the running system"
+    echo "      -t|--tag - tag to add to prplmesh-builder and prplmesh-runner images"
+}
+
+pull_and_tag() {
+    name="$1"
+    upstream_tag="$2"
+    local_tag="$3"
+
+    repo="prplfoundationinc/$name:$upstream_tag"
+
+    info "Pulling $name$local_tag from $repo"
+    run docker pull "$repo"
+    run docker tag "$repo" "$name$local_tag"
+}
+
+main() {
+    OPTS=`getopt -o 'hvnb:t:' --long verbose,help,base-image,tag -n 'parse-options' -- "$@"`
+
+    if [ $? != 0 ] ; then err "Failed parsing options." >&2 ; usage; exit 1 ; fi
+
+    eval set -- "$OPTS"
+
+    while true; do
+        case "$1" in
+            -v | --verbose)         VERBOSE=true; shift ;;
+            -h | --help)            usage; exit 0; shift ;;
+            -b | --base-image)      IMAGE="$2"; shift ; shift ;;
+            -n | --native)          IMAGE=$(
+                                        . /etc/os-release
+                                        distro="$(echo $NAME | awk '{print tolower($0)}')"
+                                        echo "$distro:$VERSION_ID"
+                                    ); shift ;;
+            -t | --tag)             TAG=":$2"; shift ; shift ;;
+            -- ) shift; break ;;
+            * ) err "unsupported argument $1"; usage; exit 1 ;;
+        esac
+    done
+
+    dbg "TAG=$TAG"
+    dbg "topdir=$topdir"
+
+    # The tag used in dockerhub is the base image with : converted to -
+    upstream_tag="$(echo "$IMAGE" | tr : -)"
+    info "Base docker image $IMAGE -> tag $tag"
+
+    pull_and_tag prplmesh-builder "$upstream_tag" "$TAG"
+    pull_and_tag prplmesh-runner "$upstream_tag" "$TAG"
+}
+
+VERBOSE=false
+NATIVE=false
+IMAGE="ubuntu:18.04"
+
+main $@
