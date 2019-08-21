f92e5e6fcf0705d31d7e7b54e15413d1a486d2b5
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-08-21 17:53:30 +0200

    tools: test_gw_repeater.sh: add force option
    Propagate the force option added to run.sh to test_gw_repeater.sh. If
    the containers are already running and the force option is not given,
    the running containers will be used. If the force option is given, the
    running containers will be stopped and removed, and new containers are
    created.
    
    Since test.sh doesn't know the -f option, rename the OPT variable to
    VERBOSE_OPT and add FORCE_OPT to contain the -f option.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/tests/test_gw_repeater.sh
+++ tools/docker/tests/test_gw_repeater.sh
@@ -18,13 +18,14 @@ usage() {
     echo "      -v|--verbose - verbosity on"
     echo "      -d|--delay - delay between starting the containers
     and testing their status (default %DELAY seconds"
+    echo "      -f|--force - kill any running containers before starting"
     echo "      -g|--gateway - gateway container name"
     echo "      -r|--repeater - repeater container name"
     echo "      --rm - remove containers after test completes"
 }
 
 main() {
-    OPTS=`getopt -o 'hvd:g:r:' --long help,verbose,rm,gateway-only,delay:,gateway:,repeater: -n 'parse-options' -- "$@"`
+    OPTS=`getopt -o 'hvd:fg:r:' --long help,verbose,rm,gateway-only,delay:,force,gateway:,repeater: -n 'parse-options' -- "$@"`
 
     if [ $? != 0 ] ; then err "Failed parsing options." >&2 ; usage; exit 1 ; fi
 
@@ -32,9 +33,10 @@ main() {
 
     while true; do
         case "$1" in
-            -v | --verbose)       VERBOSE=true; OPT="-v"; shift ;;
+            -v | --verbose)       VERBOSE=true; VERBOSE_OPT="-v"; shift ;;
             -h | --help)          usage; exit 0; shift ;;
             -d | --delay)         DELAY="$2"; shift; shift ;;
+            -f | --force)         FORCE_OPT="-f"; shift ;;
             -g | --gateway)       GW_NAME="$2"; shift; shift ;;
             -r | --repeater)      REPEATER_NAME="$2"; shift; shift ;;
             --rm)                 REMOVE=true; shift ;;
@@ -53,11 +55,11 @@ main() {
     dbg DELAY=$DELAY
 
     status "Start GW (Controller + local Agent)"
-    ${scriptdir}/../run.sh ${OPT} start-controller-agent -d -n ${GW_NAME} -m 00:11:22:33 "$@"
+    ${scriptdir}/../run.sh ${VERBOSE_OPT} ${FORCE_OPT} start-controller-agent -d -n ${GW_NAME} -m 00:11:22:33 "$@"
     [ -z $GATEWAY_ONLY ] && {
         sleep ${DELAY}
         status "Start Repeater (Remote Agent)"
-        ${scriptdir}/../run.sh ${OPT} start-agent -d -n ${REPEATER_NAME} -m aa:bb:cc:dd "$@"
+        ${scriptdir}/../run.sh ${VERBOSE_OPT} ${FORCE_OPT} start-agent -d -n ${REPEATER_NAME} -m aa:bb:cc:dd "$@"
     }
 
     status "Delay ${DELAY} seconds..."
@@ -65,10 +67,10 @@ main() {
 
     error=0
     report "GW operational" \
-        ${scriptdir}/../test.sh ${OPT} -n ${GW_NAME}
+        ${scriptdir}/../test.sh ${VERBOSE_OPT} -n ${GW_NAME}
     [ -z $GATEWAY_ONLY ] && {
         report "Repeater operational" \
-            ${scriptdir}/../test.sh ${OPT} -n ${REPEATER_NAME}
+            ${scriptdir}/../test.sh ${VERBOSE_OPT} -n ${REPEATER_NAME}
     }
     [ "$REMOVE" = "true" ] && {
         status "Deleting containers ${GW_NAME}, ${REPEATER_NAME}"
