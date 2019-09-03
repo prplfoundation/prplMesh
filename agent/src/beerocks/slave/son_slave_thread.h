3c60ba18911f4efe8103b15eb32f1eae36a46db9
Coral Malachi <coral.malachi@intel.com>
2019-09-03 10:26:00 +0300

    agent: Add ack handler
    According to section 5.8.1 in the EasyMesh Test Plan v1.0 [1],
    when the MCUT receives a client steering BTM report, an
    ACK message needs to be sent back to the agent.
    
    Therefore, the following changes were made:
    
    1. The agent subscribes to a message of type: ACK_MESSAGE.
    
    2. Add handle method which responsible for handling the ACK when
       receiving at the agent, and print: "Received ACK_MESSAGE".
    
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

--- agent/src/beerocks/slave/son_slave_thread.h
+++ agent/src/beerocks/slave/son_slave_thread.h
@@ -291,6 +291,7 @@ private:
     bool handle_ap_capability_query(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_client_association_request(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_client_steering_request(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
+    bool handle_ack_message(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
 };
 
 } // namespace son
