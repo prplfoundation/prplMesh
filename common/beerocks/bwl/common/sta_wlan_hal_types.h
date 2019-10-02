4d6b5abb8695b2be88af5d7d2e6697ae4666d8b3
Vitaly Bukhovsky <vitaly@devalore.com>
2019-10-02 20:36:25 +0300

    bwl: sync rdkb dwpal changes

---
+++ common/beerocks/bwl/common/sta_wlan_hal_types.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2016-2019 Intel Corporation
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _BWL_STA_WLAN_HAL_TYPES_H_
+#define _BWL_STA_WLAN_HAL_TYPES_H_
+
+#include <stdint.h>
+
+#include <beerocks/bcl/network/net_struct.h>
+
+namespace bwl {
+
+// 16 Octets + 4 dashes + null terminator RFC 4122
+#define UUID_STR_SIZE (16 * 2 + 4 + 4)
+
+struct SScanResult {
+    sMacAddr bssid;
+    uint8_t channel;
+    int8_t rssi;
+};
+
+} // namespace bwl
+
+#endif // _BWL_MON_WLAN_HAL_TYPES_H_
