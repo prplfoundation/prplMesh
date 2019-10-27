1e49c9e99cba49fabaf32a5cf3504c84d723a4f8
Gal Wiener <gal.wiener@intel.com>
2019-10-27 17:10:46 +0200

    autogen: update tlvparser header and cpp
    Signed-off-by: Gal Wiener <gal.wiener@intel.com>

--- framework/tlvf/AutoGenerated/include/tlvf/ieee_1905_1/tlvParser.h
+++ framework/tlvf/AutoGenerated/include/tlvf/ieee_1905_1/tlvParser.h
@@ -16,12 +16,13 @@
 #include <cstddef>
 #include <stdint.h>
 #include <tlvf/swap.h>
+#include <tlvf/CmduMessageRx.h>
 #include <tlvf/ieee_1905_1/tlvEndOfMessage.h>
 #include <tlvf/ieee_1905_1/tlvAlMacAddressType.h>
 #include <tlvf/ieee_1905_1/tlvMacAddress.h>
 #include <tlvf/ieee_1905_1/tlvDeviceInformation.h>
 #include <tlvf/ieee_1905_1/tlvDeviceBridgingCapability.h>
-#include <tlvf/ieee_1905_1/tlvNon1905NeighborDeviceList.h>
+#include <tlvf/ieee_1905_1/tlvNon1905neighborDeviceList.h>
 #include <tlvf/ieee_1905_1/tlv1905NeighborDevice.h>
 #include <tlvf/ieee_1905_1/tlvLinkMetricQuery.h>
 #include <tlvf/ieee_1905_1/tlvTransmitterLinkMetric.h>
@@ -32,54 +33,32 @@
 #include <tlvf/ieee_1905_1/tlvAutoconfigFreqBand.h>
 #include <tlvf/ieee_1905_1/tlvSupportedRole.h>
 #include <tlvf/ieee_1905_1/tlvSupportedFreqBand.h>
-#include <tlvf/ieee_1905_1/tlvWsc.h>
 #include <tlvf/ieee_1905_1/tlvPushButtonEventNotification.h>
 #include <tlvf/ieee_1905_1/tlvPushButtonJoinNotification.h>
 #include <tlvf/wfa_map/tlvSupportedService.h>
 #include <tlvf/wfa_map/tlvSearchedService.h>
 #include <tlvf/wfa_map/tlvApRadioIdentifier.h>
-#include <tlvf/wfa_map/tlvApOperationalBss.h>
-#include <tlvf/wfa_map/tlvAssociatedClients.h>
 #include <tlvf/wfa_map/tlvApRadioBasicCapabilities.h>
-#include <tlvf/wfa_map/tlvApHtCapabilities.h>
-#include <tlvf/wfa_map/tlvApVhtCapabilities.h>
-#include <tlvf/wfa_map/tlvApHeCapabilities.h>
-#include <tlvf/wfa_map/tlvSteeringPolicy.h>
-#include <tlvf/wfa_map/tlvMetricReportingPolicy.h>
 #include <tlvf/wfa_map/tlvChannelPreference.h>
 #include <tlvf/wfa_map/tlvRadioOperationRestriction.h>
 #include <tlvf/wfa_map/tlvTransmitPowerLimit.h>
 #include <tlvf/wfa_map/tlvChannelSelectionResponse.h>
 #include <tlvf/wfa_map/tlvOperatingChannelReport.h>
-#include <tlvf/wfa_map/tlvClientInfo.h>
-#include <tlvf/wfa_map/tlvClientCapabilityReport.h>
 #include <tlvf/wfa_map/tlvClientAssociationEvent.h>
 #include <tlvf/wfa_map/tlvApMetricQuery.h>
-#include <tlvf/wfa_map/tlvApMetric.h>
-#include <tlvf/wfa_map/tlvStamacAddressType.h>
-#include <tlvf/wfa_map/tlvAssociatedStaLinkMetrics.h>
-#include <tlvf/wfa_map/tlvUnassociatedStaLinkMetricsQuery.h>
-#include <tlvf/wfa_map/tlvUnassociatedStaLinkMetricsResponse.h>
-#include <tlvf/wfa_map/tlvBeaconMetricsQuery.h>
-#include <tlvf/wfa_map/tlvBeaconMetricsResponse.h>
 #include <tlvf/wfa_map/tlvSteeringRequest.h>
-#include <tlvf/wfa_map/tlvSteeringBtmReport.h>
+#include <tlvf/wfa_map/tlvSteeringBTMReport.h>
 #include <tlvf/wfa_map/tlvClientAssociationControlRequest.h>
-#include <tlvf/wfa_map/tlvBackhaulSteeringRequest.h>
-#include <tlvf/wfa_map/tlvBackhaulSteeringResponse.h>
 #include <tlvf/wfa_map/tlvHigherLayerData.h>
 #include <tlvf/wfa_map/tlvApCapability.h>
-#include <tlvf/wfa_map/tlvAssociatedStaTrafficStats.h>
-#include <tlvf/wfa_map/tlvErrorCode.h>
 
 namespace ieee1905_1 {
 
 class tlvParser 
-
 {
     public:
-};
         static void ParseTlv(CmduMessageRx cmdu_rx);
+};
 
 }; // close namespace: ieee1905_1
 
--- framework/tlvf/AutoGenerated/src/tlvf/ieee_1905_1/tlvParser.cpp
+++ framework/tlvf/AutoGenerated/src/tlvf/ieee_1905_1/tlvParser.cpp
@@ -12,232 +12,145 @@
 
 #include "tlvParser.h"
 
-class tlvParser {
-
-    void ParseTlv(CmduMessageRx cmdu_rx)
+void ParseTlv(ieee1905_1::CmduMessageRx cmdu_rx)
+{
+    switch(cmdu_rx.getNextTlvType())
     {
-        switch(cmdu_rx.getNextTlvType())
-        {
-            case (0):{
-            cmdu_rx.addClass<ieee1905_1::tlvEndOfMessage>();
-            return;
-            }
-            case (1):{
-            cmdu_rx.addClass<ieee1905_1::tlvAlMacAddressType>();
-            return;
-            }
-            case (2):{
-            cmdu_rx.addClass<ieee1905_1::tlvMacAddress>();
-            return;
-            }
-            case (3):{
-            cmdu_rx.addClass<ieee1905_1::tlvDeviceInformation>();
-            return;
-            }
-            case (4):{
-            cmdu_rx.addClass<ieee1905_1::tlvDeviceBridgingCapability>();
-            return;
-            }
-            case (6):{
-            cmdu_rx.addClass<ieee1905_1::tlvNon1905NeighborDeviceList>();
-            return;
-            }
-            case (7):{
-            cmdu_rx.addClass<ieee1905_1::tlv1905NeighborDevice>();
-            return;
-            }
-            case (8):{
-            cmdu_rx.addClass<ieee1905_1::tlvLinkMetricQuery>();
-            return;
-            }
-            case (9):{
-            cmdu_rx.addClass<ieee1905_1::tlvTransmitterLinkMetric>();
-            return;
-            }
-            case (10):{
-            cmdu_rx.addClass<ieee1905_1::tlvReceiverLinkMetric>();
-            return;
-            }
-            case (11):{
-            cmdu_rx.addClass<ieee1905_1::tlvVendorSpecific>();
-            return;
-            }
-            case (12):{
-            cmdu_rx.addClass<ieee1905_1::tlvLinkMetricResultCode>();
-            return;
-            }
-            case (13):{
-            cmdu_rx.addClass<ieee1905_1::tlvSearchedRole>();
-            return;
-            }
-            case (14):{
-            cmdu_rx.addClass<ieee1905_1::tlvAutoconfigFreqBand>();
-            return;
-            }
-            case (15):{
-            cmdu_rx.addClass<ieee1905_1::tlvSupportedRole>();
-            return;
-            }
-            case (16):{
-            cmdu_rx.addClass<ieee1905_1::tlvSupportedFreqBand>();
-            return;
-            }
-            case (17):{
-            cmdu_rx.addClass<ieee1905_1::tlvWsc>();
-            return;
-            }
-            case (18):{
-            cmdu_rx.addClass<ieee1905_1::tlvPushButtonEventNotification>();
-            return;
-            }
-            case (19):{
-            cmdu_rx.addClass<ieee1905_1::tlvPushButtonJoinNotification>();
-            return;
-            }
-            case (128):{
-            cmdu_rx.addClass<wfa_map::tlvSupportedService>();
-            return;
-            }
-            case (129):{
-            cmdu_rx.addClass<wfa_map::tlvSearchedService>();
-            return;
-            }
-            case (130):{
-            cmdu_rx.addClass<wfa_map::tlvApRadioIdentifier>();
-            return;
-            }
-            case (131):{
-            cmdu_rx.addClass<wfa_map::tlvApOperationalBss>();
-            return;
-            }
-            case (132):{
-            cmdu_rx.addClass<wfa_map::tlvAssociatedClients>();
-            return;
-            }
-            case (133):{
-            cmdu_rx.addClass<wfa_map::tlvApRadioBasicCapabilities>();
-            return;
-            }
-            case (134):{
-            cmdu_rx.addClass<wfa_map::tlvApHtCapabilities>();
-            return;
-            }
-            case (135):{
-            cmdu_rx.addClass<wfa_map::tlvApVhtCapabilities>();
-            return;
-            }
-            case (136):{
-            cmdu_rx.addClass<wfa_map::tlvApHeCapabilities>();
-            return;
-            }
-            case (137):{
-            cmdu_rx.addClass<wfa_map::tlvSteeringPolicy>();
-            return;
-            }
-            case (138):{
-            cmdu_rx.addClass<wfa_map::tlvMetricReportingPolicy>();
-            return;
-            }
-            case (139):{
-            cmdu_rx.addClass<wfa_map::tlvChannelPreference>();
-            return;
-            }
-            case (140):{
-            cmdu_rx.addClass<wfa_map::tlvRadioOperationRestriction>();
-            return;
-            }
-            case (141):{
-            cmdu_rx.addClass<wfa_map::tlvTransmitPowerLimit>();
-            return;
-            }
-            case (142):{
-            cmdu_rx.addClass<wfa_map::tlvChannelSelectionResponse>();
-            return;
-            }
-            case (143):{
-            cmdu_rx.addClass<wfa_map::tlvOperatingChannelReport>();
-            return;
-            }
-            case (144):{
-            cmdu_rx.addClass<wfa_map::tlvClientInfo>();
-            return;
-            }
-            case (145):{
-            cmdu_rx.addClass<wfa_map::tlvClientCapabilityReport>();
-            return;
-            }
-            case (146):{
-            cmdu_rx.addClass<wfa_map::tlvClientAssociationEvent>();
-            return;
-            }
-            case (147):{
-            cmdu_rx.addClass<wfa_map::tlvApMetricQuery>();
-            return;
-            }
-            case (148):{
-            cmdu_rx.addClass<wfa_map::tlvApMetric>();
-            return;
-            }
-            case (149):{
-            cmdu_rx.addClass<wfa_map::tlvStamacAddressType>();
-            return;
-            }
-            case (150):{
-            cmdu_rx.addClass<wfa_map::tlvAssociatedStaLinkMetrics>();
-            return;
-            }
-            case (151):{
-            cmdu_rx.addClass<wfa_map::tlvUnassociatedStaLinkMetricsQuery>();
-            return;
-            }
-            case (152):{
-            cmdu_rx.addClass<wfa_map::tlvUnassociatedStaLinkMetricsResponse>();
-            return;
-            }
-            case (153):{
-            cmdu_rx.addClass<wfa_map::tlvBeaconMetricsQuery>();
-            return;
-            }
-            case (154):{
-            cmdu_rx.addClass<wfa_map::tlvBeaconMetricsResponse>();
-            return;
-            }
-            case (155):{
-            cmdu_rx.addClass<wfa_map::tlvSteeringRequest>();
-            return;
-            }
-            case (156):{
-            cmdu_rx.addClass<wfa_map::tlvSteeringBtmReport>();
-            return;
-            }
-            case (157):{
-            cmdu_rx.addClass<wfa_map::tlvClientAssociationControlRequest>();
-            return;
-            }
-            case (158):{
-            cmdu_rx.addClass<wfa_map::tlvBackhaulSteeringRequest>();
-            return;
-            }
-            case (159):{
-            cmdu_rx.addClass<wfa_map::tlvBackhaulSteeringResponse>();
-            return;
-            }
-            case (160):{
-            cmdu_rx.addClass<wfa_map::tlvHigherLayerData>();
-            return;
-            }
-            case (161):{
-            cmdu_rx.addClass<wfa_map::tlvApCapability>();
-            return;
-            }
-            case (162):{
-            cmdu_rx.addClass<wfa_map::tlvAssociatedStaTrafficStats>();
-            return;
-            }
-            case (163):{
-            cmdu_rx.addClass<wfa_map::tlvErrorCode>();
-            return;
-            }
+        case (0):{
+        cmdu_rx.addClass<ieee1905_1::tlvEndOfMessage>();
+        return;
+        }
+        case (1):{
+        cmdu_rx.addClass<ieee1905_1::tlvAlMacAddressType>();
+        return;
+        }
+        case (2):{
+        cmdu_rx.addClass<ieee1905_1::tlvMacAddress>();
+        return;
+        }
+        case (3):{
+        cmdu_rx.addClass<ieee1905_1::tlvDeviceInformation>();
+        return;
+        }
+        case (4):{
+        cmdu_rx.addClass<ieee1905_1::tlvDeviceBridgingCapability>();
+        return;
+        }
+        case (6):{
+        cmdu_rx.addClass<ieee1905_1::tlvNon1905neighborDeviceList>();
+        return;
+        }
+        case (7):{
+        cmdu_rx.addClass<ieee1905_1::tlv1905NeighborDevice>();
+        return;
+        }
+        case (8):{
+        cmdu_rx.addClass<ieee1905_1::tlvLinkMetricQuery>();
+        return;
+        }
+        case (9):{
+        cmdu_rx.addClass<ieee1905_1::tlvTransmitterLinkMetric>();
+        return;
+        }
+        case (10):{
+        cmdu_rx.addClass<ieee1905_1::tlvReceiverLinkMetric>();
+        return;
+        }
+        case (11):{
+        cmdu_rx.addClass<ieee1905_1::tlvVendorSpecific>();
+        return;
+        }
+        case (12):{
+        cmdu_rx.addClass<ieee1905_1::tlvLinkMetricResultCode>();
+        return;
+        }
+        case (13):{
+        cmdu_rx.addClass<ieee1905_1::tlvSearchedRole>();
+        return;
+        }
+        case (14):{
+        cmdu_rx.addClass<ieee1905_1::tlvAutoconfigFreqBand>();
+        return;
+        }
+        case (15):{
+        cmdu_rx.addClass<ieee1905_1::tlvSupportedRole>();
+        return;
+        }
+        case (16):{
+        cmdu_rx.addClass<ieee1905_1::tlvSupportedFreqBand>();
+        return;
+        }
+        case (18):{
+        cmdu_rx.addClass<ieee1905_1::tlvPushButtonEventNotification>();
+        return;
+        }
+        case (19):{
+        cmdu_rx.addClass<ieee1905_1::tlvPushButtonJoinNotification>();
+        return;
+        }
+        case (128):{
+        cmdu_rx.addClass<wfa_map::tlvSupportedService>();
+        return;
+        }
+        case (129):{
+        cmdu_rx.addClass<wfa_map::tlvSearchedService>();
+        return;
+        }
+        case (130):{
+        cmdu_rx.addClass<wfa_map::tlvApRadioIdentifier>();
+        return;
+        }
+        case (133):{
+        cmdu_rx.addClass<wfa_map::tlvApRadioBasicCapabilities>();
+        return;
+        }
+        case (139):{
+        cmdu_rx.addClass<wfa_map::tlvChannelPreference>();
+        return;
+        }
+        case (140):{
+        cmdu_rx.addClass<wfa_map::tlvRadioOperationRestriction>();
+        return;
+        }
+        case (141):{
+        cmdu_rx.addClass<wfa_map::tlvTransmitPowerLimit>();
+        return;
+        }
+        case (142):{
+        cmdu_rx.addClass<wfa_map::tlvChannelSelectionResponse>();
+        return;
+        }
+        case (143):{
+        cmdu_rx.addClass<wfa_map::tlvOperatingChannelReport>();
+        return;
+        }
+        case (146):{
+        cmdu_rx.addClass<wfa_map::tlvClientAssociationEvent>();
+        return;
+        }
+        case (147):{
+        cmdu_rx.addClass<wfa_map::tlvApMetricQuery>();
+        return;
+        }
+        case (155):{
+        cmdu_rx.addClass<wfa_map::tlvSteeringRequest>();
+        return;
+        }
+        case (156):{
+        cmdu_rx.addClass<wfa_map::tlvSteeringBTMReport>();
+        return;
+        }
+        case (157):{
+        cmdu_rx.addClass<wfa_map::tlvClientAssociationControlRequest>();
+        return;
+        }
+        case (160):{
+        cmdu_rx.addClass<wfa_map::tlvHigherLayerData>();
+        return;
+        }
+        case (161):{
+        cmdu_rx.addClass<wfa_map::tlvApCapability>();
+        return;
         }
     }
 }
