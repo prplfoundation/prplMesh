6c45556177ffb61211abcd786b3a08fdcddc3a7c
Alex Kanter <alexx.kanter@intel.com>
2019-11-06 09:55:40 +0200

    tlvf: Add TLV_AP_METRIC
    Preparative commit for  save AP_METRICS_RESPONSE_MESSAGE TLVs to db.
    Added auto generated code for TLV_AP_METRIC.
    Important note:
    Tlvf doesn’t support optional parameters.
    Temp solution applied to pass certification.
    Following optional parameters affected:
     estimated_service_info_field_ac_be:
     estimated_service_info_field_ac_bk:
     estimated_service_info_field_ac_vo:
     estimated_service_info_field_ac_vi:
    
    W/A: define max size instead of dynamic.
    Opened task #471 regarding this issues.
    
    These parameters need to be reverted to dynamic (task  #473)
    
    Signed-off-by: Alex Kanter <alexx.kanter@intel.com>

---
+++ framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvApMetric.h
@@ -0,0 +1,94 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2016-2019 Intel Corporation
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_WFA_MAP_TLVAPMETRIC_H_
+#define _TLVF_WFA_MAP_TLVAPMETRIC_H_
+
+#include <cstddef>
+#include <stdint.h>
+#include <tlvf/swap.h>
+#include <string.h>
+#include <memory>
+#include <tlvf/BaseClass.h>
+#include "tlvf/wfa_map/eTlvTypeMap.h"
+#include "tlvf/common/sMacAddr.h"
+#include <tuple>
+#include <asm/byteorder.h>
+
+namespace wfa_map {
+
+
+class tlvApMetric : public BaseClass
+{
+    public:
+        tlvApMetric(uint8_t* buff, size_t buff_len, bool parse = false, bool swap_needed = false);
+        tlvApMetric(std::shared_ptr<BaseClass> base, bool parse = false, bool swap_needed = false);
+        ~tlvApMetric();
+
+        typedef struct sEstimatedService {
+            #if defined(__LITTLE_ENDIAN_BITFIELD)
+            uint8_t reserved : 4;
+            uint8_t include_ac_vi : 1;
+            uint8_t include_ac_vo : 1;
+            uint8_t include_ac_bk : 1;
+            uint8_t include_ac_be : 1;
+            #elif defined(__BIG_ENDIAN_BITFIELD)
+            uint8_t include_ac_be : 1;
+            uint8_t include_ac_bk : 1;
+            uint8_t include_ac_vo : 1;
+            uint8_t include_ac_vi : 1;
+            uint8_t reserved : 4;
+            #else
+            #error "Bitfield macros are not defined"
+            #endif
+            void struct_swap(){
+            }
+            void struct_init(){
+                include_ac_be = 0x1;
+            }
+        } __attribute__((packed)) sEstimatedService;
+        
+        const eTlvTypeMap& type();
+        const uint16_t& length();
+        sMacAddr& bssid();
+        uint8_t& channel_utilization();
+        uint16_t& number_of_stas_currently_associated();
+        sEstimatedService& estimated_service_parameters();
+        uint8_t* estimated_service_info_field_ac_be(size_t idx = 0);
+        uint8_t* estimated_service_info_field_ac_bk(size_t idx = 0);
+        uint8_t* estimated_service_info_field_ac_vo(size_t idx = 0);
+        uint8_t* estimated_service_info_field_ac_vi(size_t idx = 0);
+        void class_swap();
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eTlvTypeMap* m_type = nullptr;
+        uint16_t* m_length = nullptr;
+        sMacAddr* m_bssid = nullptr;
+        uint8_t* m_channel_utilization = nullptr;
+        uint16_t* m_number_of_stas_currently_associated = nullptr;
+        sEstimatedService* m_estimated_service_parameters = nullptr;
+        uint8_t* m_estimated_service_info_field_ac_be = nullptr;
+        size_t m_estimated_service_info_field_ac_be_idx__ = 0;
+        int m_lock_order_counter__ = 0;
+        uint8_t* m_estimated_service_info_field_ac_bk = nullptr;
+        size_t m_estimated_service_info_field_ac_bk_idx__ = 0;
+        uint8_t* m_estimated_service_info_field_ac_vo = nullptr;
+        size_t m_estimated_service_info_field_ac_vo_idx__ = 0;
+        uint8_t* m_estimated_service_info_field_ac_vi = nullptr;
+        size_t m_estimated_service_info_field_ac_vi_idx__ = 0;
+};
+
+}; // close namespace: wfa_map
+
+#endif //_TLVF/WFA_MAP_TLVAPMETRIC_H_
