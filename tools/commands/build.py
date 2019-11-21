6765f782031faaa2ff5eb5e274d685ec0bfc4fe4
Vitaly Bukhovsky <vitaly@devalore.com>
2019-11-21 13:33:22 +0200

    tools: maptools: don't override STAGING_DIR
    We set the STAGING_DIR environment variable from maptools because
    openwrt toolchains give a warning if it is not set. However, if
    STAGING_DIR is already set in the environment, we shouldn't override it.
    
    Only set STAGING_DIR if not set, and add a comment to explain why.
    
    Signed-off-by: Vitaly Bukhovsky <vitaly@devalore.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/commands/build.py
+++ tools/commands/build.py
@@ -21,7 +21,11 @@ class builder(object):
         self.build_path = "{}/{}".format(build_dir, name)
         self.install_path = install_dir
         self.env = os.environ.copy()
-        self.env["STAGING_DIR"] = ""
+
+        # If the STAGING_DIR environment variable is not set,
+        # use an empty variable for suppressing compiler warnings
+        if not "STAGING_DIR" in os.environ:
+            self.env["STAGING_DIR"] = ""
 
     def __str__(self):
         return "'{}' builder configuration:\n\tsrc_path: {}\n\tbuild_path: {}\n\tinstall_path: {}".format(self.name, self.src_path, self.build_path, self.install_path)
