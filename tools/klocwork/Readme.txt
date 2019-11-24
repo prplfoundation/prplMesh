669a49dce22e209cd237085a9c00147c4c9c52ad
Moran Shoeg <moranx.shoeg@intel.com>
2019-11-24 19:39:58 +0200

    tools: Port klocwork script changes from RDKB
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- tools/klocwork/Readme.txt
+++ tools/klocwork/Readme.txt
@@ -1,18 +1,57 @@
-open a new shell and make sure you are using the new path:
->> which kwauth
-Answer should be: /opt/ugw/klockwork/kw123/bin/kwauth
+### Running Klocwork on UGW/RDKB environment, on Intel Network
 
-Authenticate user with Klockwork server:
-#UGW:
->> kwauth --url https://klocwork-iind4.devtools.intel.com:8105
-#RDKB
->> kwauth --url https://klocwork3-jf.devtools.intel.com:8140/
+- Request access on ags.intel.com for the role:
+    > DevTools - Klocwork - JF - CHD-P - CHD-P Developer
 
-      
-Login: [WIN_USER_NAME]
-Password: [WIN_PASSWORD]
+- Open a new shell and make sure you are using the new path:
+    ``` bash
+    which kwauth
+    ```
+    Answer should be:
+    ```bash
+    /nfs/site/proj/chdsw_ci/common/kw/klocwork_12_3/user/bin/kwauth
+    ```
 
-# If the previous direction did not work, the klocwork is not intalled.
-# In that case, follow the "Klocwork desktop - standalone configuration (unconnected to KW Server)" direction on:
+- Authenticate user with Klockwork server once per environment/server:
+    **UGW:**
+    ``` bash
+    kwauth --url https://klocwork-iind4.devtools.intel.com:8105
+    ```
+
+    **RDKB:**
+    ```bash
+    kwauth --url https://klocwork3-jf.devtools.intel.com:8140/
+    ```
+
+    And use User and Password:
+
+    > Login: [WIN_USER_NAME]
+    > Password: [WIN_PASSWORD]
+
+ - Note: Make sure that 'external_toolchain.cfg' is configured properly on your environment.
+    If the file is not configured, config your chdlab to a platform which is configured to the same environment (UGW/RDKB):
+    ```bash
+    # On repo root (where '.repo' folder is):
+    set_git_env
+    chdlab config -s <setup_number>
+    chdlab pull
+    ```
+
+    Use maptools script to configure 'external_toolchain.cfg':
+    ```bash
+    maptools config -g
+    ```
+### Running Klocwork Linux environment, on non Intel Network
+Make sure that 'external_toolchain.cfg' is not exist on your environment.
+
+# Running Klocwork:
+```bash
+cd tools/klocwork/
+./kw.sh
+```
+-- OR --
+Trigger Klocwork task on VSCODE
+
+
+###### Reference and installation guide:
 https://wiki.ith.intel.com/display/CHDSE/Klocwork+on+Linux+Build+Servers+How-To
-# and replace the installation path to: "/opt/ugw/klockwork/kw123/"
--- tools/klocwork/kw.sh
+++ tools/klocwork/kw.sh
@@ -6,7 +6,7 @@
 #set -x
 
 echo kwcheck --version
-echo current folder: `pwd`
+echo current folder: "$(pwd)"
 echo number of input arguments: "$#"
 
 declare -a REPOS=("framework" "common" "controller" "agent")
@@ -21,7 +21,7 @@ declare PLATFORM_TYPE
 PASSIVE_MODE=false
 
 # read the options
-OPTS=`getopt -o p -n 'kw.sh' -- "$@"`
+OPTS=$(getopt -o p -n 'kw.sh' -- "$@")
 eval set -- "$OPTS"
 
 # extract options and their arguments into variables.
@@ -33,104 +33,185 @@ while true ; do
       esac   
 done
 
-PASSIVE_MODE_OPT=""
-if $PASSIVE_MODE ; then
-      PASSIVE_MODE_OPT="-f PASSIVE_MODE=ON"
-      echo "PASSIVE_MODE=ON"
-else
-      echo "PASSIVE_MODE=OFF"
-fi
-
 ################################################################
 ####################### Local Functions ########################
 ################################################################
 
 prepare_kw()
 {
-# Generate input script to klocwork checker
-rm -rf _GO_KW
-cat  > _GO_KW << DONE
-#!/bin/bash
-cd `pwd`/../../$REPO
-echo "starting kw from folder: \`pwd\`"
-echo "../tools/maptools.sh build $REPO $PASSIVE_MODE_OPT -c clean make"
-../tools/maptools.sh build $REPO $PASSIVE_MODE_OPT -c clean make
-exit
-DONE
+      ROOT_PATH=$(realpath "$(pwd)"/../../)
+      REPO_PATH=$(realpath "$(pwd)"/../../"$REPO")
+      declare TREE_PATH # only needed in this function
+      if [ -f ./../../external_toolchain.cfg ]; then
+            PLATFORM_TYPE=$(grep -Po "(?<=^TARGET_PLATFORM=).*" "$(realpath "$(pwd)"/../../external_toolchain.cfg)") # "ugw"/"rdkb"
+            TREE_PATH=$(grep -Po "(?<=^PLATFORM_BASE_DIR=).*" "$(realpath "$(pwd)"/../../external_toolchain.cfg)")
+            echo Found tree path: "$TREE_PATH"
+      else 
+            echo Did no found 'external_toolchain.cfg', assuming Linux enviorment
+            PLATFORM_TYPE="linux"
+      fi
+      echo platfrom identified: $PLATFORM_TYPE
+      REPORT_PATH=$REPO_PATH/kw_reports/$PLATFORM_TYPE
+      mkdir -p "$REPORT_PATH"
 
-ROOT_PATH=$(realpath `pwd`/../../)
-REPO_PATH=$(realpath `pwd`/../../$REPO)
-PLATFORM_TYPE=$(grep -Po "(?<=^TARGET_PLATFORM=).*" $(realpath `pwd`/../../external_toolchain.cfg)) # "ugw"/"rdkb"
-echo platfrom identified: $PLATFORM_TYPE
-REPORT_PATH=$REPO_PATH/kw_reports/$PLATFORM_TYPE
-mkdir -p $REPORT_PATH
+      # Remove old compilation script
+      rm -rf _GO_KW
 
-if [ "$PLATFORM_TYPE" = "rdkb" ]; then
-      TOOLCHAIN_PATH=$(realpath `pwd`/../../../../atom_rdkbos/build/tmp/work/core2-32-rdk-linux)
-      URL_PATH="https://klocwork3-jf.devtools.intel.com:8140/Atom-Puma7-RDKB"
-elif [ "$PLATFORM_TYPE" = "ugw" ]; then
-      TOOLCHAIN_PATH=$(grep -Po "(?<=^PLATFORM_BASE_DIR=).*" $(realpath `pwd`/../../external_toolchain.cfg))
-      URL_PATH="https://klocwork-iind4.devtools.intel.com:8105/UGW_master_grx350_rt"
-fi
+      # Generate input script to klocwork checker
+      if [ "$PLATFORM_TYPE" = "rdkb" ]; then
+            TOOLCHAIN_PATH=$(realpath "$(pwd)"/../../../../atom_rdkbos/build/tmp/work/core2-32-rdk-linux)
+            URL_PATH="https://klocwork3-jf.devtools.intel.com:8140/Atom-Puma7-RDKB"
+
+            # Generate compilation script for RDKB tree enviorment to be used by klocwork checker
+            {
+                  echo -e "#!/bin/bash"
+                  echo -e "cd ../../../../setup/"
+                  echo -e "source puma_setup -o"
+                  echo -e "echo starting kw from folder: \`pwd\`"
+                  echo -e "echo bitbake -c cleanall multiap-$REPO"
+                  echo -e "bitbake -c cleanall multiap-$REPO"
+                  echo -e "echo bitbake -c compile multiap-$REPO"
+                  echo -e "bitbake -c compile multiap-$REPO"
+                  echo -e "exit"
+            } > _GO_KW
+
+      elif [ "$PLATFORM_TYPE" = "ugw" ]; then
+            TOOLCHAIN_PATH=$(grep -Po "(?<=^PLATFORM_BASE_DIR=).*" "$(realpath "$(pwd)"/../../external_toolchain.cfg)")
+            URL_PATH="https://klocwork-iind4.devtools.intel.com:8105/UGW_master_grx350_rt"
+
+            # Generate compilation script for UGW tree enviorment to be used by klocwork checker
+            {
+                  echo -e "#!/bin/bash"
+                  echo -e "cd $TREE_PATH"
+                  echo -e "echo starting kw from folder: \`pwd\`"
+                  echo -e "echo make package/feeds/ugw_packages/multiap_$REPO/{clean,compile,install}"
+                  echo -e "make package/feeds/ugw_packages/multiap_$REPO/{clean,compile,install}"
+                  echo -e "exit"
+            } > _GO_KW
+
+            # Set compilation on UGW tree to use multiap standalone folders
+            sed -i -e "s|# CONFIG_multiap_${REPO}_USE_CUSTOM_SOURCE_DIR is not set|CONFIG_multiap_${REPO}_USE_CUSTOM_SOURCE_DIR=y|g" "$TREE_PATH"/.config
+            if ! grep -q "CONFIG_multiap_${REPO}_CUSTOM_SOURCE_DIR" "$TREE_PATH"/.config; then
+                  sed -i -e "/CONFIG_multiap_${REPO}_USE_CUSTOM_SOURCE_DIR/aCONFIG_multiap_${REPO}_CUSTOM_SOURCE_DIR=\"${REPO_PATH}\"" "$TREE_PATH"/.config
+            fi
+            
+
+      else # linux/other
+
+            PASSIVE_MODE_OPT=""
+            if $PASSIVE_MODE ; then
+                  PASSIVE_MODE_OPT="-f PASSIVE_MODE=ON"
+                  echo "PASSIVE_MODE=ON"
+            else
+                  echo "PASSIVE_MODE=OFF"
+            fi
+
+            # Generate compilation script using maptools script to be used by klocwork checker
+            {
+                  echo -e "#!/bin/bash"
+                  echo -e "cd ../"
+                  echo -e "echo starting kw from folder: \`pwd\`"
+                  echo -e "echo ./maptools.py build $REPO $PASSIVE_MODE_OPT -c clean make"
+                  echo -e "./maptools.py build $REPO $PASSIVE_MODE_OPT -c clean make"
+                  echo -e "exit"
+            } > _GO_KW
+
+      fi
+
+      chmod +x _GO_KW
 }
 
 kw()
 { 
-echo Performing KW on: $REPO.
+      echo Performing KW on: "$REPO".
 
-#prepare any build specific and paths before common section
-prepare_kw
+      #prepare any build specific and paths before common section
+      prepare_kw
 
-# Create a klocwork project based on the feeds compilation
-rm -rf .kw*/
-kwcheck create --url $URL_PATH || { rm -rf .kw*; kwcheck create; echo "*** WARNING: Creating local KW project, not synced with UGW/RDKB *** " ; }
-chmod +x _GO_KW
-kwshell -s ./_GO_KW
+      # Create a klocwork project based on the feeds compilation
+      rm -rf .kw*
 
-if [ "$PLATFORM_TYPE" = "ugw" ]; then
-# Add checkers/overrides that are used by UGW for SDL
-git archive --remote=ssh://git@gts-chd.intel.com:29418/sw_ugw/ugw_sw.git HEAD:kw_support/ kw_override.h | tar -x
-git archive --remote=ssh://git@gts-chd.intel.com:29418/sw_ugw/ugw_sw.git HEAD:kw_support/ klocwork_database.kb | tar -x
-git archive --remote=ssh://git@gts-chd.intel.com:29418/sw_ugw/ugw_sw.git HEAD:kw_support/ analysis_profile.pconf | tar -x
-kwcheck import kw_override.h
-kwcheck import klocwork_database.kb
-kwcheck import analysis_profile.pconf
-fi
+      echo Connecting to remote klocwork server...
+      kwcheck create --url $URL_PATH || \
+      { 
+            echo "*** ERROR: Connecting to KW server has failed *** ";
+            echo making sure klocwork is authenticated to the correct server, and try connect again
+            if [ "$PLATFORM_TYPE" = "rdkb" ]; then
+                  kwauth --url https://klocwork3-jf.devtools.intel.com:8140 || \
+                  {
+                        echo "*** ERROR: retval=$?, Could not authenticated to Klocwork server. exit. *** "
+                        exit
+                  }
+                  echo reconnecting to remote klocwork server...
+                  kwcheck create --url $URL_PATH || \
+                  {
+                        echo "*** ERROR: retval=$?, Could not reconnect to Klocwork server. exit. *** "
+                        exit
+                  }
+            elif [ "$PLATFORM_TYPE" = "ugw" ]; then
+                  kwauth --url https://klocwork-iind4.devtools.intel.com:8105
+                  {
+                        echo "*** ERROR: retval=$?, Could not authenticated to Klocwork server. exit. *** "
+                        exit
+                  }
+                  echo reconnecting to remote klocwork server...
+                  kwcheck create --url $URL_PATH || \
+                  {
+                        echo "*** ERROR: retval=$?, Could not reconnect to Klocwork server. exit. *** "
+                        exit
+                  }
+            else 
+                  echo "Creating local klockwork server"
+                  kwcheck create
+            fi 
+      }
 
-# Analyze and generate reports
-kwcheck run -j auto
-echo ""
-echo Generating reports...
-kwcheck list -F detailed --status 'Analyze','Fix' --report ${REPORT_PATH}/kwreport_all.log
-kwcheck list -F detailed --severity 1 --status 'Analyze','Fix' --report ${REPORT_PATH}/kwreport_critical.log
-kwcheck list -F detailed --severity 2 --status 'Analyze','Fix' --report ${REPORT_PATH}/kwreport_error.log
-kwcheck list -F detailed --severity 3 --status 'Analyze','Fix' --report ${REPORT_PATH}/kwreport_warning.log
-kwcheck list -F detailed --severity 4 --status 'Analyze','Fix' --report ${REPORT_PATH}/kwreport_review.log
-kwcheck list -F detailed --status 'Ignore','Not a Problem','Defer' --report ${REPORT_PATH}/kwreport_ignore.log
+      echo starting klockwork process
+      kwshell -s ./_GO_KW
 
-# finalize reports
-# remove local prefixes from source controlled reports
-declare -a KW_REPORTS=(${REPORT_PATH}/kwreport_all.log ${REPORT_PATH}/kwreport_critical.log ${REPORT_PATH}/kwreport_error.log ${REPORT_PATH}/kwreport_warning.log ${REPORT_PATH}/kwreport_review.log ${REPORT_PATH}/kwreport_ignore.log)
-for r in ${KW_REPORTS[@]}; do
-      cp $r ${r}.tmp
-      sed -i -e "s/${ROOT_PATH////\\/}\///g" $r # remove local path prefixes from multiap modules
-      sed -i -e "s/${TOOLCHAIN_PATH////\\/}\///g" $r # remove local path prefixes from external toolchain files
-done
+      if [ "$PLATFORM_TYPE" = "ugw" ]; then
+      # Add checkers/overrides that are used by UGW for SDL
+      git archive --remote=ssh://git@gts-chd.intel.com:29418/sw_ugw/ugw_sw.git HEAD:kw_support/ kw_override.h | tar -x
+      git archive --remote=ssh://git@gts-chd.intel.com:29418/sw_ugw/ugw_sw.git HEAD:kw_support/ klocwork_database.kb | tar -x
+      git archive --remote=ssh://git@gts-chd.intel.com:29418/sw_ugw/ugw_sw.git HEAD:kw_support/ analysis_profile.pconf | tar -x
+      kwcheck import kw_override.h
+      kwcheck import klocwork_database.kb
+      kwcheck import analysis_profile.pconf
+      fi
 
-# Generate output summary
-declare -a KW_TYPES=("1:Critical" "2:Error" "3:Warning" "4:Review")
-echo -e "Summary by components:" > ${REPORT_PATH}/kwreport_summary.log
-cp ${REPORT_PATH}/kwreport_all.log ${REPORT_PATH}/kwreport_tmp.log
-for t in ${KW_TYPES[@]}; do
-      issue_cnt=`grep -i $t ${REPORT_PATH}/kwreport_all.log | grep -civ "Ignore\|Not a Problem\|Defer"`
-      echo "    $t: $issue_cnt" >> ${REPORT_PATH}/kwreport_summary.log
-done
+      # Analyze and generate reports
+      kwcheck run -j auto
+      echo ""
+      echo Generating reports...
+      kwcheck list -F detailed --status 'Analyze','Fix' --report "${REPORT_PATH}"/kwreport_all.log
+      kwcheck list -F detailed --severity 1 --status 'Analyze','Fix' --report "${REPORT_PATH}"/kwreport_critical.log
+      kwcheck list -F detailed --severity 2 --status 'Analyze','Fix' --report "${REPORT_PATH}"/kwreport_error.log
+      kwcheck list -F detailed --severity 3 --status 'Analyze','Fix' --report "${REPORT_PATH}"/kwreport_warning.log
+      kwcheck list -F detailed --severity 4 --status 'Analyze','Fix' --report "${REPORT_PATH}"/kwreport_review.log
+      kwcheck list -F detailed --status 'Ignore','Not a Problem','Defer' --report "${REPORT_PATH}"/kwreport_ignore.log
 
-rm ${REPORT_PATH}/kwreport_tmp.log
-echo -e "\nLast KW: `date +'%d/%m/%Y %H:%M'`" >> ${REPORT_PATH}/kwreport_summary.log
+      # finalize reports
+      # remove local prefixes from source controlled reports
+      declare -a KW_REPORTS=(${REPORT_PATH}/kwreport_all.log ${REPORT_PATH}/kwreport_critical.log ${REPORT_PATH}/kwreport_error.log ${REPORT_PATH}/kwreport_warning.log ${REPORT_PATH}/kwreport_review.log ${REPORT_PATH}/kwreport_ignore.log)
+      for r in "${KW_REPORTS[@]}"; do
+            cp "$r" "${r}".tmp
+            sed -i -e "s/${ROOT_PATH////\\/}\///g" "$r" # remove local path prefixes from multiap modules
+            sed -i -e "s/${TOOLCHAIN_PATH////\\/}\///g" "$r" # remove local path prefixes from external toolchain files
+      done
 
-echo ""
+      # Generate output summary
+      declare -a KW_TYPES=("1:Critical" "2:Error" "3:Warning" "4:Review")
+      echo -e "Summary by components:" > "${REPORT_PATH}"/kwreport_summary.log
+      cp "${REPORT_PATH}"/kwreport_all.log "${REPORT_PATH}"/kwreport_tmp.log
+      for t in "${KW_TYPES[@]}"; do
+            issue_cnt=$(grep -i "$t" "${REPORT_PATH}"/kwreport_all.log | grep -civ "Ignore\|Not a Problem\|Defer")
+            echo "    $t: $issue_cnt" >> "${REPORT_PATH}"/kwreport_summary.log
+      done
 
+      rm "${REPORT_PATH}"/kwreport_tmp.log
+      echo -e "\nLast KW: $(date +'%d/%m/%Y %H:%M')" >> "${REPORT_PATH}"/kwreport_summary.log
+
+      echo ""
 }
 
 ################################################################
@@ -138,7 +219,7 @@ echo ""
 ################################################################
 
 # Repo Select
-read -p "On which repo do you with to perfrom klocwork? [0-all, 1-framework, 2-common, 3-controller, 4-agent]: " REPO
+read -pr "On which repo do you with to perfrom klocwork? [0-all, 1-framework, 2-common, 3-controller, 4-agent]: " REPO
 case $REPO in
       "0") REPO="all"          ;;
       "1") REPO="framework"    ;;
@@ -153,7 +234,7 @@ esac
 
 if [ "$REPO" = "all" ]; then
       echo "Performing KW on all repos!" 
-      for REPO in ${REPOS[@]}; do
+      for REPO in "${REPOS[@]}"; do
             kw
       done
 else
@@ -161,6 +242,3 @@ else
 fi
 
 echo DONE!
-
-
-
