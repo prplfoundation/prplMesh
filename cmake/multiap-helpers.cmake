262dc2072f2a34b35e5132b6748c7f36771253bc
Vitaly Bukhovsky <vitaly@devalore.com>
2019-11-26 12:16:35 +0200

    cmake: Add CMake color variables to the multiap-helpers.cmake file
    The multiap-helpers.cmake file is included from the top CMake file.
    
    Signed-off-by: Vitaly Bukhovsky <vitaly@devalore.com>

--- cmake/multiap-helpers.cmake
+++ cmake/multiap-helpers.cmake
@@ -1,5 +1,30 @@
+if(NOT WIN32)
+  string(ASCII 27 Esc)
+  set(ColourReset "${Esc}[m")
+  set(ColourBold  "${Esc}[1m")
+  set(Red         "${Esc}[31m")
+  set(Green       "${Esc}[32m")
+  set(Yellow      "${Esc}[33m")
+  set(Blue        "${Esc}[34m")
+  set(Magenta     "${Esc}[35m")
+  set(Cyan        "${Esc}[36m")
+  set(White       "${Esc}[37m")
+  set(BoldRed     "${Esc}[1;31m")
+  set(BoldGreen   "${Esc}[1;32m")
+  set(BoldYellow  "${Esc}[1;33m")
+  set(BoldBlue    "${Esc}[1;34m")
+  set(BoldMagenta "${Esc}[1;35m")
+  set(BoldCyan    "${Esc}[1;36m")
+  set(BoldWhite   "${Esc}[1;37m")
+endif()
+
 if (NOT ${PROJECT}_REVISION)
-    execute_process(COMMAND "git" "rev-parse" "HEAD" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE ${PROJECT}_REVISION)
+    execute_process(
+        COMMAND "git" "rev-parse" "HEAD"
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_STRIP_TRAILING_WHITESPACE 
+        OUTPUT_VARIABLE ${PROJECT}_REVISION
+    )
 endif()
 set(${PROJECT}_VERSION_MAJOR 1)
 set(${PROJECT}_VERSION_MINOR 4)
