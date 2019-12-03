e4e696ec952da8d86d7d11fc0ef74a7536778271
Moran Shoeg <moranx.shoeg@intel.com>
2019-12-03 16:45:33 +0200

    bpl: Removed unused bpl error codes
    Removed unused BPL error code from bpl_err.h.
    A lot of the error which has been removed is related to non-passive
    mode code cleanups.
    As well aligned the code which was messed up by clang-format.
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- framework/platform/bpl/include/bpl/bpl_err.h
+++ framework/platform/bpl/include/bpl/bpl_err.h
@@ -26,74 +26,54 @@ extern "C" {
 
 /* Beerocks Error Codes */
 
-#define FOREACH_ERROR_CODE(ERROR_CODE)                                                             \
-    ERROR_CODE(BPL_ERR_NONE)                                                                       \
-    ERROR_CODE(BPL_ERR_UNKNOWN)                                                                    \
-    ERROR_CODE(BPL_ERR_OPERATION_NOT_SUPPORTED)                                                    \
-    ERROR_CODE(BPL_ERR_BH_READING_DATA_FROM_THE_BRIDGE)                                            \
-    ERROR_CODE(BPL_ERR_BH_TIMEOUT_ATTACHING_TO_WPA_SUPPLICANT)                                     \
-    ERROR_CODE(BPL_ERR_BH_SCAN_FAILED_TO_INITIATE_SCAN)                                            \
-    ERROR_CODE(BPL_ERR_BH_SCAN_TIMEOUT)                                                            \
-    ERROR_CODE(BPL_ERR_BH_FAILED_TO_RETRIEVE_SCAN_RESULTS)                                         \
-    ERROR_CODE(BPL_ERR_BH_SCAN_EXCEEDED_MAXIMUM_FAILED_SCAN_ATTEMPTS)                              \
-    ERROR_CODE(BPL_ERR_BH_OBTAINING_DHCP_BRIDGE_INTERFACE)                                         \
-    ERROR_CODE(BPL_ERR_BH_OBTAINING_DHCP_BRIDGE_INTERFACE_MAX_ATTEMPTS)                            \
-    ERROR_CODE(BPL_ERR_BH_CONNECTING_TO_MASTER)                                                    \
-    ERROR_CODE(BPL_ERR_BH_CONNECTING_TO_MASTER_USING_UDS)                                          \
-    ERROR_CODE(BPL_ERR_BH_DEVICE_QUERY_TIMEOUT)                                                    \
-    ERROR_CODE(BPL_ERR_BH_ASSOCIATE_3ADDR)                                                         \
-    ERROR_CODE(BPL_ERR_BH_ASSOCIATE_3ADDR_TIMEOUT)                                                 \
-    ERROR_CODE(BPL_ERR_BH_ASSOCIATE_4ADDR_TIMEOUT)                                                 \
-    ERROR_CODE(BPL_ERR_BH_ASSOCIATE_4ADDR_FAILURE)                                                 \
-    ERROR_CODE(BPL_ERR_BH_TRANSITION_FROM_3ADDR_TO_4ADDR)                                          \
-    ERROR_CODE(BPL_ERR_BH_ROAMING)                                                                 \
-    ERROR_CODE(BPL_ERR_BH_DISCONNECTED)                                                            \
-    ERROR_CODE(BPL_ERR_BH_WPA_SUPPLICANT_TERMINATED)                                               \
-    ERROR_CODE(BPL_ERR_BH_MASTER_SOCKET_DISCONNECTED)                                              \
-    ERROR_CODE(BPL_ERR_BH_SLAVE_SOCKET_DISCONNECTED)                                               \
-    ERROR_CODE(BPL_ERR_BH_OPEN_UDP_SOCKET_FAILURE)                                                 \
-    ERROR_CODE(BPL_ERR_BH_STOPPED)                                                                 \
-    ERROR_CODE(BPL_ERR_SLAVE_CONNECTING_TO_BACKHAUL_MANAGER)                                       \
-    ERROR_CODE(BPL_ERR_SLAVE_TIMEOUT_GET_WLAN_READY_STATUS_REQUEST)                                \
-    ERROR_CODE(BPL_ERR_SLAVE_TIMEOUT_WIFI_CREDENTIALS_SET_REQUEST)                                 \
-    ERROR_CODE(BPL_ERR_SLAVE_TIMEOUT_IFACE_ENABLE_REQUEST)                                         \
-    ERROR_CODE(BPL_ERR_SLAVE_TIMEOUT_IFACE_DISABLE_REQUEST)                                        \
-    ERROR_CODE(BPL_ERR_SLAVE_TIMEOUT_IFACE_RESTORE_REQUEST)                                        \
-    ERROR_CODE(BPL_ERR_SLAVE_TIMEOUT_IFACE_RESTART_REQUEST)                                        \
-    ERROR_CODE(BPL_ERR_SLAVE_MASTER_KEEP_ALIVE_TIMEOUT)                                            \
-    ERROR_CODE(BPL_ERR_SLAVE_INVALID_MASTER_SOCKET)                                                \
-    ERROR_CODE(BPL_ERR_SLAVE_IFACE_CHANGE_STATE_FAILED)                                            \
-    ERROR_CODE(BPL_ERR_SLAVE_IFACE_RESTORE_FAILED)                                                 \
-    ERROR_CODE(BPL_ERR_SLAVE_WIFI_CREDENTIALS_SET_FAILED)                                          \
-    ERROR_CODE(BPL_ERR_SLAVE_POST_INIT_CONFIG_FAILED)                                              \
-    ERROR_CODE(BPL_ERR_SLAVE_TX_CHANGE_STATE_FAILED)                                               \
-    ERROR_CODE(BPL_ERR_SLAVE_UPDATE_CREDENTIALS_FAILED)                                            \
-    ERROR_CODE(BPL_ERR_SLAVE_FAILED_CONNECT_TO_PLATFORM_MANAGER)                                   \
-    ERROR_CODE(BPL_ERR_SLAVE_PLATFORM_MANAGER_REGISTER_TIMEOUT)                                    \
-    ERROR_CODE(BPL_ERR_SLAVE_SLAVE_BACKHAUL_MANAGER_DISCONNECTED)                                  \
-    ERROR_CODE(BPL_ERR_SLAVE_STOPPED)                                                              \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_START)                                                           \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_DISCONNECTED)                                                    \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_HOSTAP_DISABLED)                                                 \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_ATTACH_FAIL)                                                     \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_SUDDEN_DETACH)                                                   \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_HAL_DISCONNECTED)                                                \
-    ERROR_CODE(BPL_ERR_AP_MANAGER_CAC_TIMEOUT)                                                     \
-    ERROR_CODE(BPL_ERR_MONITOR_DISCONNECTED)                                                       \
-    ERROR_CODE(BPL_ERR_MONITOR_HOSTAP_DISABLED)                                                    \
-    ERROR_CODE(BPL_ERR_MONITOR_ATTACH_FAIL)                                                        \
-    ERROR_CODE(BPL_ERR_MONITOR_SUDDEN_DETACH)                                                      \
-    ERROR_CODE(BPL_ERR_MONITOR_HAL_DISCONNECTED)                                                   \
-    ERROR_CODE(BPL_ERR_MONITOR_REPORT_PROCESS_FAIL)                                                \
-    ERROR_CODE(BPL_ERR_CONFIG_PLATFORM_REPORTED_INVALID_CONFIGURATION)                             \
-    ERROR_CODE(BPL_ERR_CONFIG_BACKHAUL_WIRED_INTERFACE_IS_UNSUPPORTED)                             \
-    ERROR_CODE(BPL_ERR_CONFIG_BACKHAUL_WIRELESS_INTERFACE_IS_UNSUPPORTED)                          \
-    ERROR_CODE(BPL_ERR_CONFIG_NO_VALID_BACKHAUL_INTERFACE)                                         \
-    ERROR_CODE(BPL_ERR_CONFIG_BRIDGE_INTERFACE_IS_EMPTY)                                           \
-    ERROR_CODE(BPL_ERR_WIFI_CONFIGURATION_CHANGE_TIMEOUT)                                          \
-    ERROR_CODE(BPL_ERR_WATCHDOG_PROCESS_STUCK)                                                     \
-    ERROR_CODE(BPL_ERR_WATCHDOG_PROCESS_ZOMBIE)                                                    \
-    ERROR_CODE(BPL_ERR_LAST)
+// clang-format off
+
+#define FOREACH_ERROR_CODE(ERROR_CODE)                                                              \
+/* 00 */ ERROR_CODE(BPL_ERR_NONE)                                                                   \
+/* 01 */ ERROR_CODE(BPL_ERR_UNKNOWN)                                                                \
+/* 02 */ ERROR_CODE(BPL_ERR_OPERATION_NOT_SUPPORTED)                                                \
+/* 03 */ ERROR_CODE(BPL_ERR_BH_READING_DATA_FROM_THE_BRIDGE)                                        \
+/* 04 */ ERROR_CODE(BPL_ERR_BH_TIMEOUT_ATTACHING_TO_WPA_SUPPLICANT)                                 \
+/* 05 */ ERROR_CODE(BPL_ERR_BH_SCAN_FAILED_TO_INITIATE_SCAN)                                        \
+/* 06 */ ERROR_CODE(BPL_ERR_BH_SCAN_TIMEOUT)                                                        \
+/* 07 */ ERROR_CODE(BPL_ERR_BH_SCAN_EXCEEDED_MAXIMUM_FAILED_SCAN_ATTEMPTS)                          \
+/* 08 */ ERROR_CODE(BPL_ERR_BH_CONNECTING_TO_MASTER)                                                \
+/* 09 */ ERROR_CODE(BPL_ERR_BH_ASSOCIATE_4ADDR_TIMEOUT)                                             \
+/* 10 */ ERROR_CODE(BPL_ERR_BH_ASSOCIATE_4ADDR_FAILURE)                                             \
+/* 11 */ ERROR_CODE(BPL_ERR_BH_ROAMING)                                                             \
+/* 12 */ ERROR_CODE(BPL_ERR_BH_DISCONNECTED)                                                        \
+/* 13 */ ERROR_CODE(BPL_ERR_BH_WPA_SUPPLICANT_TERMINATED)                                           \
+/* 14 */ ERROR_CODE(BPL_ERR_BH_SLAVE_SOCKET_DISCONNECTED)                                           \
+/* 15 */ ERROR_CODE(BPL_ERR_BH_STOPPED)                                                             \
+/* 16 */ ERROR_CODE(BPL_ERR_SLAVE_CONNECTING_TO_BACKHAUL_MANAGER)                                   \
+/* 17 */ ERROR_CODE(BPL_ERR_SLAVE_MASTER_KEEP_ALIVE_TIMEOUT)                                        \
+/* 18 */ ERROR_CODE(BPL_ERR_SLAVE_INVALID_MASTER_SOCKET)                                            \
+/* 19 */ ERROR_CODE(BPL_ERR_SLAVE_FAILED_CONNECT_TO_PLATFORM_MANAGER)                               \
+/* 20 */ ERROR_CODE(BPL_ERR_SLAVE_PLATFORM_MANAGER_REGISTER_TIMEOUT)                                \
+/* 21 */ ERROR_CODE(BPL_ERR_SLAVE_SLAVE_BACKHAUL_MANAGER_DISCONNECTED)                              \
+/* 22 */ ERROR_CODE(BPL_ERR_SLAVE_STOPPED)                                                          \
+/* 23 */ ERROR_CODE(BPL_ERR_AP_MANAGER_START)                                                       \
+/* 24 */ ERROR_CODE(BPL_ERR_AP_MANAGER_DISCONNECTED)                                                \
+/* 25 */ ERROR_CODE(BPL_ERR_AP_MANAGER_HOSTAP_DISABLED)                                             \
+/* 26 */ ERROR_CODE(BPL_ERR_AP_MANAGER_ATTACH_FAIL)                                                 \
+/* 27 */ ERROR_CODE(BPL_ERR_AP_MANAGER_SUDDEN_DETACH)                                               \
+/* 28 */ ERROR_CODE(BPL_ERR_AP_MANAGER_HAL_DISCONNECTED)                                            \
+/* 29 */ ERROR_CODE(BPL_ERR_AP_MANAGER_CAC_TIMEOUT)                                                 \
+/* 30 */ ERROR_CODE(BPL_ERR_MONITOR_DISCONNECTED)                                                   \
+/* 31 */ ERROR_CODE(BPL_ERR_MONITOR_HOSTAP_DISABLED)                                                \
+/* 32 */ ERROR_CODE(BPL_ERR_MONITOR_ATTACH_FAIL)                                                    \
+/* 33 */ ERROR_CODE(BPL_ERR_MONITOR_SUDDEN_DETACH)                                                  \
+/* 34 */ ERROR_CODE(BPL_ERR_MONITOR_HAL_DISCONNECTED)                                               \
+/* 35 */ ERROR_CODE(BPL_ERR_MONITOR_REPORT_PROCESS_FAIL)                                            \
+/* 36 */ ERROR_CODE(BPL_ERR_CONFIG_PLATFORM_REPORTED_INVALID_CONFIGURATION)                         \
+/* 37 */ ERROR_CODE(BPL_ERR_CONFIG_BACKHAUL_WIRED_INTERFACE_IS_UNSUPPORTED)                         \
+/* 38 */ ERROR_CODE(BPL_ERR_CONFIG_BACKHAUL_WIRELESS_INTERFACE_IS_UNSUPPORTED)                      \
+/* 39 */ ERROR_CODE(BPL_ERR_CONFIG_NO_VALID_BACKHAUL_INTERFACE)                                     \
+/* 40 */ ERROR_CODE(BPL_ERR_WATCHDOG_PROCESS_STUCK)                                                 \
+/* 41 */ ERROR_CODE(BPL_ERR_WATCHDOG_PROCESS_ZOMBIE)                                                \
+/* 42 */ ERROR_CODE(BPL_ERR_LAST)
+
+// clang-format on
 
 // States ENUM
 enum eErrorCode { FOREACH_ERROR_CODE(GENERATE_ERROR_ENUM) };
