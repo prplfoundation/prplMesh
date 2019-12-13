73facd1d9a76255051670782740328e3c351152e
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-12-13 11:37:06 +0100

    common: bwl: correct sta_wlan_hal_dummy::process_dummy_event
    When commit f26011b0da changed the interface of process_dummy_event,
    sta_wlan_hal_dummy wasn't updated accordingly. This wasn't noticed until
    now because we don't test with wireless backhaul in dummy mode so
    sta_wlan_hal_dummy doesn't get used.
    
    Align the definition of process_dummy_event() with the definition in
    base_wlan_hal_dummy.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- common/beerocks/bwl/dummy/sta_wlan_hal_dummy.h
+++ common/beerocks/bwl/dummy/sta_wlan_hal_dummy.h
@@ -60,7 +60,7 @@ public:
     std::string get_bssid() override;
 
 protected:
-    virtual bool process_dummy_event(char *buffer, int bufLen, const std::string &opcode) override;
+    virtual bool process_dummy_event(parsed_obj_map_t &parsed_obj) override;
 
     // Overload for Monitor events
     bool event_queue_push(sta_wlan_hal::Event event, std::shared_ptr<void> data = {})
