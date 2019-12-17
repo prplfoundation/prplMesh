cb06c1086b9e76a3239225700210a2d5b404f80c
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-12-17 12:04:38 +0200

    framework: tlvf: add ClassList class
    Currently, the CmduMessage class conflates the CMDU-specific things with
    the general concept of a list of TLVs.
    
    Create a new class ClassList that generalizes the handling of lists of
    any TLVF generated classes (this includes lists of TLVs).
    
    In a later commit/PR, this will be used in CmduMessage to represent the
    list of TLVF classes that compose a CMDU - the CmduHeader and the list
    of TLVs themselves.
    
    In addition, this new container class will be used to represent inner
    TLVs data such as vendor specific TLVs which contain a beerocks
    operation header and class, and WSC TLVs which contains a list of
    attributes.
    
    All those will extend the ClassList to provide the parsing and creating
    capabilities for the inner data.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>
    Co-authored-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

---
+++ framework/tlvf/src/src/ClassList.cpp
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2019 Arnout Vandecappelle (Essensium/Mind)
+ * Copyright (c) 2019 Tomer Eliyahu (Intel)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <tlvf/ClassList.h>
+#include <tlvf/ieee_1905_1/tlvEndOfMessage.h>
+#include <tlvf/tlvflogging.h>
+
+ClassList::ClassList(uint8_t *buff, size_t buff_len, bool parse, bool swap)
+    : m_buff(buff), m_buff_len(buff_len), m_parse(parse), m_swap(swap)
+{
+}
+
+std::shared_ptr<BaseClass> ClassList::getClass(size_t idx) const
+{
+    if (m_class_vector.size() > idx) {
+        return std::shared_ptr<BaseClass>(m_class_vector.at(idx));
+    } else {
+        return nullptr;
+    }
+}
+
+size_t ClassList::getMessageLength() const
+{
+    if (m_buff == nullptr)
+        return 0;
+    size_t msg_len = 0;
+    for (auto const &c : m_class_vector) {
+        msg_len += c->getLen();
+    }
+    return msg_len;
+}
+
+void ClassList::swap()
+{
+    if (!m_buff)
+        return;
+
+    // call all tlv finalize functions
+    for (auto const &c : m_class_vector) {
+        c->class_swap();
+    }
+
+    m_swapped = !m_swapped;
+}
+
+void ClassList::reset(bool parse, bool swap)
+{
+    if (!parse)
+        memset(m_buff, 0, m_buff_len);
+    m_parse     = parse;
+    m_swap      = swap;
+    m_finalized = false;
+    m_swapped   = false;
+    for (auto &c : m_class_vector) {
+        c.reset();
+    }
+    m_class_vector.clear();
+}
+
+bool ClassList::finalize(bool swap_needed)
+{
+    if (m_class_vector.empty()) {
+        TLVF_LOG(ERROR) << "Finalize() called on an empty ClassList!";
+        return false;
+    }
+
+    if (m_finalized)
+        return true;
+
+    if (!m_class_vector.back()->isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "TLV post init failed";
+        return false;
+    }
+
+    if (swap_needed)
+        swap();
+
+    m_finalized = true;
+    return true;
+}
