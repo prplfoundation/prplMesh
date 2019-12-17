9c98cc8eba48010a1f66ea2afdb71bd8bf41ba0a
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-12-17 12:05:24 +0200

    common: tlvf: add beerocks_header class
    Preperative commit.
    Add beerocks_header header file which implements beerocks_header class
    and derives from ClassList.
    This will be used to replace the current beerocks vendor specific
    handling and align it to use the inner Class lists approach according to
    
    beerocks_header is not the best name for this class, since it is basically
    a list of TLV like classes - first being the action header (cACTION_HEADER),
    and the second is the operation itself.
    
    A more appropriate name would have been beerocks_actions, but to keep
    backward compatability, the name is kept (a comment is added in the code
    in case we'll ever want to change the name.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

---
+++ common/beerocks/tlvf/src/include/beerocks/tlvf/beerocks_header.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2019 Tomer Eliyahu (Intel Corporation)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_BEEROCKS_HEADER_H_
+#define _TLVF_BEEROCKS_HEADER_H_
+
+#include <beerocks/tlvf/beerocks_message_header.h>
+#include <tlvf/ClassList.h>
+
+namespace beerocks {
+
+/**
+ * @brief beerocks_header
+ *
+ * Each vendor specific message used for internal and external communication
+ * between the different components in prplMesh, via UDS or the BTL, includes
+ * an action header and the actual message data.
+ * These stored in the vendor specific TLV payload as a separate TLVlist.
+ *
+ * Note that this is not really a "header", a more appropriate name would be
+ * beerocks_actions - but since the name beerocks_header is widely used, we keep
+ * it for backward comparability. Same goes to all the getter functions.
+ *
+ */
+class beerocks_header : public ClassList {
+public:
+    beerocks_header() = delete;
+    beerocks_header(uint8_t *buff, size_t buff_len, bool parse) : ClassList(buff, buff_len, parse)
+    {
+    }
+    std::shared_ptr<beerocks_message::cACTION_HEADER> actionhdr()
+    {
+        return getClass<beerocks_message::cACTION_HEADER>();
+    }
+    beerocks_message::eAction action()
+    {
+        return actionhdr() ? actionhdr()->action() : beerocks_message::ACTION_NONE;
+    }
+    uint8_t action_op() { return actionhdr() ? actionhdr()->action_op() : 0; };
+    uint16_t id() { return actionhdr() ? actionhdr()->id() : 0; }
+};
+
+} // namespace beerocks
+
+#endif // _TLVF_BEEROCKS_HEADER_H_
