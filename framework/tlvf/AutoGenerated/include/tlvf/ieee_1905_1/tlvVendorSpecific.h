19e29b14caa5c7f0ab92867038932ae2baa1695e
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2019-12-17 12:05:41 +0200

    tlvf: add payload to vendor specific messages
    Preparative commit.
    Add payload to vendor specific messages, which will be used as the
    buffer for the inner TLV list - beerocks_header.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- framework/tlvf/AutoGenerated/include/tlvf/ieee_1905_1/tlvVendorSpecific.h
+++ framework/tlvf/AutoGenerated/include/tlvf/ieee_1905_1/tlvVendorSpecific.h
@@ -22,6 +22,7 @@
 #include <tlvf/ClassList.h>
 #include "tlvf/ieee_1905_1/eTlvType.h"
 #include "tlvf/ieee_1905_1/sVendorOUI.h"
+#include <tuple>
 
 namespace ieee1905_1 {
 
@@ -39,8 +40,11 @@ class tlvVendorSpecific : public BaseClass
         };
         
         const eTlvType& type();
-        uint16_t& length();
+        const uint16_t& length();
         sVendorOUI& vendor_oui();
+        size_t payload_length() { return m_payload_idx__ * sizeof(uint8_t); }
+        uint8_t* payload(size_t idx = 0);
+        bool alloc_payload(size_t count = 1);
         void class_swap() override;
         bool finalize() override;
         static size_t get_initial_size();
@@ -50,6 +54,9 @@ class tlvVendorSpecific : public BaseClass
         eTlvType* m_type = nullptr;
         uint16_t* m_length = nullptr;
         sVendorOUI* m_vendor_oui = nullptr;
+        uint8_t* m_payload = nullptr;
+        size_t m_payload_idx__ = 0;
+        int m_lock_order_counter__ = 0;
 };
 
 }; // close namespace: ieee1905_1
