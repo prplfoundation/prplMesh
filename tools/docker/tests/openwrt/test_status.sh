72b608576f6c60538602205f2712996e9e9fb520
Raphaël Mélotte <raphael.melotte@mind.be>
2019-12-18 10:45:39 +0100

    tools: docker: openwrt: add a test script for the status
    We need to know if the deployed build is running correctly on the
    device.
    
    Add a basic test that restarts prplmesh, then checks the status every
    second until it's reported as operational. If it's not operational
    after a pre-defined amount of time, exit with an error code.
    
    Restarting prplmesh is currently needed because it never runs
    successfully after the installation (not without a restart).
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

---
+++ tools/docker/tests/openwrt/test_status.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+usage() {
+    echo "usage: $(basename $0) <target>"
+}
+
+if [ -z "$1" ] ; then
+    usage
+    exit 1
+fi
+
+TARGET="$1"
+
+# the first start currently never succeeds, we need to restart it first.
+ssh "$TARGET" <<"EOF"
+/opt/prplmesh/prplmesh_utils.sh restart
+TIMEOUT=10
+for _ in $(seq 1 "$TIMEOUT") ; do
+    if /opt/prplmesh/prplmesh_utils.sh status ; then
+        exit 0
+    fi
+    sleep 1
+done
+exit 1
+EOF
+
