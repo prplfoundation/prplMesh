e49d3437a9f8645c51325f46d58142cda704469b
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-12-18 05:27:57 -0800

    tlvf: Refactor CmduMessage to use ClassList (breaks build)
    Warning - this commit breaks the build. It is kept only for easier code
    review of the feature as a whole.
    
    ---
    
    Now that we have ClassList which implements a generic container of
    tlvf-generated objects (classes), use it in CmduMessage:
    
    - Now that we have a container for all types of tlvf classes, and since
    cCMDU_HEADER is also a tlvf-generated class, simplify the CmduMessage
    class implementation by having it always set as the first member in the
    ClassList vector.
    
    - Set and buff_len as constructor arguments since they should
    never change.
    
    - Implement forward wrappers to the ClassList for backward compatability
    with the rest of the prplMesh components.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- framework/tlvf/src/include/tlvf/CmduMessage.h
+++ framework/tlvf/src/include/tlvf/CmduMessage.h
@@ -10,6 +10,7 @@
 #define _CmduMessage_H_
 
 #include <memory>
+#include <tlvf/ClassList.h>
 #include <tlvf/ieee_1905_1/cCmduHeader.h>
 #include <tlvf/ieee_1905_1/eTlvType.h>
 #include <tlvf/tlvflogging.h>
@@ -20,133 +21,40 @@ namespace ieee1905_1 {
 class CmduMessage {
 
 public:
-    CmduMessage();
-    ~CmduMessage();
+    CmduMessage() = delete;
+    CmduMessage(uint8_t *buff, size_t buff_len) : msg(buff, buff_len){};
+    ~CmduMessage(){};
 
-public:
-    std::shared_ptr<cCmduHeader> getCmduHeader() const;
+    ClassList msg;
 
-    template <class T, class = typename std::enable_if<std::is_base_of<BaseClass, T>::value>::type>
-    std::shared_ptr<T> addClass()
-    {
-
-        auto last_tlv = m_class_vector.empty() ? nullptr : m_class_vector.back();
-        // do not allow to use addClass method if the last tlv is not fully initialized
-        if (last_tlv) {
-            if (!last_tlv->isPostInitSucceeded()) {
-                TLVF_LOG(ERROR) << "TLV post init failed";
-                return nullptr;
-            }
-        }
-
-        std::shared_ptr<T> ptr;
-        if (m_cmdu_header) {
-            if (m_class_vector.size() == 0) {
-                ptr = std::make_shared<T>(m_cmdu_header, m_parse, m_swap);
-            } else {
-                ptr = std::make_shared<T>(m_class_vector.back(), m_parse, m_swap);
-            }
-            if (!ptr || ptr->isInitialized() == false) {
-                return std::shared_ptr<T>();
-            } else {
-                m_class_vector.push_back(std::shared_ptr<BaseClass>(ptr));
-            }
-        } else {
-            return std::shared_ptr<T>();
-        }
-        return ptr;
-    }
-
+    std::shared_ptr<cCmduHeader> getCmduHeader() const { return msg.getClass<cCmduHeader>(); };
     static uint16_t getCmduHeaderLength() { return kCmduHeaderLength; }
-
-    /**
-     * @brief Get the Class object at index idx in the all classes array
-     * 
-     * @param idx index in the all classes array
-     * @return std::shared_ptr<BaseClass> to the class object at index idx, nullptr if not found
-     */
-    std::shared_ptr<BaseClass> getClass(size_t idx) const;
-
-    /**
-     * @brief Get the (first) Class object
-     * 
-     * @tparam T class template
-     * @return std::shared_ptr<T> to the first object found of type T, nullptr if not found
-     */
-    template <class T> std::shared_ptr<T> getClass() const
-    {
-        for (size_t idx = 0; idx < getClassCount(); idx++) {
-            if (auto c = std::dynamic_pointer_cast<T>(getClass(idx)))
-                return c;
-        }
-        return nullptr;
-    }
-
-    /**
-     * @brief Get a class object of type T in index `idx` in the logical array containing
-     *        all classes of type T.
-     * 
-     * @tparam T class template
-     * @param idx index in the class T array
-     * @return std::shared_ptr<T> to the T class at index `idx` in the class T array
-     */
-    template <class T> std::shared_ptr<T> getClass(size_t idx) const
-    {
-        size_t idx_ = 0;
-        for (size_t i = 0; i < getClassCount(); i++) {
-            if (auto c = std::dynamic_pointer_cast<T>(getClass(i))) {
-                if (idx_++ == idx)
-                    return c;
-            }
-        }
-        return nullptr;
-    }
-
-    /**
-     * @brief Get the number of classes of type T
-     * 
-     * @tparam T class template
-     * @return size_t number of classes of type T
-     */
-    template <class T> size_t getClassCount() const
-    {
-        size_t count = 0;
-        for (size_t i = 0; i < getClassCount(); i++) {
-            if (auto c = std::dynamic_pointer_cast<T>(getClass(i))) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    size_t getClassCount() const;
-    const std::vector<std::shared_ptr<BaseClass>> &getClassVector() const;
-    size_t getMessageLength() const;
-    size_t getMessageBuffLength() const;
-    uint8_t *getMessageBuff() const;
+    eMessageType getMessageType();
+    uint16_t getMessageId();
     bool getNextTlvType(eTlvType &tlvType) const;
     int getNextTlvType() const;
     uint16_t getNextTlvLength() const;
     uint8_t *getNextTlvData() const;
-    void swap();
-    bool swap_needed() { return m_swap; }
-    bool is_finalized() const { return m_finalized; };
-    bool is_swapped() const { return m_swapped; };
-    eMessageType getMessageType();
-    uint16_t getMessageId();
 
-protected:
-    void reset();
+    // Forward wrapper functions
+    // TODO check which of them can be removed
+    template <class T> std::shared_ptr<T> addClass() { return msg.addClass<T>(); }
+    std::shared_ptr<BaseClass> getClass(size_t idx) const { return msg.getClass(idx); };
+    template <class T> std::shared_ptr<T> getClass() const { return msg.template getClass<T>(); };
+    template <class T> std::shared_ptr<T> getClass(size_t idx) const
+    {
+        return msg.template getClass<T>(idx);
+    };
+    template <class T> size_t getClassCount() const { return msg.template getClassCount<T>(); };
+    size_t getMessageLength() const { return msg.getMessageLength(); };
+    size_t getMessageBuffLength() const { return msg.getMessageBuffLength(); };
+    uint8_t *getMessageBuff() const { return msg.getMessageBuff(); };
+    void swap() { msg.swap(); };
+    bool swap_needed() { return msg.swap_needed(); }
+    bool is_finalized() const { return msg.is_finalized(); };
+    bool is_swapped() const { return msg.is_swapped(); };
 
-    size_t m_buff_len            = 0;
-    bool m_finalized             = false;
-    bool m_swapped               = false;
-    bool m_parse                 = false;
-    bool m_swap                  = false;
-    bool m_dynamically_allocated = false;
-    uint8_t *m_buff              = nullptr;
-    std::shared_ptr<cCmduHeader> m_cmdu_header;
-    std::vector<std::shared_ptr<BaseClass>> m_class_vector;
+    static const size_t kMaxCmduLength      = 1500;
     static const uint16_t kCmduHeaderLength = 8;
     static const uint16_t kTlvHeaderLength  = 3;
 };
