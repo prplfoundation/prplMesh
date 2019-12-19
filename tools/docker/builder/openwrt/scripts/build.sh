3b72fc5231184ff76f0d9aab3bda52348bcddb99
Raphaël Mélotte <raphael.melotte@mind.be>
2019-12-19 09:00:32 +0100

    tools: docker: openwrt: move the source directory to home
    The source used to compile prplMesh was previously mounted as a volume
    under '/'. This was possible when running the container manually. When
    using in CI, we cannot always control where the source will be
    cloned (it depends on the runner configuration to allow that or not).
    
    Moreover, the openwrt user inside the container cannot add a symlink
    or copy files to '/', because it doesn't have the permission to write
    there.
    
    scripts/build.sh: look for the source in prplMesh_source under the
    user home instead of '/'.
    
    tools/docker/builder/openwrt/build.sh: mount the source volume in
    prplMesh_source in the user home.
    
    While we're at it, remove the TARGET_PLATFORM_TYPE patching from both
    files, because it's not needed anymore.
    
    With these changes, the CI does not have to rely on where the source
    will be cloned, and can just add a symlink in
    /home/openwrt/prplMesh_source pointing to the source (for example).
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/docker/builder/openwrt/scripts/build.sh
+++ tools/docker/builder/openwrt/scripts/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh -e
 
-sed -ri "s/-DTARGET_PLATFORM_TYPE=[^ ]*/-DTARGET_PLATFORM_TYPE=${TARGET_PLATFORM_TYPE}/" feeds/prpl/prplmesh/Makefile
-
 # We have to copy the source directory, because we may not have
 # write access to it, and openwrt needs to at least write '.source_dir':
-cp -r /prplMesh /home/openwrt/prplMesh
+cp -r /home/openwrt/prplMesh_source /home/openwrt/prplMesh
 # We want to make sure that we do not keep anything built from the host:
 rm -rf /home/openwrt/prplMesh/build
 
 make package/feeds/prpl/prplmesh/prepare USE_SOURCE_DIR="/home/openwrt/prplMesh" V=s
 make package/feeds/prpl/prplmesh/compile V=sc -j"$(nproc)"
-find bin -name 'prplmesh_*.ipk' -exec cp -v {} "prplmesh-${TARGET}-${TARGET_PLATFORM_TYPE}-${OPENWRT_VERSION}-${PRPLMESH_VERSION}.ipk" \;
+find bin -name 'prplmesh_*.ipk' -exec cp -v {} "prplmesh-${TARGET}-${OPENWRT_VERSION}-${PRPLMESH_VERSION}.ipk" \;
