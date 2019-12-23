c3438cd1a584230513b24ba80088c4f25b95f99e
Denis Bakin <denis.bakin@minim.co>
2019-12-23 17:44:57 -0500

    tools: docker: usability tweaks
    - use sed instead of the '--exclude' option of git-describe, because
      it's currently not supported by git in some distros (Ubuntu 16.04)
    - added command line option to specify docker build options. This can
      be used to pass the "--no-cache" option to docker for example (it
      might be necessary to force a rebuild since OpenWrt does not lock in
      versions of the feeds and each build therefore might be different
      even if openwrt commit SHA is still the same)
    - move the check of TARGET_DEVICE after the loop, so it also detects a
      missing -d option
    - fixed a few typos
    
    Signed-off-by: Denis Bakin <denis.bakin@minim.co>
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -9,6 +9,7 @@ usage() {
     echo "usage: $(basename $0) -d <target_device> [-hfiortv]"
     echo "  options:"
     echo "      -h|--help - show this help menu"
+    echo "      -b|--build-options - add docker build options"
     echo "      -d|--target-device the device to build for"
     echo "      -f|--prpl-feed the prpl feed to use"
     echo "      -i|--image - build the docker image only"
@@ -26,6 +27,7 @@ build_image() {
            --build-arg OPENWRT_VERSION \
            --build-arg TARGET \
            --build-arg PRPL_FEED \
+           $BUILD_OPTIONS \
            "$scriptdir/"
 }
 
@@ -48,7 +50,7 @@ build_prplmesh() {
 }
 
 main() {
-    OPTS=`getopt -o 'hd:f:io:r:t:v' --long help,device:,prpl-feed:,image,openwrt-version:,openwrt-repository:,tag:,verbose -n 'parse-options' -- "$@"`
+    OPTS=`getopt -o 'hb:d:f:io:r:t:v' --long help,build-options:,device:,prpl-feed:,image,openwrt-version:,openwrt-repository:,tag:,verbose -n 'parse-options' -- "$@"`
 
     if [ $? != 0 ] ; then err "Failed parsing options." >&2 ; usage; exit 1 ; fi
 
@@ -59,25 +61,8 @@ main() {
     while true; do
         case "$1" in
             -h | --help)               usage; exit 0; shift ;;
-            -d | --target-device)
-                TARGET_DEVICE="$2"; shift ; shift
-                case "$TARGET_DEVICE" in
-                    turris-omnia)
-                        TARGET=mvebu
-                        ;;
-                    glinet-b1300)
-                        TARGET=ipq40xx
-                        ;;
-                    *)
-                        err "Unknown target device: $TARGET_DEVICE"
-                        info "Currently supported targets are:"
-			for i in $SUPPORTED_TARGETS ; do
-			    info "$i"
-			done
-                        exit 1
-                        ;;
-                esac
-                ;;
+            -b | --build-options)      BUILD_OPTIONS="$2"; shift; shift ;;
+            -d | --target-device)      TARGET_DEVICE="$2"; shift ; shift ;;
             -f | --prpl-feed)          PRPL_FEED="$2"; shift; shift ;;
             -i | --image)              IMAGE_ONLY=true; shift ;;
             -o | --openwrt-version)    OPENWRT_VERSION="$2"; shift; shift ;;
@@ -89,12 +74,30 @@ main() {
         esac
     done
 
+    dbg "BUILD_OPTIONS=$BUILD_OPTIONS"
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
     dbg "OPENWRT_VERSION=$OPENWRT_VERSION"
     dbg "PRPL_FEED=$PRPL_FEED"
-    dbg "IMAGE_ONlY=IMAGE_ONLY"
+    dbg "IMAGE_ONLY=$IMAGE_ONLY"
     dbg "TARGET_DEVICE=$TARGET_DEVICE"
     dbg "TAG=$TAG"
+
+    case "$TARGET_DEVICE" in
+        turris-omnia)
+            TARGET=mvebu
+            ;;
+        glinet-b1300)
+            TARGET=ipq40xx
+            ;;
+        *)
+            err "Unknown target device: $TARGET_DEVICE"
+            info "Currently supported targets are:"
+            for i in $SUPPORTED_TARGETS ; do
+                info "$i"
+            done
+            exit 1
+            ;;
+    esac
 
     if [ -n "$TAG" ] ; then
         image_tag="$TAG"
@@ -106,7 +109,11 @@ main() {
     export OPENWRT_REPOSITORY
     export OPENWRT_VERSION
     export TARGET
-    PRPLMESH_VERSION="$(git describe --always --dirty --exclude '*')"
+    # We want to exclude tags from the git-describe output because we
+    # have no relevant tags to use at the moment.
+    # The '--exclude' option of git-describe is not available on older
+    # git version, so we use sed instead.
+    PRPLMESH_VERSION="$(git describe --always --dirty | sed -e 's/.*-g//')"
     export PRPLMESH_VERSION
     export PRPL_FEED
 
@@ -122,6 +129,7 @@ main() {
 
 IMAGE_ONLY=false
 VERBOSE=false
+BUILD_OPTIONS=""
 OPENWRT_REPOSITORY='https://git.prpl.dev/prplmesh/prplwrt.git'
 OPENWRT_VERSION='9d2efd'
 PRPL_FEED='https://git.prpl.dev/prplmesh/iwlwav.git^06a0126d5fb53b1d65bad90757a5f9f5f77419ca'
