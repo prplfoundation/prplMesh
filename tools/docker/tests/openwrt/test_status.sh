c53c72eb1612d8727d8264cf694a1a26a8126c35
Denis Bakin <denis.bakin@minim.co>
2019-12-24 15:31:24 -0500

    tools: ci: collect diagnostic data
    Collecting diagnostic data is important to understand why a particular
    test failed on some device.
    
    Collect device information before running the status test:
    - OS release date info and version
    - uci configuration for prplmesh, network and wireless
    - ifconfig output
    
    Collect info after running the status test:
    - output of bml_conn_map and bml_nw_map_query
    - kernel messages
    
    Also store all all the information in a tarball, and save it as an artifact.
    
    Signed-off-by: Denis Bakin <denis.bakin@minim.co>

--- tools/docker/tests/openwrt/test_status.sh
+++ tools/docker/tests/openwrt/test_status.sh
@@ -11,7 +11,28 @@ fi
 
 TARGET="$1"
 
-# the first start currently never succeeds, we need to restart it first.
+# Collect diagnostic data before the restart
+echo "Collecting pre-test-run diagnostic data..."
+ssh "$TARGET" <<"EOF" > "${TARGET}_diags.log"
+date
+echo "Release:"
+cat /etc/*_release
+echo "Version:"
+cat /etc/*_version
+echo
+echo "UCI Configuration (prplMesh, network and wireless):"
+uci export prplmesh
+uci export network
+uci export wireless
+echo
+echo "ifconfig output:"
+ifconfig
+echo "'iw list' output:"
+iw list
+EOF
+
+# The first start currently never succeeds, we need to restart it first.
+echo "Attempting to start/restart prplMesh ..."
 ssh "$TARGET" <<"EOF"
 /opt/prplmesh/prplmesh_utils.sh restart -d
 TIMEOUT=30
@@ -23,10 +44,25 @@ for _ in $(seq 1 "$TIMEOUT") ; do
 done
 exit 1
 EOF
-
-# Save exit status and capture the logs
+# Save exit status
 TEST_STATUS=$?
+
+# Collect post-run diagnostics
+echo "Collecting post-test-run diagnostic data..."
+ssh "$TARGET" <<"EOF" >> "${TARGET}_diags.log"
+date
+echo "Output of bml_conn_map:"
+/opt/prplmesh/bin/beerocks_cli -c bml_conn_map 2>&1
+echo "Output of bml_nw_map_query:"
+/opt/prplmesh/bin/beerocks_cli -c bml_nw_map_query 2>&1
+echo "Kernel messages:"
+dmesg
+EOF
+
+# Capture the logs
+echo "Capturing the prplMesh logs..."
 ssh "$TARGET" "tar -C /tmp/beerocks/logs -czvf - ./" > "${TARGET}_logs.tar.gz"
+
 echo "Logs file:"
 ls -l "$PWD/${TARGET}_logs.tar.gz"
 
@@ -34,4 +70,3 @@ echo "Stopping prplMesh"
 ssh "$TARGET" /opt/prplmesh/prplmesh_utils.sh stop
 
 exit $TEST_STATUS
-
