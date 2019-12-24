65e81c33dd7c209d966710953f8463db6b88f569
Denis Bakin <denis.bakin@minim.co>
2019-12-24 03:17:37 -0500

    tools: ci: add test run logs
    To make it easier to debug failing tests, get the logs folder from the
    target, compress it and save it as an artifact.
    
    Also when restarting prplMesh, delete the old logs first.
    
    Signed-off-by: Denis Bakin <denis.bakin@minim.co>

--- tools/docker/tests/openwrt/test_status.sh
+++ tools/docker/tests/openwrt/test_status.sh
@@ -13,7 +13,7 @@ TARGET="$1"
 
 # the first start currently never succeeds, we need to restart it first.
 ssh "$TARGET" <<"EOF"
-/opt/prplmesh/prplmesh_utils.sh restart
+/opt/prplmesh/prplmesh_utils.sh restart -d
 TIMEOUT=30
 for _ in $(seq 1 "$TIMEOUT") ; do
     if /opt/prplmesh/prplmesh_utils.sh status ; then
@@ -24,5 +24,14 @@ done
 exit 1
 EOF
 
+# Save exit status and capture the logs
+TEST_STATUS=$?
+ssh "$TARGET" "tar -C /tmp/beerocks/logs -czvf - ./" > "${TARGET}_logs.tar.gz"
+echo "Logs file:"
+ls -l "$PWD/${TARGET}_logs.tar.gz"
+
 echo "Stopping prplMesh"
 ssh "$TARGET" /opt/prplmesh/prplmesh_utils.sh stop
+
+exit $TEST_STATUS
+
