b1be389dda00b280f833e953d7c8c20c03d0abad
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-12-25 03:03:21 -0800

    tlvf: update autogen files for  WSC_Attributes.yml
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- framework/tlvf/AutoGenerated/include/tlvf/WSC/WSC_Attributes.h
+++ framework/tlvf/AutoGenerated/include/tlvf/WSC/WSC_Attributes.h
@@ -17,13 +17,6 @@
 #include <stdint.h>
 #include <tlvf/swap.h>
 #include "tlvf/WSC/eWscAttributes.h"
-#include "tlvf/WSC/eWscValues8.h"
-#include "tlvf/WSC/eWscMessageType.h"
-#include "tlvf/WSC/eWscConn.h"
-#include "tlvf/WSC/eWscState.h"
-#include "tlvf/WSC/eWscRfBands.h"
-#include "tlvf/WSC/eWscAssoc.h"
-#include "tlvf/WSC/eWscValues16.h"
 #include "tlvf/WSC/eWscAuth.h"
 #include "tlvf/WSC/eWscEncr.h"
 #include <string.h>
@@ -32,6 +25,13 @@
 #include <tlvf/ClassList.h>
 #include <tuple>
 #include <tlvf/tlvfutils.h>
+#include "tlvf/WSC/eWscValues8.h"
+#include "tlvf/WSC/eWscMessageType.h"
+#include "tlvf/WSC/eWscConn.h"
+#include "tlvf/WSC/eWscRfBands.h"
+#include "tlvf/WSC/eWscAssoc.h"
+#include "tlvf/WSC/eWscValues16.h"
+#include "tlvf/WSC/eWscState.h"
 #include "tlvf/WSC/eWscLengths.h"
 #include "tlvf/WSC/eWscVendorId.h"
 #include "tlvf/WSC/eWscVendorExt.h"
@@ -68,297 +68,6 @@ class cWscAttrUuidR;
 class cWscAttrAuthenticator;
 class cWscAttrRegistrarNonce;
 class cWscAttrVersion2;
-typedef struct sWscAttrVersion {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscValues8 data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_VERSION;
-        data_length = 0x1;
-        data = WSC_VERSION;
-    }
-} __attribute__((packed)) sWscAttrVersion;
-
-typedef struct sWscAttrMessageType {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscMessageType data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_MSG_TYPE;
-        data_length = 0x1;
-        data = WSC_MSG_TYPE_M1;
-    }
-} __attribute__((packed)) sWscAttrMessageType;
-
-typedef struct sWscAttrUuidE {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint8_t data[WSC_UUID_LENGTH];
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_UUID_E;
-        data_length = WSC_UUID_LENGTH;
-    }
-} __attribute__((packed)) sWscAttrUuidE;
-
-typedef struct sWscAttrUuidR {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint8_t data[WSC_UUID_LENGTH];
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_UUID_R;
-        data_length = WSC_UUID_LENGTH;
-    }
-} __attribute__((packed)) sWscAttrUuidR;
-
-typedef struct sWscAttrMac {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    sMacAddr data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        data.struct_swap();
-    }
-    void struct_init(){
-        attribute_type = ATTR_MAC_ADDR;
-        data_length = WSC_MAC_LENGTH;
-        data.struct_init();
-    }
-} __attribute__((packed)) sWscAttrMac;
-
-typedef struct sWscAttrEnrolleeNonce {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint8_t data[WSC_NONCE_LENGTH];
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_ENROLLEE_NONCE;
-        data_length = WSC_NONCE_LENGTH;
-    }
-} __attribute__((packed)) sWscAttrEnrolleeNonce;
-
-typedef struct sWscAttrRegistrarNonce {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint8_t data[WSC_NONCE_LENGTH];
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_REGISTRAR_NONCE;
-        data_length = WSC_NONCE_LENGTH;
-    }
-} __attribute__((packed)) sWscAttrRegistrarNonce;
-
-typedef struct sWscAttrPublicKey {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint8_t data[WSC_PUBLIC_KEY_LENGTH];
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_PUBLIC_KEY;
-        data_length = WSC_PUBLIC_KEY_LENGTH;
-    }
-} __attribute__((packed)) sWscAttrPublicKey;
-
-typedef struct sWscAttrAuthenticationTypeFlags {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint16_t data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_AUTH_TYPE_FLAGS;
-        data_length = 0x2;
-    }
-} __attribute__((packed)) sWscAttrAuthenticationTypeFlags;
-
-typedef struct sWscAttrEncryptionTypeFlags {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint16_t data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_ENCR_TYPE_FLAGS;
-        data_length = 0x2;
-    }
-} __attribute__((packed)) sWscAttrEncryptionTypeFlags;
-
-typedef struct sWscAttrConnectionTypeFlags {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscConn data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_CONN_TYPE_FLAGS;
-        data_length = 0x1;
-        data = WSC_CONN_ESS;
-    }
-} __attribute__((packed)) sWscAttrConnectionTypeFlags;
-
-typedef struct sWscAttrConfigurationMethods {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint16_t data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_CONFIG_METHODS;
-        data_length = 0x2;
-        data = (WSC_CONFIG_PHY_PUSHBUTTON | WSC_CONFIG_VIRT_PUSHBUTTON);
-    }
-} __attribute__((packed)) sWscAttrConfigurationMethods;
-
-typedef struct sWscAttrWscState {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscState data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_WSC_STATE;
-        data_length = 0x1;
-        data = WSC_STATE_NOT_CONFIGURED;
-    }
-} __attribute__((packed)) sWscAttrWscState;
-
-typedef struct sWscAttrPrimaryDeviceType {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint16_t category_id;
-    uint32_t oui;
-    uint16_t sub_category_id;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&category_id));
-        tlvf_swap(32, reinterpret_cast<uint8_t*>(&oui));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&sub_category_id));
-    }
-    void struct_init(){
-        attribute_type = ATTR_PRIMARY_DEV_TYPE;
-        data_length = WSC_PRIMARY_DEV_TYPE_LENGTH;
-        category_id = WSC_DEV_NETWORK_INFRA;
-        oui = 0x50f204;
-    }
-} __attribute__((packed)) sWscAttrPrimaryDeviceType;
-
-typedef struct sWscAttrRfBands {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscRfBands data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_RF_BANDS;
-        data_length = 0x1;
-    }
-} __attribute__((packed)) sWscAttrRfBands;
-
-typedef struct sWscAttrAssociationState {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscAssoc data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_ASSOC_STATE;
-        data_length = 0x2;
-        data = WSC_ASSOC_NOT_ASSOC;
-    }
-} __attribute__((packed)) sWscAttrAssociationState;
-
-typedef struct sWscAttrDevicePasswordID {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscValues16 data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_DEV_PASSWORD_ID;
-        data_length = 0x2;
-        data = DEV_PW_PUSHBUTTON;
-    }
-} __attribute__((packed)) sWscAttrDevicePasswordID;
-
-typedef struct sWscAttrConfigurationError {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    eWscValues16 data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_CONFIG_ERROR;
-        data_length = 0x2;
-        data = WSC_CFG_NO_ERROR;
-    }
-} __attribute__((packed)) sWscAttrConfigurationError;
-
-typedef struct sWscAttrOsVersion {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    uint32_t data;
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-        tlvf_swap(32, reinterpret_cast<uint8_t*>(&data));
-    }
-    void struct_init(){
-        attribute_type = ATTR_OS_VERSION;
-        data_length = WSC_OS_VERSION_LENGTH;
-        data = 0x80000001;
-    }
-} __attribute__((packed)) sWscAttrOsVersion;
-
 typedef struct sWscAttrVersion2 {
     eWscAttributes attribute_type;
     uint16_t data_length;
@@ -383,19 +92,6 @@ typedef struct sWscAttrVersion2 {
         subelement_value = WSC_VERSION2;
     }
 } __attribute__((packed)) sWscAttrVersion2;
-
-typedef struct sWscIeVersion2 {
-    uint8_t subelement_id;
-    uint8_t subelement_length;
-    uint8_t subelement_value;
-    void struct_swap(){
-    }
-    void struct_init(){
-        subelement_id = 0x0;
-        subelement_length = 0x1;
-        subelement_value = WSC_VERSION2;
-    }
-} __attribute__((packed)) sWscIeVersion2;
 
 typedef struct sWscAttrVendorExtMultiAp {
     eWscAttributes attribute_type;
@@ -482,20 +178,6 @@ typedef struct sWscAttrBssid {
         data_length = WSC_MAC_LENGTH;
     }
 } __attribute__((packed)) sWscAttrBssid;
-
-typedef struct sWscAttrAuthenticator {
-    eWscAttributes attribute_type;
-    uint16_t data_length;
-    char data[WSC_AUTHENTICATOR_LENGTH];
-    void struct_swap(){
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&attribute_type));
-        tlvf_swap(16, reinterpret_cast<uint8_t*>(&data_length));
-    }
-    void struct_init(){
-        attribute_type = ATTR_AUTHENTICATOR;
-        data_length = WSC_AUTHENTICATOR_LENGTH;
-    }
-} __attribute__((packed)) sWscAttrAuthenticator;
 
 
 class cConfigData : public BaseClass
