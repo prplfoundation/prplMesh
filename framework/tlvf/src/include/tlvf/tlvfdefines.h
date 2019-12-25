5936c7644092a7d2b5d4b29a8a3c3236c58d951c
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-12-25 06:38:02 -0800

    tlvf: add tlvfdefines.h for FALLTHROUGH define
    Add FALLTHROUGH support to tlvf.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ framework/tlvf/src/include/tlvf/tlvfdefines.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2019 Tomer Eliyahu (Intel Corporation)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_DEFINES_H_
+#define _TLVF_DEFINES_H_
+
+#if __GNUC__ >= 7 || __cplussplus >= 201703L
+#define FALLTHROUGH __attribute__((fallthrough))
+#else
+// clang-format off
+#define FALLTHROUGH do { } while (0)
+// clang-format on
+#endif
+
+#endif
