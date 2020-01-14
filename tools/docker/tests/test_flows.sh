607f758dc9534ff67bc0aa526dc9d81978d33de2
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2020-01-14 15:55:25 +0100

    test_flows: add optimal_path_dummy to ALL_TESTS
    The optimal_path_dummy test was added in commit f41f2fc76c but it wasn't
    added to ALL_TESTS, so it never got executed.
    
    Add it now.
    
    Note that this must be done at the beginning. A shortcoming of our
    current test_flows approach is that no AP Auto(re)configuration is done
    at the beginning of each test, so it inherits the configuration from
    previous tests. Since the initial_ap_config, ap_config_renew and
    ap_config_bss_tear_down tests tear down some radios, they make the
    optimal_path_dummy test fail, since it uses all radios. A better
    solution would of course be to properly configure all APs, but just
    doing the optimal_path_dummy test first is simpler for the time being.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/tests/test_flows.sh
+++ tools/docker/tests/test_flows.sh
@@ -6,7 +6,14 @@
 # See LICENSE file for more details.
 ###############################################################
 
-ALL_TESTS="topology initial_ap_config ap_config_renew ap_config_bss_tear_down channel_selection
+# NOTE optimal_path_dummy must be done at the beginning. A shortcoming of our current test_flows
+# approach is that no AP Auto(re)configuration is done at the beginning of each test, so it inherits
+# the configuration from previous tests. Since the initial_ap_config, ap_config_renew and
+# ap_config_bss_tear_down tests tear down some radios, they make the optimal_path_dummy test fail,
+# since it uses all radios. A better solution would of course be to properly configure all APs,
+# but just doing the optimal_path_dummy test first is simpler for the time being.
+
+ALL_TESTS="optimal_path_dummy topology initial_ap_config ap_config_renew ap_config_bss_tear_down channel_selection
            ap_capability_query client_capability_query combined_infra_metrics
            client_steering_mandate client_steering_dummy client_association_dummy client_steering_policy client_association
            higher_layer_data_payload_trigger higher_layer_data_payload"
