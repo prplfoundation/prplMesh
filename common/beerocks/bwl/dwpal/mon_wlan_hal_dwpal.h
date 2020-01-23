a95f6fe7c8139935112882e18072c114185778ad
Adam Dov <adamx.dov@intel.com>
2020-01-23 21:27:43 +0200

    mon_wlan_dwpal: added received result size to error log print
    Print dwpal_nl_cmd_get returned value, containing the received
    result size if the latter does not match the expected result size.
    
    Signed-off-by: Adam Dov <adamx.dov@intel.com>

--- common/beerocks/bwl/dwpal/mon_wlan_hal_dwpal.h
+++ common/beerocks/bwl/dwpal/mon_wlan_hal_dwpal.h
@@ -59,12 +59,13 @@ private:
     bool dwpal_get_scan_params_fg(sScanCfgParams &params)
     {
         const size_t expected_result_size = sizeof(params) + NL_ATTR_HDR;
+        size_t received_result_size =
+            dwpal_nl_cmd_get(m_radio_info.iface_name, LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS,
+                             m_nl_buffer, NL_MAX_REPLY_BUFFSIZE);
 
-        if (expected_result_size != dwpal_nl_cmd_get(m_radio_info.iface_name,
-                                                     LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS,
-                                                     m_nl_buffer, NL_MAX_REPLY_BUFFSIZE)) {
-            LOG(ERROR) << "LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS failed! returned size does "
-                          "not match sScanCfgParams!";
+        if (expected_result_size != received_result_size) {
+            LOG(ERROR) << "LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS failed! expected size = "
+                       << expected_result_size << ", received size = " << received_result_size;
             return false;
         }
         std::copy_n(&m_nl_buffer[NL_ATTR_HDR], sizeof(params),
@@ -75,12 +76,13 @@ private:
     bool dwpal_get_scan_params_bg(sScanCfgParamsBG &params)
     {
         const size_t expected_result_size = sizeof(params) + NL_ATTR_HDR;
+        size_t received_result_size =
+            dwpal_nl_cmd_get(m_radio_info.iface_name, LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS_BG,
+                             m_nl_buffer, NL_MAX_REPLY_BUFFSIZE);
 
-        if (expected_result_size != dwpal_nl_cmd_get(m_radio_info.iface_name,
-                                                     LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS_BG,
-                                                     m_nl_buffer, NL_MAX_REPLY_BUFFSIZE)) {
-            LOG(ERROR) << "LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS_BG failed! returned size does "
-                          "not match sScanCfgParamsBG!";
+        if (expected_result_size != received_result_size) {
+            LOG(ERROR) << "LTQ_NL80211_VENDOR_SUBCMD_GET_SCAN_PARAMS_BG failed! expected size = "
+                       << expected_result_size << ", received size = " << received_result_size;
             return false;
         }
         std::copy_n(&m_nl_buffer[NL_ATTR_HDR], sizeof(params),
