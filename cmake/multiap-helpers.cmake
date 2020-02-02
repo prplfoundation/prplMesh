c13c0ccf88703fa85d61fee2bd05b74f54cfdf33
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-02-02 14:16:34 +0000

    cmake: Update BUILD_DATE format to %F_%H-%M-%S
    Preparative commit.
    Update BUILD_DATE to an easy to grep format.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- cmake/multiap-helpers.cmake
+++ cmake/multiap-helpers.cmake
@@ -39,7 +39,7 @@ if (NOT REVISION)
 endif()
 
 if (NOT BUILD_DATE)
-    execute_process(COMMAND "date" "+%F %T" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE BUILD_DATE)
+    execute_process(COMMAND "date" "+%F_%H-%M-%S" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE BUILD_DATE)
 endif()
 
 message(STATUS "prplmesh Version: ${prplmesh_VERSION}")
