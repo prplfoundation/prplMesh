71b59c8f97e3a5de84046eedca3620827442ba63
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-02-07 19:32:26 +0000

    tools: rdkb toolchain builder fixes
    By default, the user and group id given by useradd is 1000, which is why
    this docker image will work only for the first user in the server.
    
    Fix this by adding the user and group id in the entrypoint script by
    adding a new user and running the bitbake command with su.
    
    In addition, rename build.sh to bitbake.sh, beautify and optimize the
    Dockerfile, and run shellcheck on the entripoint and bitbake scripts.
    
    Now we can alias bitbake to call build.sh bitbake "$@" and run it as
    follows:
    bitbake rdk-generic-broadband-image
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- tools/docker/builder/rdkb/toolchain/entrypoint.sh
+++ tools/docker/builder/rdkb/toolchain/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo RDKB_ROOT=$RDKB_ROOT
-cd $RDKB_ROOT/setup
+
+echo RDKB_ROOT="$RDKB_ROOT"
+echo USER="$USER"
+echo USER_ID="$USER_ID"
+echo GROUP_ID="$GROUP_ID"
+
+groupadd -g "${GROUP_ID}" "${USER}"
+useradd -l -u "${USER_ID}" -g "${USER}" "${USER}"
+su "${USER}" <<EOSU
+if ! cd "${RDKB_ROOT}"/setup ; then echo "Failed to cd to ${RDKB_ROOT}/setup"; exit 1; fi
 
 if [ -d "${RDKB_ROOT}/atom_rdkbos/build" ]; then 
     . puma_setup -o 
@@ -8,4 +16,6 @@ else
     . puma_setup -c configs/p7_atom_rdkbos_wave600_dev-debug.config
 fi
 
-exec "$@"
+echo bitbake $@
+bitbake $@
+EOSU
