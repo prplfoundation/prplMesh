4ff538765e8a91fdadf38c92a755699d0f9bf168
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-02-11 15:58:38 +0000

    tlvf: Add dump results request and response: AutoGenerate
    This is a follow-up commit for
    - tlfv: Add dump results request and response
    And contains all the auto-generated code for the tlfv
    
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_monitor.cpp
+++ common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_monitor.cpp
@@ -2871,6 +2871,132 @@ bool cACTION_MONITOR_CHANNEL_SCAN_TRIGGER_SCAN_RESPONSE::init()
     return true;
 }
 
+cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::~cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST() {
+}
+void cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_MONITOR), reinterpret_cast<uint8_t*>(m_action_op));
+}
+
+bool cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::get_initial_size()
+{
+    size_t class_size = 0;
+    return class_size;
+}
+
+bool cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_REQUEST::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
+cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::~cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE() {
+}
+uint8_t& cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::success() {
+    return (uint8_t&)(*m_success);
+}
+
+void cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_MONITOR), reinterpret_cast<uint8_t*>(m_action_op));
+}
+
+bool cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(uint8_t); // success
+    return class_size;
+}
+
+bool cACTION_MONITOR_CHANNEL_SCAN_DUMP_RESULTS_RESPONSE::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_success = (uint8_t*)m_buff_ptr__;
+    if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
+        return false;
+    }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
 cACTION_MONITOR_CHANNEL_SCAN_TRIGGERED_NOTIFICATION::cACTION_MONITOR_CHANNEL_SCAN_TRIGGERED_NOTIFICATION(uint8_t* buff, size_t buff_len, bool parse) :
     BaseClass(buff, buff_len, parse) {
     m_init_succeeded = init();
