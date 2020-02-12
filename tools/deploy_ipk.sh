203f8ed0db7ae4155067cbdd84cccb89ecf71266
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-02-12 12:00:05 +0000

    deploy_ipk.sh add --force-depends to opkg remove
    Add --force-depends since if prplmesh is already part of the base image,
    there might be other packages depending on it, which will make opkg
    remove fail.
    In practice, prplmesh dependent packages are not actively using prplmesh
    so it is fine to ignore that.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- tools/deploy_ipk.sh
+++ tools/deploy_ipk.sh
@@ -35,8 +35,10 @@ deploy() {
 # we don't want opkg to stay locked with a previous failed invocation.
 # when using this, make sure no one is using opkg in the meantime!
 pgrep opkg | xargs kill -SIGINT
-# remove any previously installed prplmesh:
-opkg remove prplmesh prplmesh-dwpal prplmesh-nl80211
+# remove any previously installed prplmesh. Use force-depends in case
+# there are packages depending on prplmesh which will cause opkg remove
+# to fail without this:
+opkg remove --force-depends prplmesh prplmesh-dwpal prplmesh-nl80211
 # currently opkg remove does not remove everything from /opt/prplmesh:
 rm -rf /opt/prplmesh
 opkg install -V2 "$DEST_FOLDER/$IPK_FILENAME"
@@ -77,6 +79,6 @@ main() {
     deploy "$@"
 }
 
-SSH_OPTIONS="\"-oBatchMode yes\""
+SSH_OPTIONS="\"-oBatchMode yes\" \"-oStrictHostKeyChecking no\""
 
 main "$@"
