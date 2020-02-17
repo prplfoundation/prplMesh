13c26834a61520b9805e3a970c2a71fc7445b0ae
Coral Malachi <coral.malachi@intel.com>
2020-02-17 08:41:42 +0000

    agent: son_slave: remove client capability handling
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

--- agent/src/beerocks/slave/son_slave_thread.h
+++ agent/src/beerocks/slave/son_slave_thread.h
@@ -240,7 +240,6 @@ private:
     bool handle_channel_preference_query(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_channel_selection_request(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_multi_ap_policy_config_request(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
-    bool handle_client_capability_query(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_client_association_request(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_client_steering_request(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
     bool handle_ack_message(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_rx);
