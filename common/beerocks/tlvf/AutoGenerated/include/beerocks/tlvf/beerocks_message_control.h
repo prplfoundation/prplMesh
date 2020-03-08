f56f248cf81906f8a97218dc6997e1c35624bd42
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-03-08 15:43:11 +0000

    btlvf: update autogenerated files
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_control.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_control.h
@@ -27,6 +27,25 @@
 namespace beerocks_message {
 
 
+class cACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST : public BaseClass
+{
+    public:
+        cACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST();
+
+        static eActionOp_CONTROL get_action_op(){
+            return (eActionOp_CONTROL)(ACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST);
+        }
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eActionOp_CONTROL* m_action_op = nullptr;
+};
+
 class cACTION_CONTROL_SLAVE_JOINED_NOTIFICATION : public BaseClass
 {
     public:
