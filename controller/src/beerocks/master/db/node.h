889dc693b16ae65272a0cd28978cf943c666bffb
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-03-08 16:53:21 +0000

    controller: db: add is_prplmesh and set_prplmesh
    Preparative commit.
    Add is_prplmesh and set_prplmesh APIs to the controller database which
    will be used to mark prplmesh agents.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- controller/src/beerocks/master/db/node.h
+++ controller/src/beerocks/master/db/node.h
@@ -250,6 +250,7 @@ public:
         bool add_ap_metric_data(std::shared_ptr<wfa_map::tlvApMetric> ApMetricData);
     };
 
+    bool is_prplmesh                = false;
     beerocks::eBandType band_type   = beerocks::eBandType::INVALID_BAND;
     beerocks::eIfaceType iface_type = beerocks::IFACE_TYPE_ETHERNET;
     std::chrono::steady_clock::time_point last_seen;
