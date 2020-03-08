3b81751d263fa5ba404ba7f27208f52d3bbb91e8
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-03-08 17:07:49 +0000

    btlvf: update autogenerated files
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_control.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_control.h
@@ -46,6 +46,25 @@ class cACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST : public BaseClass
         eActionOp_CONTROL* m_action_op = nullptr;
 };
 
+class cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE : public BaseClass
+{
+    public:
+        cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE();
+
+        static eActionOp_CONTROL get_action_op(){
+            return (eActionOp_CONTROL)(ACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE);
+        }
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eActionOp_CONTROL* m_action_op = nullptr;
+};
+
 class cACTION_CONTROL_SLAVE_JOINED_NOTIFICATION : public BaseClass
 {
     public:
--- common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_control.cpp
+++ common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_control.cpp
@@ -73,6 +73,64 @@ bool cACTION_CONTROL_SLAVE_HANDSHAKE_REQUEST::init()
     return true;
 }
 
+cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::~cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE() {
+}
+void cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_CONTROL), reinterpret_cast<uint8_t*>(m_action_op));
+}
+
+bool cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::get_initial_size()
+{
+    size_t class_size = 0;
+    return class_size;
+}
+
+bool cACTION_CONTROL_SLAVE_HANDSHAKE_RESPONSE::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
 cACTION_CONTROL_SLAVE_JOINED_NOTIFICATION::cACTION_CONTROL_SLAVE_JOINED_NOTIFICATION(uint8_t* buff, size_t buff_len, bool parse) :
     BaseClass(buff, buff_len, parse) {
     m_init_succeeded = init();
