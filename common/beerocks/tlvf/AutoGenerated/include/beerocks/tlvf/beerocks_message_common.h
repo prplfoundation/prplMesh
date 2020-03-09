475102ef72e55016e8e56776b8409d3999a14e40
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-03-09 08:47:20 +0000

    agent: mark prplmesh controller
    Preparative commit.
    Mark prplmesh controller in backhaul manager and propagate it to the
    slaves (radio agents).
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_common.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_common.h
@@ -783,7 +783,7 @@ typedef struct sBackhaulParams {
     uint8_t backhaul_is_wireless;
     uint8_t backhaul_iface_type;
     uint8_t is_backhaul_manager;
-    uint8_t gw_manage_type;
+    uint8_t is_prplmesh_controller;
     beerocks::net::sScanResult backhaul_scan_measurement_list[beerocks::message::BACKHAUL_SCAN_MEASUREMENT_MAX_LENGTH];
     void struct_swap(){
         gw_ipv4.struct_swap();
