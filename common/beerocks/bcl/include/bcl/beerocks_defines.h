79b9356b2f5f62d07acb1fbc08c3db6c0d39e61f
Coral Malachi <coral.malachi@intel.com>
2020-03-11 13:36:10 +0000

    common: beerocks: add association length constant
    The absolute maximum size of any frame according to the 802.11
    specifications is MMPDU size of 2304 bytes. The actual size of a
    (re)association frame should be in the range of a couple of hundreds of
    bytes, so be on the safe side and set the maximum size to 2KB.
    
    For that reason, add a constant variable that is set to the desired size.
    
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

--- common/beerocks/bcl/include/bcl/beerocks_defines.h
+++ common/beerocks/bcl/include/bcl/beerocks_defines.h
@@ -40,13 +40,18 @@ static constexpr int DISCOVERY_NOTIFICATION_TIMEOUT_SEC = 60;
 namespace message {
 
 enum eStructsConsts {
-    VERSION_LENGTH                = 16,
-    NODE_NAME_LENGTH              = 32,
-    IFACE_NAME_LENGTH             = 32 + 4, //need extra 1 byte for null termination + alignment
-    SUPPORTED_CHANNELS_LENGTH     = 64,     //support upto # channels, every channel item is 32-bit
-    HOSTAP_ERR_MSG_LENGTH         = 64,
-    WIFI_DRIVER_VER_LENGTH        = 32 + 4,
-    WIFI_SSID_MAX_LENGTH          = 32 + 1 + 3, //need extra 1 byte for null termination + alignment
+    VERSION_LENGTH            = 16,
+    NODE_NAME_LENGTH          = 32,
+    IFACE_NAME_LENGTH         = 32 + 4, //need extra 1 byte for null termination + alignment
+    SUPPORTED_CHANNELS_LENGTH = 64,     //support upto # channels, every channel item is 32-bit
+    HOSTAP_ERR_MSG_LENGTH     = 64,
+    WIFI_DRIVER_VER_LENGTH    = 32 + 4,
+    WIFI_SSID_MAX_LENGTH      = 32 + 1 + 3, //need extra 1 byte for null termination + alignment
+    // The absolute maximum size of any frame according to the 802.11 specification
+    // is MMPDU size of 2304 bytes. The actual size of an (re)association frame
+    // should be in the range of a couple of hundreds of bytes, so be on the safe side
+    // and set the maximum size to 2KB
+    ASSOCIATION_MAX_LENGTH        = 2048,
     WIFI_PASS_MAX_LENGTH          = 64 + 1 + 3, //need extra 1 byte for null termination + alignment
     USER_PASS_LEN                 = 64 + 1 + 3, //need extra 1 byte for null termination + alignment
     DEV_INFO_STR_MAX_LEN          = 32,
