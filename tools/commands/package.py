e50565eac2c0f60c374f408b718cc8ec8b688b88
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-03-12 19:43:24 +0200

    package.py: make code PEP8-compatible
    Making package.py PEP8-compatible as a part of
    preparation for automatic style checks of python files on CI.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- tools/commands/package.py
+++ tools/commands/package.py
@@ -1,16 +1,14 @@
 import logging
 import argparse
 import os
-import getpass
 import subprocess
-import collections
 import shutil
 import tarfile
 import time
 import filecmp
 
 logger = logging.getLogger("package")
-package_modules=['framework', 'common', 'controller', 'agent', 'tools']
+package_modules = ['framework', 'common', 'controller', 'agent', 'tools']
 license = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../LICENSE')
 header_template = '''/* SPDX-License-Identifier: BSD-2-Clause-Patent
  *
@@ -23,10 +21,13 @@ header_template = '''/* SPDX-License-Identifier: BSD-2-Clause-Patent
 
 ignored_keywords = ['/external/', '/ipkg-install/']
 
+
 class mappackage(object):
     def __init__(self, args):
-        if args.verbose: logger.setLevel(logging.DEBUG)
-        modules = package_modules if 'all' in args.modules else [m for m in package_modules if m in args.modules]
+        if args.verbose:
+            logger.setLevel(logging.DEBUG)
+        modules = package_modules if 'all' in args.modules else [
+            m for m in package_modules if m in args.modules]
         modules_dir = os.path.realpath(args.map_path)
         package_dir = os.path.realpath(modules_dir + '/package')
         h = {'framework': None, 'common': None, 'controller': None, 'agent': None}
@@ -43,22 +44,31 @@ class mappackage(object):
             dst_path = "{}/{}".format(package_dir, name)
             if not filecmp.cmp(license, src_path + '/LICENSE'):
                 raise Exception("{} has invalid license, aborting!".format(name))
-            stdout, stderr = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], cwd=src_path, stdout=subprocess.PIPE).communicate()
-            try: h[name] = stdout.decode("utf-8").strip()
-            except: pass
+            stdout, stderr = subprocess.Popen(
+                ['git', 'rev-parse', '--short', 'HEAD'],
+                cwd=src_path, stdout=subprocess.PIPE).communicate()
+            try:
+                h[name] = stdout.decode("utf-8").strip()
+            except Exception:
+                pass
             logger.debug("Copy {} to {}".format(src_path, dst_path))
-            shutil.copytree(src_path, dst_path, symlinks=True, ignore=lambda directory, contents: [".git"])
-            with open("{}/VERSION".format(dst_path), 'w') as f: f.write("intel_multiap_{}_{}_{}".format(name, time.strftime("%Y%m%d"), h[name]))
+            shutil.copytree(src_path, dst_path, symlinks=True,
+                            ignore=lambda directory, contents: [".git"])
+            with open("{}/VERSION".format(dst_path), 'w') as f:
+                f.write("intel_multiap_{}_{}_{}".format(
+                    name, time.strftime("%Y%m%d"), h[name]))
             for root, dirs, files in os.walk(dst_path):
                 for name in files:
                     self.remove_patents(os.path.join(root, name))
                     self.verify_header(os.path.join(root, name))
-        
-        with tarfile.open("{}/intel_multiap_{}.tar.gz".format(package_dir, time.strftime("%Y%m%d")), "w:gz") as tar:
+
+        with tarfile.open("{}/intel_multiap_{}.tar.gz".format(package_dir, time.strftime("%Y%m%d")),
+                          "w:gz") as tar:
             tar.add(package_dir, arcname=os.path.basename(package_dir))
 
     def verify_header(self, file):
-        if file.endswith(('.h', '.hpp', '.c', '.cpp')) and not any(s in file for s in ignored_keywords):
+        if file.endswith(('.h', '.hpp', '.c', '.cpp')) and \
+                         not any(s in file for s in ignored_keywords):
             with open(file) as f:
                 if header_template not in f.read():
                     logger.error("{} does not have the required header template".format(file))
@@ -69,17 +79,22 @@ class mappackage(object):
             with open(file, "r") as f:
                 lines = f.readlines()
             with open(file, "w") as f:
-                skipline=0
+                skipline = 0
                 for line in lines:
-                    if '#ifdef' in line and 'PATENT' in line: skipline+=1
-                    elif skipline and '#ifdef' in line: skipline+=1
-                    if not skipline: f.write(line)
-                    if skipline and '#endif' in line: skipline-=1
+                    if '#ifdef' in line and 'PATENT' in line:
+                        skipline += 1
+                    elif skipline and '#ifdef' in line:
+                        skipline += 1
+                    if not skipline:
+                        f.write(line)
+                    if skipline and '#endif' in line:
+                        skipline -= 1
 
     @staticmethod
     def configure_parser(parser=argparse.ArgumentParser(prog='package')):
         parser.help = "multiap_sw standalone packaging module"
-        parser.add_argument('modules', choices=['all'] + package_modules, nargs='+', help='module[s] to package')
+        parser.add_argument('modules', choices=['all'] +
+                            package_modules, nargs='+', help='module[s] to package')
         parser.add_argument("--verbose", "-v", action="store_true", help="verbosity on")
 
         return parser
