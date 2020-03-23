dafc96ecdcc653bc79ae4c17c6355927d182247b
Coral Malachi <coral.malachi@intel.com>
2020-03-23 18:41:57 +0000

    not fill - pass
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
@@ -1902,7 +1902,7 @@ bool backhaul_manager::handle_client_capability_query(ieee1905_1::CmduMessageRx 
         auto associated_clients = m_radio_info_map[client_ruid].associated_clients_map[client_vap];
         auto associatedClientsTuple = associated_clients[client_info_tlv_r->client_mac()];
         auto len                    = std::get<1>(associatedClientsTuple).length();
-        // client_capability_report_tlv->alloc_association_frame(15);
+        client_capability_report_tlv->alloc_association_frame(1);
         // auto s = std::get<1>(associatedClientsTuple);
         LOG(DEBUG) << "**********************************************************************";
 
