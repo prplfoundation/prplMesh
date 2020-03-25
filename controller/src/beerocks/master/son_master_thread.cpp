5984ef820fcf2005e07f20ccef6fa9b3b698c4da
Moran Shoeg <moranx.shoeg@intel.com>
2020-03-25 18:15:08 +0000

    controller: Add src_mac to some messages prints
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- controller/src/beerocks/master/son_master_thread.cpp
+++ controller/src/beerocks/master/son_master_thread.cpp
@@ -1482,7 +1482,8 @@ bool master_thread::handle_cmdu_1905_topology_notification(const std::string &sr
                                                            ieee1905_1::CmduMessageRx &cmdu_rx)
 {
     auto mid = cmdu_rx.getMessageId();
-    LOG(DEBUG) << "Received TOPOLOGY_NOTIFICATION_MESSAGE, mid=" << std::hex << int(mid);
+    LOG(DEBUG) << "Received TOPOLOGY_NOTIFICATION_MESSAGE, from " << src_mac << ", mid=" << std::hex
+               << int(mid);
 
     // IEEE 1905.1 defines that TOPOLOGY_NOTIFICATION_MESSAGE must containt one 1905.1 AL MAC
     // address type TLV, and MultiAp standard extend it with zero or one Client Association Event
@@ -1603,7 +1604,8 @@ bool master_thread::handle_cmdu_1905_topology_response(const std::string &src_ma
                                                        ieee1905_1::CmduMessageRx &cmdu_rx)
 {
     auto mid = cmdu_rx.getMessageId();
-    LOG(DEBUG) << "Received TOPOLOGY_RESPONSE_MESSAGE, mid=" << std::hex << int(mid);
+    LOG(DEBUG) << "Received TOPOLOGY_RESPONSE_MESSAGE from " << src_mac << ", mid=" << std::hex
+               << int(mid);
 
     auto tlvDeviceInformation = cmdu_rx.getClass<ieee1905_1::tlvDeviceInformation>();
     if (!tlvDeviceInformation) {
