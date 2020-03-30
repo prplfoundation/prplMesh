prplfoundation/prplMesh

commit 372fc6eb6ff60c88988f9e0e1b306dd3f952df67

controller: avoid explicit call to constructor
On the make_pair invocation, cppcheck would fail with the following error:
error: SymbolDatabase bailout; unhandled code [syntaxError]

It looks like a bug in cppcheck that I could not reproduce with a
simpler example.

Since we anyway don't need the explicit call to the constructor, use
{} to initialize the pair.

Fixes 1 cppcheck issue of severity error.

Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

controller/src/beerocks/master/tasks/rdkb/rdkb_wlan_task_db.cpp
@@ -126,7 +126,7 @@ rdkb_wlan_task_db::get_ap_config(const std::string &bssid)
         }
     }
     LOG(ERROR) << "can't find entry for bssid=" << bssid;
-    return std::make_pair(false, beerocks_message::sSteeringApConfig());
+    return std::make_pair(false, {});
 }
 
 std::unordered_map<std::string, std::shared_ptr<beerocks_message::sSteeringClientConfig>>

commit e50ec6586228179e22211bc8a1d5c6ff05a20675

controller: fix compilation error on rdkb
Commit 372fc6eb6ff60c88988f9e0e1b306dd3f952df67 introduced a
compilation error:
rdkb_wlan_task_db.cpp:129:36: error: no matching function for call to 'make_pair(bool, <brace-enclosed initializer list>)'
|      return std::make_pair(false, {});

To fix it, we could create the pair like this:
return {false, {}};

However, since bool is value-initialized to false, we can directly do
this just as well:
return {};

Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

controller/src/beerocks/master/tasks/rdkb/rdkb_wlan_task_db.cpp
@@ -126,7 +126,7 @@ rdkb_wlan_task_db::get_ap_config(const std::string &bssid)
         }
     }
     LOG(ERROR) << "can't find entry for bssid=" << bssid;
-    return std::make_pair(false, {});
+    return {};
 }
 
 std::unordered_map<std::string, std::shared_ptr<beerocks_message::sSteeringClientConfig>>