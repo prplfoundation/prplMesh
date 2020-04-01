07d596df9e7aacffd432efb1a8207aa2abce4464
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-04-01 16:27:20 +0300

    logger_widget.py: make code PEP8-compliant
    Make logger_widget.py PEP8-compliant as a part of
    preparation for automatic style checks of python files on CI.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- tools/beerocks_analyzer/logger_widget.py
+++ tools/beerocks_analyzer/logger_widget.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python
-import sys
-import os
-import ctypes
-import threading
-import socket
-import time
-
-from PySide2.QtCore import *
-from PySide2.QtGui import *
+from PySide2.QtCore import Signal, QObject
+from PySide2.QtGui import QTextCursor
 from PySide2.QtWidgets import QWidget, QTextEdit, QTabWidget, QFormLayout
 
 
 class UpdateSig(QObject):
     sig = Signal(float)
+
 
 class LoggerWidget(QWidget):
     def __init__(self, parent=None):
@@ -21,14 +15,14 @@ class LoggerWidget(QWidget):
         self.logOutput.setReadOnly(True)
         self.logOutput.setLineWrapMode(QTextEdit.NoWrap)
         self.logOutput.moveCursor(QTextCursor.End)
-        #self.font = self.logOutput.font()
-        #self.font.setFamily("Courier")
-        #self.font.setPointSize(10)
-        #self.logOutput.setCurrentFont(font)
-        #self.logOutput.setTextColor(color)
-        #self.logOutput.resize(800,200)
-        #self.setMinimumWidth(1000)
-        
+        # self.font = self.logOutput.font()
+        # self.font.setFamily("Courier")
+        # self.font.setPointSize(10)
+        # self.logOutput.setCurrentFont(font)
+        # self.logOutput.setTextColor(color)
+        # self.logOutput.resize(800,200)
+        # self.setMinimumWidth(1000)
+
         self.tab_widget = QTabWidget(self)
         self.tab_widget.addTab(self.logOutput, "EventsLogger")
 
