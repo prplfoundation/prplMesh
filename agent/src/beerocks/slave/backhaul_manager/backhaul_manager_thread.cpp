630cf70a684eb6021963323345830fc4d24a0cca
Coral Malachi <coral.malachi@intel.com>
2020-04-08 09:18:27 +0000

    agent: backhaul manager: save association frame by client
    When the ACTION_BACKHAUL_CLIENT_ASSOCIATED_NOTIFICATION vendor-specific
    message is received on the backhaul manager,  remove that client form the
    associated_clients_t map (since maybe that client was associated with
    another radio) and fill the info again with the frame body of the most
    recently received (Re)Association Request frame from this client.
    
    That way the associated_clients_t map always contains info of each client,
    and we can easily check which radio and VAP the client is associated with
    and get the association frame.
    
    Signed-off-by: Coral Malachi <coral.malachi@intel.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
@@ -2001,10 +2001,21 @@ bool backhaul_manager::handle_slave_backhaul_message(std::shared_ptr<SSlaveSocke
             return false;
         }
 
-        // Set client association information for associated client
+        //remove this client from other radios
+        remove_client_from_all_radios(msg->client_mac());
+
         auto &associated_clients =
             m_radio_info_map[msg->iface_mac()].associated_clients_map[msg->bssid()];
-        associated_clients[msg->client_mac()] = std::chrono::steady_clock::now();
+
+        sClientInfo client_info;
+        client_info.client_mac = msg->client_mac();
+        client_info.radio_mac  = msg->iface_mac();
+        client_info.bssid      = msg->bssid();
+        client_info.time_stamp = std::chrono::steady_clock::now();
+        client_info.asso_len   = msg->association_frame_length();
+        memcpy(client_info.assoc_req, msg->association_frame(), client_info.asso_len);
+        associated_clients[msg->client_mac()] = client_info;
+
         break;
     }
     case beerocks_message::ACTION_BACKHAUL_CLIENT_DISCONNECTED_NOTIFICATION: {
