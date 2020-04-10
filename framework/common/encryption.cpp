412fd3a37ec001a72016e2b8640e5a991314a2e8
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2020-04-10 11:32:57 +0200

    treewide: remove duplicate Copyright lines
    The automatic replacement in the previous commit created a few cases
    where the same copyright line appears twice. Remove these duplicates.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- framework/common/encryption.cpp
+++ framework/common/encryption.cpp
@@ -2,7 +2,6 @@
  *
  * Copyright (c) 2017, Broadband Forum
  * Copyright (c) 2019 the prplMesh contributors (see AUTHORS.md)
- * Copyright (c) 2019 the prplMesh contributors (see AUTHORS.md)
  *
  * This code is subject to the terms of the BSD+Patent license.
  * See LICENSE file for more details.
