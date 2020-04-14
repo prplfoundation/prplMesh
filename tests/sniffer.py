03d040ad18fd31dce50621f8864a3cd2e91fd914
Gal Wiener <gal.wiener@intel.com>
2020-04-14 11:45:35 +0000

    tests/sniffer: add tshark capture and parse method
    Add a method parsing the pcap file into json using tshark, deserializing
    it with python.
    
    We need to know the file to which we are capturing for this. Therefore,
    store it in the Sniffer object, and reset it when the capture is
    stopped. Note that the latter makes it impossible to use the
    get_packet_capture() method after the test - for now, that's how we
    intend to use it, but this may change in the future.
    
    Signed-off-by: Gal Wiener <gal.wiener@intel.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tests/sniffer.py
+++ tests/sniffer.py
@@ -6,8 +6,8 @@
 ###############################################################
 
 import os
+import json
 import subprocess
-
 from opts import debug, err, status
 
 
@@ -17,13 +17,14 @@ class Sniffer:
         self.interface = interface
         self.tcpdump_log_dir = tcpdump_log_dir
         self.tcpdump_proc = None
+        self.current_outputfile = None
 
     def start(self, outputfile_basename):
         '''Start tcpdump to outputfile.'''
         debug("Starting tcpdump, output file {}.pcap".format(outputfile_basename))
         os.makedirs(os.path.join(self.tcpdump_log_dir, 'logs'), exist_ok=True)
-        outputfile = os.path.join(self.tcpdump_log_dir, outputfile_basename) + ".pcap"
-        command = ["tcpdump", "-i", self.interface, '-U', '-w', outputfile]
+        self.current_outputfile = os.path.join(self.tcpdump_log_dir, outputfile_basename) + ".pcap"
+        command = ["tcpdump", "-i", self.interface, '-U', '-w', self.current_outputfile]
         self.tcpdump_proc = subprocess.Popen(command, stderr=subprocess.PIPE)
         # tcpdump takes a while to start up. Wait for the appropriate output before continuing.
         # poll() so we exit the loop if tcpdump terminates for any reason.
@@ -37,6 +38,26 @@ class Sniffer:
         else:
             err("tcpdump terminated")
             self.tcpdump_proc = None
+            self.current_outputfile = None
+
+    def get_packet_capture(self):
+        '''Get a list of packets from the last started tcpdump.'''
+        if not self.current_outputfile:
+            err("get_packet_capture but no capture file")
+            return []
+        tshark_command = ['tshark', '-r', self.current_outputfile, '-T', 'json']
+        tshark_result = subprocess.run(tshark_command, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+        if tshark_result.returncode != 0:
+            err(tshark_result.stderr)
+            err("tshark failed: {}".format(tshark_result.returncode))
+            return []
+        try:
+            capture = json.loads(tshark_result.stdout)
+            return capture
+        except json.JSONDecodeError as error:
+            err("capture JSON decoding failed: %s".format(error))
+            return []
 
     def stop(self):
         '''Stop tcpdump if it is running.'''
@@ -44,3 +65,4 @@ class Sniffer:
             status("Terminating tcpdump")
             self.tcpdump_proc.terminate()
             self.tcpdump_proc = None
+            self.current_outputfile = None
