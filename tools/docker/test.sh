3bb023b4fcd1c55d4d65860a6ac3b9518dfbea93
Ivan Efimov <i.efimov@inango-systems.com>
2020-04-18 22:28:54 +0500

    scripts: tools/docker/test.sh: fix shellcheck issues SC2164, SC2068, SC1090, SC2034
    Fix shellcheck issues in tools/docker/test.sh:
      https://www.shellcheck.net/wiki/SC2164 -- Use 'cd ... || exit' or 'cd ... || return' in case cd fails.
      https://www.shellcheck.net/wiki/SC2068 -- Double quote to prevent globbing and word splitting.
      https://www.shellcheck.net/wiki/SC1090 -- Can't follow non-constant source. Use a directive to specify location.
    
    Signed-off-by: Ivan Efimov <i.efimov@inango-systems.com>

--- tools/docker/test.sh
+++ tools/docker/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ###############################################################
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 # SPDX-FileCopyrightText: 2019-2020 the prplMesh contributors (see AUTHORS.md)
@@ -6,13 +6,14 @@
 # See LICENSE file for more details.
 ###############################################################
 
-scriptdir="$(cd "${0%/*}"; pwd)"
+scriptdir="$(cd "${0%/*}" && pwd)"
 rootdir="${scriptdir%/*/*}"
 
-. ${rootdir}/tools/functions.sh
+# shellcheck source=../../tools/functions.sh
+. "${rootdir}/tools/functions.sh"
 
 usage() {
-    echo "usage: $(basename $0) [-hv] [-n name]"
+    echo "usage: $(basename "$0") [-hv] [-n name]"
     echo "  options:"
     echo "      -h|--help - show this help menu"
     echo "      -v|--verbose - verbosity on"
@@ -20,7 +21,11 @@ usage() {
 }
 
 main() {
-    OPTS=`getopt -o 'hvn:' --long verbose,help,name -n 'parse-options' -- "$@"`
+    if ! OPTS=$(getopt -o 'hvn:' --long verbose,help,name -n 'parse-options' -- "$@"); then
+        err "Failed parsing options." >&2
+        usage
+        exit 1
+    fi
 
     eval set -- "$OPTS"
 
@@ -34,9 +39,10 @@ main() {
         esac
     done
 
-    run docker container exec ${NAME} ${rootdir}/build/install/scripts/prplmesh_utils.sh status $OPT
+    run docker container exec "${NAME}" "${rootdir}/build/install/scripts/prplmesh_utils.sh" status $OPT
 }
 
 VERBOSE=false
 NAME=prplMesh
-main $@
+
+main "$@"
