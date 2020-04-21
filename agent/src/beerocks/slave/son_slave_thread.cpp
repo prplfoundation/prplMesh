610a9b7c2abb64e4fe04b83877c278e9bb2c2f07
Ran Regev <ran.regev@devalore.com>
2020-04-21 13:04:06 +0300

    agent: slave: fine tuning 1905->vs transmormation
    It seems that the transformation that is done up
    to this change is missing. Added operatrion class
    and report.
    
    This is probably the first change in a serious of changes
    in this transformation:
    Transform No. I
    
    MAP-4.7.9_ETH:netgear-rax40
    
    Signed-off-by: Ran regev <ran.regev@devalore.com>

--- agent/src/beerocks/slave/son_slave_thread.cpp
+++ agent/src/beerocks/slave/son_slave_thread.cpp
@@ -4447,6 +4447,8 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
     auto mid = cmdu_rx.getMessageId();
     LOG(DEBUG) << "Received BEACON_METRICS_QUERY_MESSAGE, mid=" << std::hex << int(mid);
 
+    /*
+    // Note: No need to send ack - the backhaul already sent the ACK
     // 1. send ACK to the controller
     if (!cmdu_tx.create(mid, ieee1905_1::eMessageType::ACK_MESSAGE)) {
         LOG(ERROR) << "cmdu creation of type ACK_MESSAGE, has failed";
@@ -4457,6 +4459,7 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
         LOG(ERROR) << "failed sending ACK to the controller";
         return false;
     }
+    */
 
     // 2. forward the request to the monitor
     // using vs message
@@ -4483,9 +4486,10 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
     Legend: 
         V - fine, we have value
         ? - don't know where to take the value from
+        U - uncertain, set but not sure it is the correct value
     V uint8_t measurement_mode;
     V uint8_t channel;
-    ? int16_t op_class;
+    V int16_t op_class;
     V uint16_t repeats;
     V uint16_t rand_ival;
     V uint16_t duration;
@@ -4494,7 +4498,7 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
     V uint8_t parallel;
     V uint8_t enable;
     V uint8_t request;
-    V uint8_t report;
+    U uint8_t report;
     V uint8_t mandatory_duration;
     V uint8_t expected_reports_count;
     V uint8_t use_optional_ssid;
@@ -4522,12 +4526,16 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
     measurement_request_out.rand_ival = beerocks::BEACON_MEASURE_DEFAULT_RANDOMIZATION_INTERVAL;
     measurement_request_out.sta_mac   = beaconMetricsQuery->associated_sta_mac();
 
+    measurement_request_out.op_class = beaconMetricsQuery->operating_class();
+
+    // FixMe: not sure about this
+    measurement_request_out.report = beaconMetricsQuery->reporting_detail_value();
+
     // values based on https://github.com/prplfoundation/prplMesh/pull/1114#discussion_r406326546
     measurement_request_out.repeats            = 0;
     measurement_request_out.parallel           = 0;
     measurement_request_out.enable             = 0;
     measurement_request_out.request            = 1;
-    measurement_request_out.report             = 0;
     measurement_request_out.mandatory_duration = 0;
     measurement_request_out.use_optional_ssid  = 0;
 
@@ -4540,6 +4548,7 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
     // FixMe:
     // here (I think) we need to copy m_ap_channel_reports_list from the 1905 beaconMetricsQuery
     // into ap_ch_report char array of the vs message (measurement_request_out)
+
     // currently not supported
     // if (0 != measurement_request_out.use_optional_ap_ch_report) {
     //    LOG(ERROR) << "unsupported optional channel report in the request";
@@ -4551,7 +4560,6 @@ bool slave_thread::handle_beacon_metrics_query_request(Socket *sd,
     // end FixMe
 
     // FixMe:
-    // measurement_request_out.op_class = ??
     // measurement_request_out.use_optional_wide_band_ch_switch = ??
     // measurement_request_out.new_ch_width = ??
     // measurement_request_out.new_ch_center_freq_seg_0 = ??
