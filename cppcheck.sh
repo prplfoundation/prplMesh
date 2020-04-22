266c33c2b8cfbddba4e93d2d829eee1b0a285296
Raphaël Mélotte <raphael.melotte@mind.be>
2020-04-22 16:37:02 +0200

    cppcheck: move the wrapper to tools/docker
    Since cppcheck.sh won't be the only wrapper that runs with docker,
    it's best to move it. clang-format wrapper is already under
    tools/docker, so it makes sense to also put it there.
    
    While we're at it, add a license header.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- cppcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scriptdir="$(cd "${0%/*}" && pwd)"
-rootdir="${scriptdir}"
-
-docker run --rm -it -w "$rootdir" -v "$rootdir:$rootdir"  prplfoundationinc/prplmesh-builder:alpine-3.11.3 "$rootdir"/tools/docker/static-analysis/cppcheck.sh -j"$(nproc)" .
