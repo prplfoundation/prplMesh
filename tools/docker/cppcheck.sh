77048bede83b8809a0f66d04c929bfef95900478
Raphaël Mélotte <raphael.melotte@mind.be>
2020-04-22 16:43:31 +0200

    tools: docker: also run cppcheck-new-issues.sh
    Multiple developers like to be able to run locally the jobs that we
    run in CI.
    
    Because cppcheck results are so dependent on the version being used,
    it needs to be run with the same docker image that is used in CI.
    
    When running tools/docker/cppcheck.sh, also run what the
    "cppcheck-diff" job runs, in the same docker image.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/docker/cppcheck.sh
+++ tools/docker/cppcheck.sh
@@ -11,3 +11,6 @@ scriptdir="$(cd "${0%/*}" && pwd)"
 rootdir="$(realpath "$scriptdir/../..")"
 
 docker run --rm -it -w "$rootdir" -v "$rootdir:$rootdir" --user="${SUDO_UID:-$UID}"  prplfoundationinc/prplmesh-builder:alpine-3.11.3 "$rootdir"/tools/docker/static-analysis/cppcheck.sh -j"$(nproc)" .
+
+# Also check for new issues:
+docker run --rm -it -w "$rootdir" -v "$rootdir:$rootdir" --user="${SUDO_UID:-$UID}" prplfoundationinc/prplmesh-builder:alpine-3.11.3 "$rootdir"/ci/cppcheck/cppcheck-new-issues.sh .
