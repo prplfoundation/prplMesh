35d84c46e2fba5ba6f4587b109f976071c1f397d
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-04-23 07:02:37 +0000

    agent: add backhaul start_wps_pbc and datapath
    Preparative commit.
    Add start_wps_pbc API in the backhaul manager which is called by the
    agent ucc listener on startWpsPbc command, and pass the message till it
    gets to the ap manager.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- agent/src/beerocks/slave/ap_manager_thread.cpp
+++ agent/src/beerocks/slave/ap_manager_thread.cpp
@@ -867,6 +867,11 @@ bool ap_manager_thread::handle_cmdu(Socket *sd, ieee1905_1::CmduMessageRx &cmdu_
 
         break;
     }
+    case beerocks_message::ACTION_APMANAGER_START_WPS_PBC_REQUEST: {
+        LOG(DEBUG) << "Got ACTION_APMANAGER_START_WPS_PBC_REQUEST";
+        //TODO
+        break;
+    }
     case beerocks_message::ACTION_APMANAGER_CLIENT_IRE_CONNECTED_NOTIFICATION: {
         auto update =
             beerocks_header
