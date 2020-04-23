7108e21ccd91aeabdc67b00d7a22f381fbc14d36
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2020-04-23 16:04:20 +0200

    docker: runner: save bridge MAC address
    Due to limitations of the controller topology database (each node must
    have a different MAC address), the bridge MAC must be different from any
    interface MAC.
    
    Apparently, this doesn't cause an issue in CI at the moment. Probably
    that's because the fake ETHERNET node that is created also has the same
    IP address as the backhaul interface address, and these problems
    counteract each other. However, this accidental solution only happens
    because docker generates sequential MAC addresses for the interfaces. If
    random addresses are used instead (which is what podman does), things
    break down again.
    
    Fix this by setting the br-lan MAC address explicitly instead of
    inheriting the address of one of the interfaces. When the an interface
    is added to the bridge, the bridge inherits its MAC address. So save the
    (random) MAC address that we get when the bridge is created, and
    re-apply it after adding all interfaces.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/runner/entrypoint.sh
+++ tools/docker/runner/entrypoint.sh
@@ -11,6 +11,7 @@ run() {
     "$@" || exit $?
 }
 
+
 # Use the ip address that was allocated by the daemon to this
 # container (from eth0)
 bridge_ip="$(ip addr show dev eth0 | awk '/^ *inet / {print $2}')"
@@ -18,12 +19,19 @@ bridge_ip="$(ip addr show dev eth0 | awk '/^ *inet / {print $2}')"
 run ip link add          br-lan   type bridge
 run ip link add          wlan0    type dummy
 run ip link add          wlan2    type dummy
+
+# When an interface is added to the bridge, the bridge inherits its MAC address.
+# It shouldn't be the same as any other interface because that messes up the topology in the
+# controller, however. Therefore, save the MAC address an re-apply it later.
+bridge_mac="$(ip link show dev br-lan | awk '/^ *link\/ether / {print $2}')"
+
 run ip link set      dev eth0     master br-lan
 run ip link set      dev wlan0    master br-lan
 run ip link set      dev wlan2    master br-lan
 run ip address flush dev eth0
 run ip link set      dev wlan0    up
 run ip link set      dev wlan2    up
+run ip link set      dev br-lan   addr "$bridge_mac"
 run ip address add   dev br-lan "$bridge_ip"
 run ip link set      dev br-lan   up
 
