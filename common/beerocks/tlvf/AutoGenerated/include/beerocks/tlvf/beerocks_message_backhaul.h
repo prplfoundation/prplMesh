7fbf9775b2dda9ab656058e51c36eb2cd4a41eb9
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-04-23 14:59:41 +0300

    tlvf: add ACTION_BACKHAUL_HOSTAP_STATS_MEASUREMENT_REQUEST
    This message is added in order to trigger HOSTAP_STATS_MEASUREMENT by backhaul.
    It is needed for an intermediate implementation of Associated STA Link Metrics flow.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_backhaul.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_backhaul.h
@@ -540,6 +540,27 @@ class cACTION_BACKHAUL_CLIENT_DISCONNECTED_NOTIFICATION : public BaseClass
         sMacAddr* m_bssid = nullptr;
 };
 
+class cACTION_BACKHAUL_HOSTAP_STATS_MEASUREMENT_REQUEST : public BaseClass
+{
+    public:
+        cACTION_BACKHAUL_HOSTAP_STATS_MEASUREMENT_REQUEST(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cACTION_BACKHAUL_HOSTAP_STATS_MEASUREMENT_REQUEST(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cACTION_BACKHAUL_HOSTAP_STATS_MEASUREMENT_REQUEST();
+
+        static eActionOp_BACKHAUL get_action_op(){
+            return (eActionOp_BACKHAUL)(ACTION_BACKHAUL_HOSTAP_STATS_MEASUREMENT_REQUEST);
+        }
+        uint8_t& sync();
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eActionOp_BACKHAUL* m_action_op = nullptr;
+        uint8_t* m_sync = nullptr;
+};
+
 }; // close namespace: beerocks_message
 
 #endif //_BEEROCKS/TLVF_BEEROCKS_MESSAGE_BACKHAUL_H_
