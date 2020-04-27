b5d8d89f32e262ecc1436af5e581d38495869ab1
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-04-27 21:48:55 +0300

    tlvf: add ACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST
    Currently Hostap Measurements request is used instead of
    ACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST,
    but it lacks MAC address field.
    Associated Link Measurements require MAC field because
    it collects information about one specific STA.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_monitor.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_monitor.h
@@ -423,6 +423,29 @@ class cACTION_MONITOR_HOSTAP_ACTIVITY_NOTIFICATION : public BaseClass
         sApActivityNotificationParams* m_params = nullptr;
 };
 
+class cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST : public BaseClass
+{
+    public:
+        cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST();
+
+        static eActionOp_MONITOR get_action_op(){
+            return (eActionOp_MONITOR)(ACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST);
+        }
+        uint8_t& sync();
+        sMacAddr& sta_mac();
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eActionOp_MONITOR* m_action_op = nullptr;
+        uint8_t* m_sync = nullptr;
+        sMacAddr* m_sta_mac = nullptr;
+};
+
 class cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_RESPONSE : public BaseClass
 {
     public:
--- common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_monitor.cpp
+++ common/beerocks/tlvf/AutoGenerated/src/beerocks/tlvf/beerocks_message_monitor.cpp
@@ -1326,6 +1326,86 @@ bool cACTION_MONITOR_HOSTAP_ACTIVITY_NOTIFICATION::init()
     return true;
 }
 
+cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::~cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST() {
+}
+uint8_t& cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::sync() {
+    return (uint8_t&)(*m_sync);
+}
+
+sMacAddr& cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::sta_mac() {
+    return (sMacAddr&)(*m_sta_mac);
+}
+
+void cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::class_swap()
+{
+    tlvf_swap(8*sizeof(eActionOp_MONITOR), reinterpret_cast<uint8_t*>(m_action_op));
+    m_sta_mac->struct_swap();
+}
+
+bool cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(uint8_t); // sync
+    class_size += sizeof(sMacAddr); // sta_mac
+    return class_size;
+}
+
+bool cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_REQUEST::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_sync = (uint8_t*)m_buff_ptr__;
+    if (!buffPtrIncrementSafe(sizeof(uint8_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint8_t) << ") Failed!";
+        return false;
+    }
+    m_sta_mac = (sMacAddr*)m_buff_ptr__;
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
+        return false;
+    }
+    if (!m_parse__) { m_sta_mac->struct_init(); }
+    if (m_parse__) { class_swap(); }
+    return true;
+}
+
 cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_RESPONSE::cACTION_MONITOR_CLIENT_ASSOCIATED_STA_LINK_METRIC_RESPONSE(uint8_t* buff, size_t buff_len, bool parse) :
     BaseClass(buff, buff_len, parse) {
     m_init_succeeded = init();
