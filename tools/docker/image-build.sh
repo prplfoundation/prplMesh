675104003bca08838f8971af5aa6f08359226549
Vitaly Bukhovsky <vitaly@devalore.com>
2020-04-28 13:25:56 +0000

    docker: image-builder: support selective build
    Support building only the "builder" or "runner" images (previous logic always built both).
    When using the VSCode devcontainer, everything is built
    locally, so the "builder" container is not required.
    
    Remove the unused "VERBOSE" flag and fix shellcheck warnings.
    
    Signed-off-by: Vitaly Bukhovsky <vitaly@devalore.com>

--- tools/docker/image-build.sh
+++ tools/docker/image-build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ###############################################################
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 # SPDX-FileCopyrightText: 2019-2020 the prplMesh contributors (see AUTHORS.md)
@@ -9,22 +9,24 @@
 scriptdir="$(cd "${0%/*}" || exit 1; pwd)"
 rootdir="${scriptdir%/*/*}"
 
-# shellcheck source=../../tools/functions.sh
+# Suppress shellcheck's "Can't follow non-constant source" warning
+# shellcheck disable=SC1090
 . "${rootdir}/tools/functions.sh"
 
 usage() {
     echo "usage: $(basename "$0") [-hvbt]"
     echo "  mandatory:"
-    echo "      type - image type <runner/builder>"
+    echo "      -i|--image - specify the image(s) to build (can be specified multiple times)"
+    echo "                   supported values: builder/runner"
     echo "  options:"
     echo "      -h|--help - show this help menu"
-    echo "      -v|--verbose - verbosity on"
     echo "      -b|--base-image - Base OS image to use (Dockerfile 'FROM')"
-    echo "      -t|--tag - tag to add to prplmesh-builder and prplmesh-runner images"
+    echo "      -t|--tag - tag to add to the built images"
+    echo "      -v|--verbose - verbose output"
 }
 
 main() {
-    if ! OPTS=$(getopt -o 'hvb:t:' --long verbose,help,base-image,tag -n 'parse-options' -- "$@"); then
+    if ! OPTS=$(getopt -o 'hb:t:i:v' --long help,base-image:,tag:,image:,verbose -n 'parse-options' -- "$@"); then
         err "Failed parsing options." >&2
         usage
         exit 1
@@ -34,34 +36,47 @@ main() {
 
     while true; do
         case "$1" in
-            -v | --verbose)         VERBOSE=true; shift ;;
             -h | --help)            usage; exit 0; shift ;;
-            -b | --base-image)      IMAGE="$2"; shift ; shift ;;
+            -b | --base-image)      BASE_IMAGE="$2"; shift ; shift ;;
             -t | --tag)             TAG=":$2"; shift ; shift ;;
+            -i | --image)           BUILD_IMAGES="${BUILD_IMAGES}$2;"; shift ; shift ;;
+            -v | --verbose)         export VERBOSE=true; shift ;;
             -- ) shift; break ;;
             * ) err "unsupported argument $1"; usage; exit 1 ;;
         esac
     done
 
-    dbg "IMAGE=$IMAGE"
-    dbg "TAG=$TAG"
+    dbg "BASE_IMAGE=${BASE_IMAGE}:${TAG}"
+    dbg "TAG=${TAG}"
+    dbg "BUILD_IMAGES=${BUILD_IMAGES}"
     dbg "rootdir=$rootdir"
 
-    info "Base docker image $IMAGE"
-    info "Generating builder docker image (prplmesh-builder$TAG)"
-    run docker image build \
-        --build-arg image="$IMAGE" \
-        --tag "prplmesh-builder$TAG" \
-        "${scriptdir}/builder"
+    # Make sure that a build image was specified
+    if [[ "${BUILD_IMAGES}" == ";" ]]; then
+        err "build image not specified!"
+        usage; exit 1
+    fi
 
-    info "Generating runner docker image (prplmesh-runner$TAG)"
-    run docker image build \
-        --build-arg image="$IMAGE" \
-        --tag "prplmesh-runner$TAG" \
-        "${scriptdir}/runner"
+    if [[ "${BUILD_IMAGES}" == *";builder;"* ]]; then
+        info "Generating builder docker image (prplmesh-builder$TAG)"
+        info "Base docker image $BASE_IMAGE"
+        run docker image build \
+            --build-arg image="$BASE_IMAGE" \
+            --tag "prplmesh-builder$TAG" \
+            "${scriptdir}/builder"
+    fi
+
+    if [[ "${BUILD_IMAGES}" == *";runner;"* ]]; then
+        info "Generating runner docker image (prplmesh-runner$TAG)"
+        info "Base docker image $BASE_IMAGE"
+        run docker image build \
+            --build-arg image="$BASE_IMAGE" \
+            --tag "prplmesh-runner$TAG" \
+            "${scriptdir}/runner"
+    fi
 }
 
-VERBOSE=false
-IMAGE="ubuntu:18.04"
+BASE_IMAGE="ubuntu:18.04"
+BUILD_IMAGES=";"
 
 main "$@"
