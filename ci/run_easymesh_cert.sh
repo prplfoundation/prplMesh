30b188fd7f0d416cc68b5664a2cd887fc657936c
Raphaël Mélotte <raphael.melotte@mind.be>
2020-04-28 16:33:29 +0200

    CI: run_easymesh_cert.sh: add an option to skip downloading prplMesh
    If we want to use run_easymesh_cert.sh in Gitlab CI, we have to add
    the ability to skip the download, as Gitlab already takes care of that
    with the dependencies.
    
    - add a new option, --skip-download, to skip the download (but not
      necessarily the upgrade). It defaults to false, to keep the same
      behavior as before.
    
    - move the downloaded ipk to build/$TARGET_DEVICE. Since that's where
      Gitlab CI downloads the ipk, it's easier to put the downloaded one
      there.
    
    - only store the ipk and build info if an ipk was deployed.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- ci/run_easymesh_cert.sh
+++ ci/run_easymesh_cert.sh
@@ -32,6 +32,7 @@ usage() {
     echo "      --owncloud-upload - whether or not to upload results to owncloud. (default: false)"
     echo "      --owncloud-path - relative path in the owncloud server to upload results to. (default: $OWNCLOUD_PATH)"
     echo "      --skip-upgrade - skip prplmesh upgrade (default: false)"
+    echo "      --skip-download - skip prplmesh download (default: false)"
     echo "'test' can either be a test name, or the path to a file containing"
     echo " test names (newline-separated)."
     echo
@@ -75,24 +76,31 @@ upgrade_prplmesh() {
         echo "skip prplmesh upgrade for non prplmesh device $TARGET_DEVICE"
         return 1
     fi
-    info "download latest ipk"
-    "$TOOLS_PATH"/download_ipk.sh ${VERBOSE:+ -v} || {
-        err "Failed to download prplmesh.ipk, abort"
-        return 1
-    }
-    
+    local ipk_dir="$rootdir/build/$TARGET_DEVICE/"
+
+    if [ "$DOWNLOAD_PRPLMESH" = true ] ; then
+        info "download latest ipk in $ipk_dir"
+        mkdir -p "$ipk_dir"
+        "$TOOLS_PATH"/download_ipk.sh --path "$ipk_dir" ${VERBOSE:+ -v} || {
+            err "Failed to download prplmesh.ipk, abort"
+            return 1
+        }
+    fi
+
     info "prplmesh build info:"
-    cat "$PRPLMESH_BUILDINFO"
+    cat "$ipk_dir/$PRPLMESH_BUILDINFO"
 
     info "deploy latest ipk to $TARGET_DEVICE_SSH"
-    "$TOOLS_PATH"/deploy_ipk.sh "$TARGET_DEVICE_SSH" "$PRPLMESH_IPK" || {
+    "$TOOLS_PATH"/deploy_ipk.sh "$TARGET_DEVICE_SSH" "$ipk_dir/$PRPLMESH_IPK" || {
         err "Failed to deploy prplmesh.ipk, abort"
         return 1
     }
+    mv "$ipk_dir/$PRPLMESH_IPK" "$LOG_FOLDER"
+    mv "$ipk_dir/$PRPLMESH_BUILDINFO" "$LOG_FOLDER"
 }
 
 main() {
-    if ! OPTS=$(getopt -o 'hvb:o:e:d:s:' --long help,verbose,log-folder:,easymesh-cert:,device:,ssh:,owncloud-upload,owncloud-path:,skip-upgrade -n 'parse-options' -- "$@"); then
+    if ! OPTS=$(getopt -o 'hvb:o:e:d:s:' --long help,verbose,log-folder:,easymesh-cert:,device:,ssh:,owncloud-upload,owncloud-path:,skip-upgrade,skip-download -n 'parse-options' -- "$@"); then
         err "Failed parsing options." >&2
         usage
         exit 1
@@ -111,6 +119,7 @@ main() {
             --owncloud-upload)      OWNCLOUD_UPLOAD=true; shift;;
             --owncloud-path)        OWNCLOUD_PATH="$2"; shift 2;;
             --skip-upgrade)         UPGRADE_PRPLMESH=false; shift;;
+            --skip-download)        DOWNLOAD_PRPLMESH=false; shift;;
             -- ) shift; break ;;
             * ) err "unsupported argument $1"; usage; exit 1;;
         esac
@@ -128,8 +137,6 @@ main() {
 
     [ "$UPGRADE_PRPLMESH" = "true" ] && {
         upgrade_prplmesh
-        mv "$PRPLMESH_IPK" "$LOG_FOLDER"
-        mv "$PRPLMESH_BUILDINFO" "$LOG_FOLDER"
     }
     
     info "Start running tests"
@@ -159,5 +166,6 @@ TARGET_DEVICE="netgear-rax40"
 TARGET_DEVICE_SSH="$TARGET_DEVICE-1"
 OWNCLOUD_PATH=Nightly/agent_certification
 UPGRADE_PRPLMESH=true
+DOWNLOAD_PRPLMESH=true
 
 main "$@"
