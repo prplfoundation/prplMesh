8f36b5e48a71755f857defe49fd08e4b40c40d14
Ran Regev <ran.regev@devalore.com>
2020-05-11 21:30:47 +0300

    added beacon metrics query to cmdu-rx list of messages
    Signed-off-by: Ran Regev <ran.regev@devalore.com>

--- framework/tlvf/src/src/CmduMessageRx.cpp
+++ framework/tlvf/src/src/CmduMessageRx.cpp
@@ -33,6 +33,7 @@
 #include <tlvf/wfa_map/tlvApMetrics.h>
 #include <tlvf/wfa_map/tlvApRadioBasicCapabilities.h>
 #include <tlvf/wfa_map/tlvApRadioIdentifier.h>
+#include <tlvf/wfa_map/tlvBeaconMetricsQuery.h>
 #include <tlvf/wfa_map/tlvChannelPreference.h>
 #include <tlvf/wfa_map/tlvChannelSelectionResponse.h>
 #include <tlvf/wfa_map/tlvClientAssociationControlRequest.h>
@@ -207,6 +208,9 @@ std::shared_ptr<BaseClass> CmduMessageRx::parseNextTlv()
     }
     case (149): {
         return msg.addClass<wfa_map::tlvStaMacAddressType>();
+    }
+    case (153): {
+        return msg.addClass<wfa_map::tlvBeaconMetricsQuery>();
     }
     case (155): {
         return msg.addClass<wfa_map::tlvSteeringRequest>();
