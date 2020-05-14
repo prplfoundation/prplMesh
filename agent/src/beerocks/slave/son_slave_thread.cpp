40014b05ae7abc559bd85a08eba4d376b9263fa1
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-05-14 14:09:40 +0300

    son_slave: START_MONITORING: remove debug output of ipv4
    This is a preparation to removal of `ipv4` field from `sClientMonitoringParams`.
    
    It was used to supply IPv4 address to STA monitoring subsystem,
    but now it is supplied by `ACTION_MONITOR_CLIENT_NEW_IP_ADDRESS_NOTIFICATION` from son_slave.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/slave/son_slave_thread.cpp
+++ agent/src/beerocks/slave/son_slave_thread.cpp
@@ -531,9 +531,8 @@ bool slave_thread::handle_cmdu_control_message(Socket *sd,
         }
 
         std::string client_mac = network_utils::mac_to_string(request_in->params().mac);
-        std::string client_ip = network_utils::ipv4_to_string(request_in->params().ipv4);
 
-        LOG(DEBUG) << "START_MONITORING_REQUEST: mac=" << client_mac << " ip=" << client_ip;
+        LOG(DEBUG) << "START_MONITORING_REQUEST: mac=" << client_mac;
 
         if (request_in->params().is_ire) {
             auto request_out = message_com::create_vs_message<
