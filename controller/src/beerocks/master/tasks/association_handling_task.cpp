2cc9abafb9336339d82269ade390b3bc9b629dbe
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-05-14 14:09:40 +0300

    controller: START_MONITORING: do not set bridge mac
    This is a preparation to removal of `bridge_4addr_mac` field from `sClientMonitoringParams`.
    
    It was used to supply bridge mac to STA monitoring subsystem,
    but now bridge mac is stored as `al_mac` member of `monitor_thread`.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- controller/src/beerocks/master/tasks/association_handling_task.cpp
+++ controller/src/beerocks/master/tasks/association_handling_task.cpp
@@ -107,19 +107,7 @@ void association_handling_task::work()
         request->params().mac    = network_utils::mac_from_string(sta_mac);
         request->params().ipv4   = network_utils::ipv4_from_string(database.get_node_ipv4(sta_mac));
         request->params().vap_id = database.get_node_vap_id(sta_mac);
-        request->params().is_ire = false;
-
-        //add bridge mac for ires
-        if (database.get_node_type(sta_mac) == beerocks::TYPE_IRE_BACKHAUL) {
-            request->params().is_ire = true;
-            auto bridge_container    = database.get_node_children(sta_mac, beerocks::TYPE_IRE);
-            if (!bridge_container.empty()) {
-                std::string bridge_4addr_mac = *bridge_container.begin();
-                request->params().bridge_4addr_mac =
-                    network_utils::mac_from_string(bridge_4addr_mac);
-                LOG(DEBUG) << "IRE " << sta_mac << " is on a bridge with mac " << bridge_4addr_mac;
-            }
-        }
+        request->params().is_ire = database.get_node_type(sta_mac) == beerocks::TYPE_IRE_BACKHAUL;
 
         auto radio_mac = database.get_node_parent_radio(new_hostap_mac);
         auto agent_mac = database.get_node_parent_ire(radio_mac);
