04e7721b2d30095ea755ff4b1eb046f792426eba
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2020-05-16 14:44:19 +0300

    agent: monitor: supply bridge_4addr_mac and ipv4 by START_MONITORING
    ACTION_MONITOR_CLIENT_START_MONITORING_REQUEST was used to trigger STA monitoring.
    Now it is done by STA_Connected BWL handler.
    
    bridge_4addr_mac and ipv4 are not supplied by STA_Connected,
    we still need START_MONITORING_REQUEST, but its functions are narrowed.
    
    It does not trigger measurements anymore, it only supplies the missing data
    to existing measurements.
    
    Signed-off-by: Vitalii Komisarenko <vitalii.komisarenko@gmail.com>

--- agent/src/beerocks/monitor/monitor_thread.cpp
+++ agent/src/beerocks/monitor/monitor_thread.cpp
@@ -801,34 +801,17 @@ bool monitor_thread::handle_cmdu_vs_message(Socket &sd, ieee1905_1::CmduMessageR
         }
 
         auto sta_node = mon_db.sta_find(sta_mac);
-        if (sta_node) {
-            mon_db.sta_erase(sta_mac);
-        }
-
-        auto vap_node = mon_db.vap_get_by_id(vap_id);
-        if (vap_node == nullptr) {
-            LOG(ERROR) << "vap_id " << vap_id << " doesn't not exists";
+        if (!sta_node) {
             response->success() = false;
             message_com::send_cmdu(slave_socket, cmdu_tx);
             return false;
         }
 
-        sta_node = mon_db.sta_add(sta_mac, vap_id);
         sta_node->set_ipv4(sta_ipv4);
         sta_node->set_bridge_4addr_mac(set_bridge_4addr_mac);
 
         response->success() = true;
         message_com::send_cmdu(slave_socket, cmdu_tx);
-#ifdef BEEROCKS_RDKB
-        //clean rdkb monitor data if already in database.
-        auto client = mon_rdkb_hal.conf_get_client(sta_mac);
-        if (client != nullptr) {
-            client->setStartTime(std::chrono::steady_clock::now());
-            client->setLastSampleTime(std::chrono::steady_clock::now());
-            client->setAccumulatedPackets(0);
-            client->clearData();
-        }
-#endif
         break;
     }
     case beerocks_message::ACTION_MONITOR_CLIENT_NEW_IP_ADDRESS_NOTIFICATION: {
