f5dd39aa7717ea10a96ac96087edf690b5ba8a9d
Gal Wiener <gal.wiener@intel.com>
2020-05-17 01:30:59 +0300

    ci/owncloud: add remote filesystem methods
    in preparation for uploading files to the remote server, add methods for
    recursively creating remote directories from a given path.
    
    Signed-off-by: Gal Wiener <gal.wiener@intel.com>

--- ci/owncloud/upload_to_owncloud.py
+++ ci/owncloud/upload_to_owncloud.py
@@ -1,3 +1,47 @@
+from os import path
+from getpass import getuser
 from webdav3.client import Client
 from opts import debug, message, opts
+
+def get_path_components(dirname):
+    """
+    get_path_components(dirname)
+
+    Gets a list of all subpaths needed to build a directory, including itself.
+
+    Parameters
+    ----------
+    dirname : str
+        A given directory path
+    Returns
+    -------
+    A python list of path strings
+    For an input 'a/b/c' returns ['a/b/c', 'a/b', 'a']
+    """
+    path_list = [dirname, ]
+    tmp = path.split(dirname)
+    while tmp[0] != "" and not path.ismount(tmp[0]):
+        path_list.append(tmp[0])
+        tmp = path.split(tmp[0])
+    path_list.reverse()
+    return path_list
+
+
+def mkdir_recursively(webdav_client, remote_path):
+    """
+    mkdir_recursively(remote_path)
+
+    Recursively creates all directories needed to create the input path.
+
+    Parameters
+    ----------
+    remote_path: str
+    """
+    pathlist = get_path_components(remote_path)
+    for p in pathlist:
+        try:
+            webdav_client.mkdir(p)
+        except BaseException as e:
+            debug(e)
+
 
