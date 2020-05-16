cae4eb0bfed73db00237294e4ba188b99d937f7a
Gal Wiener <gal.wiener@intel.com>
2020-05-17 01:34:24 +0300

    ci/owncloud: add getter of server credentials
    add a method for getting the webdav server's credentials, by default
    they're retreived from ~/.netrc, but there's an option for retreiving
    them from an external file.
    
    Signed-off-by: Gal Wiener <gal.wiener@intel.com>

--- ci/owncloud/upload_to_owncloud.py
+++ ci/owncloud/upload_to_owncloud.py
@@ -1,7 +1,45 @@
 from os import path
 from getpass import getuser
+import netrc
 from webdav3.client import Client
+import json
 from opts import debug, message, opts
+
+default_hostname = "https://ftp.essensium.com/owncloud/remote.php/dav/files/"
+
+
+def get_credentials(options_path=None, force_netrc=True):
+    """
+    get_credentials(options_path=None, force_netrc=True)
+
+    Gets the user's default credentials from either an external options file or ~/.netrc
+
+    Parameters
+    ----------
+    options_path : str, optional
+                path to the options file
+
+    force_netrc : bool
+                flag indicating whether to skip parsing the options
+                file and skip straight to the contents of ~/.netrc
+    Returns
+    -------
+    dictionary with 3 key/value pairs,
+    keys are: {webdav_login", "webdav_password","webdav_hostname")
+    """
+    if not force_netrc and path.exists(options_path):
+        with open(options_path) as options_file:
+            options = json.load(options_file)
+            return options
+
+    netrc_file = netrc.netrc()
+    credentials = netrc_file.authenticators("ftp.essensium.com")
+    return {
+        "webdav_login": credentials[0],
+        "webdav_password": credentials[2],
+        "webdav_hostname": default_hostname + credentials[0]
+    }
+
 
 def get_path_components(dirname):
     """
