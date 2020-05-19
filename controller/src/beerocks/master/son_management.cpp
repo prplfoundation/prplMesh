7182753fb65daac66851889265ed8e6d2fcb27c7
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-05-19 06:58:08 +0000

    controller: son_management: add support for all-channel scan.
    The "all-channels" scan uses a specific channel pool to be used in order
    to denote that the user wishes to scan on all channel.
    The pool is a set of only a single value set to "0".
    When the pool matches the requirements for an "all-channel" scan, all
    the radio's supported channels, need to be set as the DCS's channel
    pool.
    
    Add function that identifies the "all-channel" pool.
    Add function that gets the radio's support channels and sets them to the
    given channel pool.
    
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- controller/src/beerocks/master/son_management.cpp
+++ controller/src/beerocks/master/son_management.cpp
@@ -30,6 +30,45 @@ using namespace beerocks;
 using namespace net;
 using namespace son;
 
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////// Local Module Functions ///////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Check if the given channel pool fulfils the "all-channel" scan requirement.
+ * @param channel_pool_set: Unordered set containing the current channel pool.
+ * @return true if pool matches the "all-channel" scan requirement
+ * @return false if not.
+ */
+bool is_scan_all_channels_request(std::unordered_set<uint8_t> &channel_pool_set)
+{
+    return channel_pool_set.size() == 1 && *channel_pool_set.begin() == SCAN_ALL_CHANNELS;
+}
+
+/**
+ * @brief Get the channel pool containing all the supported channels.
+ * @param channel_pool_set:  Set containing the current channel pool.
+ * @param channel_pool_size: Size of the given set.
+ * @param database:          Reference to the DB.
+ * @param radio_mac:         MAC address of radio.
+ */
+static void get_pool_of_all_supported_channels(std::unordered_set<uint8_t> &channel_pool_set,
+                                               uint8_t &channel_pool_size, db &database,
+                                               const sMacAddr &radio_mac)
+{
+    LOG(DEBUG) << "Setting channel pool to all channels";
+    channel_pool_set.clear();
+    auto all_channels = database.get_hostap_supported_channels(tlvf::mac_to_string(radio_mac));
+    std::transform(all_channels.begin(), all_channels.end(),
+                   std::inserter(channel_pool_set, channel_pool_set.end()),
+                   [](beerocks::message::sWifiChannel &c) -> uint8_t { return c.channel; });
+    channel_pool_size = channel_pool_set.size();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// Implementation ///////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 void son_management::handle_cli_message(Socket *sd,
                                         std::shared_ptr<beerocks_header> beerocks_header,
                                         ieee1905_1::CmduMessageTx &cmdu_tx, db &database,
@@ -1950,8 +1989,12 @@ void son_management::handle_bml_message(Socket *sd,
         }
         if (channel_pool != nullptr && channel_pool_size != CHANNEL_SCAN_INVALID_PARAM &&
             op_error_code == eChannelScanOperationCode::SUCCESS) {
+            auto u_channel_pool_size = uint8_t(channel_pool_size);
             auto channel_pool_set =
                 std::unordered_set<uint8_t>(channel_pool, channel_pool + channel_pool_size);
+            if (is_scan_all_channels_request(channel_pool_set))
+                get_pool_of_all_supported_channels(channel_pool_set, u_channel_pool_size, database,
+                                                   radio_mac);
             op_error_code = database.set_channel_scan_pool(radio_mac, channel_pool_set, false)
                                 ? op_error_code
                                 : eChannelScanOperationCode::INVALID_PARAMS_CHANNELPOOL;
@@ -2218,7 +2261,9 @@ void son_management::handle_bml_message(Socket *sd,
             break;
         }
 
-        LOG(DEBUG) << "set_channel_scan_pool " << channel_pool_size;
+        if (is_scan_all_channels_request(channel_pool_set))
+            get_pool_of_all_supported_channels(channel_pool_set, channel_pool_size, database,
+                                               radio_mac);
         if (!database.set_channel_scan_pool(radio_mac, channel_pool_set, true)) {
             LOG(ERROR) << "set_channel_scan_pool failed";
             response->op_error_code() =
