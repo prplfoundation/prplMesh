073f7e0a84c2f67c8a1f0a5236ae2214e9f35a10
Mario Maz <mmazzapater@gmail.com>
2020-05-20 12:20:05 +0200

    docker: add scripts to create coverage reports
    Add scripts to create coverage reports both natively and using docker
    image.
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

---
+++ tools/docker/coverage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+###############################################################
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# SPDX-FileCopyrightText: 2019-2020 the prplMesh contributors (see AUTHORS.md)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+###############################################################
+
+scriptdir="$(cd "${0%/*}" || exit 1; pwd)"
+rootdir="${scriptdir%/*/*}"
+
+# shellcheck source=functions.sh
+. "${rootdir}/tools/functions.sh"
+
+main() {
+    image=${PRPLMESH_BUILDER_IMAGE-"${DOCKER_REGISTRY}"prplmesh-builder}
+    # Default docker arguments
+    docker_args=(
+        --workdir "${rootdir}"
+        --user "${SUDO_UID:-$(id -u)}:${SUDO_GID:-$(id -g)}"
+        -e "USER=${SUDO_USER:-${USER}}"
+        -v "${rootdir}:${rootdir}"
+        --name prplMesh-coverage
+        --entrypoint "./coverage.sh"
+    )
+
+    run docker container run "${docker_args[@]}" --rm "${image}" "$@"
+}
+
+main "$@"
