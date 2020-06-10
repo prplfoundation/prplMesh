4fa51f1219620270bb336dbd409907dc18fa93fe
Raphaël Mélotte <raphael.melotte@mind.be>
2020-06-10 18:05:41 +0200

    tools: openwrt: output what is being done
    The grep filter in .gitlab-ci.yml is also not needed anymore, since we
    don't run make with with V=sc anymore.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- tools/docker/builder/openwrt/scripts/build-openwrt.sh
+++ tools/docker/builder/openwrt/scripts/build-openwrt.sh
@@ -6,6 +6,7 @@
 # See LICENSE file for more details.
 ###############################################################
 
+printf '\033[1;35m%s Configuring prplWrt\n\033[0m' "$(date --iso-8601=seconds --universal)"
 mkdir -p files/etc
 #   We need to keep the hashes in the firmware, to later know if an upgrade is needed:
 printf '%s=%s\n' "OPENWRT_REPOSITORY" "$OPENWRT_REPOSITORY" >> files/etc/prplwrt-version
@@ -43,5 +44,8 @@ else
     make defconfig
     printf '%s=%s\n' "PRPL_FEED" "$PRPL_FEED" >> files/etc/prplwrt-version
 fi
+printf '\033[1;35m%s Building prplWrt\n\033[0m' "$(date --iso-8601=seconds --universal)"
 make -j"$(nproc)"
+
+printf '\033[1;35m%s Cleaning prplMesh\n\033[0m' "$(date --iso-8601=seconds --universal)"
 make package/prplmesh/clean
