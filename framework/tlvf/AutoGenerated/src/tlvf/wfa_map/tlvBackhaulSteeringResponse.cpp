4af5e0a059c053f6629da532d5082c611e68f9d5
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-06-11 16:37:55 +0300

    framework: tlvf: Turn on Backhaul STA Steering TLV's
    For auto-generating TLV for Backhaul STA Steering
    Request/Response needs to add yaml files to the tlv_conf.yaml.
    It is turning on auto-generation code from those TLV.
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

---
+++ framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvBackhaulSteeringResponse.cpp
@@ -0,0 +1,143 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <tlvf/wfa_map/tlvBackhaulSteeringResponse.h>
+#include <tlvf/tlvflogging.h>
+
+using namespace wfa_map;
+
+tlvBackhaulSteeringResponse::tlvBackhaulSteeringResponse(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+tlvBackhaulSteeringResponse::tlvBackhaulSteeringResponse(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+tlvBackhaulSteeringResponse::~tlvBackhaulSteeringResponse() {
+}
+const eTlvTypeMap& tlvBackhaulSteeringResponse::type() {
+    return (const eTlvTypeMap&)(*m_type);
+}
+
+const uint16_t& tlvBackhaulSteeringResponse::length() {
+    return (const uint16_t&)(*m_length);
+}
+
+sMacAddr& tlvBackhaulSteeringResponse::backhaul_station_mac() {
+    return (sMacAddr&)(*m_backhaul_station_mac);
+}
+
+sMacAddr& tlvBackhaulSteeringResponse::target_bssid() {
+    return (sMacAddr&)(*m_target_bssid);
+}
+
+tlvBackhaulSteeringResponse::eResultCode& tlvBackhaulSteeringResponse::result_code() {
+    return (eResultCode&)(*m_result_code);
+}
+
+void tlvBackhaulSteeringResponse::class_swap()
+{
+    tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
+    m_backhaul_station_mac->struct_swap();
+    m_target_bssid->struct_swap();
+    tlvf_swap(8*sizeof(eResultCode), reinterpret_cast<uint8_t*>(m_result_code));
+}
+
+bool tlvBackhaulSteeringResponse::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+        *m_length -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t tlvBackhaulSteeringResponse::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(eTlvTypeMap); // type
+    class_size += sizeof(uint16_t); // length
+    class_size += sizeof(sMacAddr); // backhaul_station_mac
+    class_size += sizeof(sMacAddr); // target_bssid
+    class_size += sizeof(eResultCode); // result_code
+    return class_size;
+}
+
+bool tlvBackhaulSteeringResponse::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_type = reinterpret_cast<eTlvTypeMap*>(m_buff_ptr__);
+    if (!m_parse__) *m_type = eTlvTypeMap::TLV_BACKHAUL_STEERING_RESPONSE;
+    if (!buffPtrIncrementSafe(sizeof(eTlvTypeMap))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(eTlvTypeMap) << ") Failed!";
+        return false;
+    }
+    m_length = reinterpret_cast<uint16_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_length = 0;
+    if (!buffPtrIncrementSafe(sizeof(uint16_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint16_t) << ") Failed!";
+        return false;
+    }
+    m_backhaul_station_mac = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
+        return false;
+    }
+    if(m_length && !m_parse__){ (*m_length) += sizeof(sMacAddr); }
+    if (!m_parse__) { m_backhaul_station_mac->struct_init(); }
+    m_target_bssid = reinterpret_cast<sMacAddr*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(sMacAddr))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(sMacAddr) << ") Failed!";
+        return false;
+    }
+    if(m_length && !m_parse__){ (*m_length) += sizeof(sMacAddr); }
+    if (!m_parse__) { m_target_bssid->struct_init(); }
+    m_result_code = reinterpret_cast<eResultCode*>(m_buff_ptr__);
+    if (!buffPtrIncrementSafe(sizeof(eResultCode))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(eResultCode) << ") Failed!";
+        return false;
+    }
+    if(m_length && !m_parse__){ (*m_length) += sizeof(eResultCode); }
+    if (m_parse__) { class_swap(); }
+    if (m_parse__) {
+        if (*m_type != eTlvTypeMap::TLV_BACKHAUL_STEERING_RESPONSE) {
+            TLVF_LOG(ERROR) << "TLV type mismatch. Expected value: " << int(eTlvTypeMap::TLV_BACKHAUL_STEERING_RESPONSE) << ", received value: " << int(*m_type);
+            return false;
+        }
+    }
+    return true;
+}
+
+
