2c43b827372aee09ab7f02beca28e78ef8ca4096
odkq <pablo@odkq.com>
2020-06-24 20:21:18 +0200

    boardfarm-ci: Add exit status check in _run_shell_cmd
    Export $ROOT_DIR into images
    
    Signed-off-by: odkq <pablo@odkq.com>

--- tests/environment.py
+++ tests/environment.py
@@ -243,7 +243,8 @@ class ALEntityDocker(ALEntity):
     The entity is defined from the name of the container, the rest is derived from that.
     '''
 
-    def __init__(self, name: str, is_controller: bool = False):
+    def __init__(self, name: str, is_controller: bool = False,
+                 compose: bool = False):
         self.name = name
         self.bridge_name = 'br-lan'
 
@@ -257,16 +258,19 @@ class ALEntityDocker(ALEntity):
                 re.search(r'ucc_listener_port=(?P<port>[0-9]+)', config_file.read()).group('port')
 
         # On WSL, connect to the locally exposed container port
-        if on_wsl:
-            published_port_output = subprocess.check_output(
-                ["docker", "port", name, ucc_port]).decode('utf-8').split(":")
-            device_ip = published_port_output[0]
-            ucc_port = int(published_port_output[1])
+        if not compose:
+            if on_wsl:
+                published_port_output = subprocess.check_output(
+                    ["docker", "port", name, ucc_port]).decode('utf-8').split(":")
+                device_ip = published_port_output[0]
+                ucc_port = int(published_port_output[1])
+            else:
+                device_ip_output = self.command(
+                    'ip', '-f', 'inet', 'addr', 'show', self.bridge_name)
+                device_ip = re.search(
+                    r'inet (?P<ip>[0-9.]+)', device_ip_output.decode('utf-8')).group('ip')
         else:
-            device_ip_output = self.command(
-                'ip', '-f', 'inet', 'addr', 'show', self.bridge_name)
-            device_ip = re.search(
-                r'inet (?P<ip>[0-9.]+)', device_ip_output.decode('utf-8')).group('ip')
+            device_ip = name
 
         ucc_socket = UCCSocket(device_ip, ucc_port)
         mac = ucc_socket.dev_get_parameter('ALid')
