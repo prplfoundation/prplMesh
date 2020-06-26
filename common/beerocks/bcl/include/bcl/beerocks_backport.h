eccfdd5f652472f7436024c35cb9ac8653a744a4
Vitaly Bukhovsky <vitaly@devalore.com>
2020-06-26 17:17:10 +0000

    bcl: template argument validation helper
    Using type traits, add a helper template class for checking if a
    template argument is derived from std::chrono::duration.
    
    An example usage of this helper can be to statically fail the build
    if the provided template class is of incorrect type:
    static_assert(is_chrono_duration<T>::value,
                  "T must be derived from std::chrono::duration");
    
    Signed-off-by: Vitaly Bukhovsky <vitaly@devalore.com>

--- common/beerocks/bcl/include/bcl/beerocks_backport.h
+++ common/beerocks/bcl/include/bcl/beerocks_backport.h
@@ -9,7 +9,9 @@
 #ifndef _BEEROCKS_BACKPORT_H_
 #define _BEEROCKS_BACKPORT_H_
 
+#include <chrono>
 #include <memory>
+#include <type_traits>
 
 #if __cplusplus < 201402L
 namespace std {
@@ -20,5 +22,15 @@ template <typename T, typename... Args> std::unique_ptr<T> make_unique(Args &&..
 }
 } // namespace std
 #endif
+
+// Until prplMesh will be built using C++20, there's no simple way to fail
+// the built if a template argument is not derived from std::chrono::duration.
+// This little trick allows using static_assert and fail the build in this case.
+template <typename T> struct is_chrono_duration : std::false_type {
+};
+
+template <typename R, typename P>
+struct is_chrono_duration<std::chrono::duration<R, P>> : std::true_type {
+};
 
 #endif // _BEEROCKS_BACKPORT_H_
