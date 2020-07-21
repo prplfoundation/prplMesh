603bf8ee42400bce3b250f70ab6d4fad6f9bbf7b
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-07-21 16:19:18 +0300

    common: tlvf: Remove deprecated messages
    Need to remove deprecated messages:
    ACTION_CONTROL_CLIENT_STATISTICS_11K_REQUEST
    ACTION_CONTROL_CLIENT_STATISTICS_11K_RESPONSE
    
    https://jira.prplfoundation.org/browse/PPM-291
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_control.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_control.h
@@ -1074,48 +1074,6 @@ class cACTION_CONTROL_CLIENT_BEACON_11K_RESPONSE : public BaseClass
         sBeaconResponse11k* m_params = nullptr;
 };
 
-class cACTION_CONTROL_CLIENT_STATISTICS_11K_REQUEST : public BaseClass
-{
-    public:
-        cACTION_CONTROL_CLIENT_STATISTICS_11K_REQUEST(uint8_t* buff, size_t buff_len, bool parse = false);
-        explicit cACTION_CONTROL_CLIENT_STATISTICS_11K_REQUEST(std::shared_ptr<BaseClass> base, bool parse = false);
-        ~cACTION_CONTROL_CLIENT_STATISTICS_11K_REQUEST();
-
-        static eActionOp_CONTROL get_action_op(){
-            return (eActionOp_CONTROL)(ACTION_CONTROL_CLIENT_STATISTICS_11K_REQUEST);
-        }
-        sStatisticsRequest11k& params();
-        void class_swap() override;
-        bool finalize() override;
-        static size_t get_initial_size();
-
-    private:
-        bool init();
-        eActionOp_CONTROL* m_action_op = nullptr;
-        sStatisticsRequest11k* m_params = nullptr;
-};
-
-class cACTION_CONTROL_CLIENT_STATISTICS_11K_RESPONSE : public BaseClass
-{
-    public:
-        cACTION_CONTROL_CLIENT_STATISTICS_11K_RESPONSE(uint8_t* buff, size_t buff_len, bool parse = false);
-        explicit cACTION_CONTROL_CLIENT_STATISTICS_11K_RESPONSE(std::shared_ptr<BaseClass> base, bool parse = false);
-        ~cACTION_CONTROL_CLIENT_STATISTICS_11K_RESPONSE();
-
-        static eActionOp_CONTROL get_action_op(){
-            return (eActionOp_CONTROL)(ACTION_CONTROL_CLIENT_STATISTICS_11K_RESPONSE);
-        }
-        sStatisticsResponse11k& params();
-        void class_swap() override;
-        bool finalize() override;
-        static size_t get_initial_size();
-
-    private:
-        bool init();
-        eActionOp_CONTROL* m_action_op = nullptr;
-        sStatisticsResponse11k* m_params = nullptr;
-};
-
 class cACTION_CONTROL_STEERING_CLIENT_SET_GROUP_REQUEST : public BaseClass
 {
     public:
