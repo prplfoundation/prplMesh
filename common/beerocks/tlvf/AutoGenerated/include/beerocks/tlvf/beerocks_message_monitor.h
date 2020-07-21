bdc84728f90942f59becbf02c3918b03c5e6dfc7
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-07-21 18:34:53 +0300

    common: tlvf: Remove deprecated VS message
    Need to remove deprecated VS message
    ACTION_MONITOR_CLIENT_STATISTICS_11K_RESPONSE.
    
    https://jira.prplfoundation.org/browse/PPM-291
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_monitor.h
+++ common/beerocks/tlvf/AutoGenerated/include/beerocks/tlvf/beerocks_message_monitor.h
@@ -613,48 +613,6 @@ class cACTION_MONITOR_CLIENT_CHANNEL_LOAD_11K_RESPONSE : public BaseClass
         sStaChannelLoadResponse11k* m_params = nullptr;
 };
 
-class cACTION_MONITOR_CLIENT_STATISTICS_11K_REQUEST : public BaseClass
-{
-    public:
-        cACTION_MONITOR_CLIENT_STATISTICS_11K_REQUEST(uint8_t* buff, size_t buff_len, bool parse = false);
-        explicit cACTION_MONITOR_CLIENT_STATISTICS_11K_REQUEST(std::shared_ptr<BaseClass> base, bool parse = false);
-        ~cACTION_MONITOR_CLIENT_STATISTICS_11K_REQUEST();
-
-        static eActionOp_MONITOR get_action_op(){
-            return (eActionOp_MONITOR)(ACTION_MONITOR_CLIENT_STATISTICS_11K_REQUEST);
-        }
-        sStatisticsRequest11k& params();
-        void class_swap() override;
-        bool finalize() override;
-        static size_t get_initial_size();
-
-    private:
-        bool init();
-        eActionOp_MONITOR* m_action_op = nullptr;
-        sStatisticsRequest11k* m_params = nullptr;
-};
-
-class cACTION_MONITOR_CLIENT_STATISTICS_11K_RESPONSE : public BaseClass
-{
-    public:
-        cACTION_MONITOR_CLIENT_STATISTICS_11K_RESPONSE(uint8_t* buff, size_t buff_len, bool parse = false);
-        explicit cACTION_MONITOR_CLIENT_STATISTICS_11K_RESPONSE(std::shared_ptr<BaseClass> base, bool parse = false);
-        ~cACTION_MONITOR_CLIENT_STATISTICS_11K_RESPONSE();
-
-        static eActionOp_MONITOR get_action_op(){
-            return (eActionOp_MONITOR)(ACTION_MONITOR_CLIENT_STATISTICS_11K_RESPONSE);
-        }
-        sStatisticsResponse11k& params();
-        void class_swap() override;
-        bool finalize() override;
-        static size_t get_initial_size();
-
-    private:
-        bool init();
-        eActionOp_MONITOR* m_action_op = nullptr;
-        sStatisticsResponse11k* m_params = nullptr;
-};
-
 class cACTION_MONITOR_CLIENT_NEW_IP_ADDRESS_NOTIFICATION : public BaseClass
 {
     public:
