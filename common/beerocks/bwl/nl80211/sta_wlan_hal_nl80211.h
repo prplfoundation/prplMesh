02f8b298ad5db53731766ca9ab5e1276aa685d2e
Mario Maz <mmazzapater@gmail.com>
2020-07-21 18:04:06 +0200

    bwl: nl80211: complete sta_wlan_hal
    https://jira.prplfoundation.org/browse/PPM-2
    
    The sta_wlan_hal for nl80211 is only partially implemented. Complete it
    with methods connect, disconnect, roam, is_connected and update_status.
    
    A large part of the code can be taken from dwpal.
    
    The rest seems unused or unnecessary in easymesh flows.
    
    Signed-off-by: Mario Maz <mmazzapater@gmail.com>

--- common/beerocks/bwl/nl80211/sta_wlan_hal_nl80211.h
+++ common/beerocks/bwl/nl80211/sta_wlan_hal_nl80211.h
@@ -73,6 +73,165 @@ protected:
     }
 
 private:
+    /**
+     * @brief Connection status information.
+     *
+     * Current WPA/EAPOL/EAP status information obtained with the STATUS wpa_supplicant command.
+     */
+    struct ConnectionStatus {
+        /**
+         * Basic Service Set Identifier.
+         */
+        std::string bssid;
+
+        /**
+         * Channel frequency in MHz for IBSS.
+         */
+        int freq;
+
+        /**
+         * Service Set Identifier (network name).
+         */
+        std::string ssid;
+
+        /**
+         * Network ID
+         */
+        int id;
+
+        /**
+         * IEEE 802.11 operation mode
+         */
+        std::string mode;
+
+        /**
+         * Allowed pairwise ciphers (WPA_CIPHER_*)
+         */
+        std::string pairwise_cipher;
+
+        /**
+         * Allowed group ciphers (WPA_CIPHER_*)
+         */
+        std::string group_cipher;
+
+        /**
+         * Allowed key management protocols (WPA_KEY_MGMT_*)
+         */
+        std::string key_mgmt;
+
+        /**
+         * Current wpa_supplicant state
+         */
+        std::string wpa_state;
+
+        /**
+         * MAC address of the network interface
+         */
+        std::string address;
+
+        /**
+         * Universally Unique Identifier of the device
+         */
+        std::string uuid;
+    };
+
+    /**
+     * @brief Adds a new network.
+     *
+     * Executes wpa_supplicant command ADD_NETWORK to create new network with empty configuration.
+     * The new network is disabled and once it has been configured it can be enabled with
+     * ENABLE_NETWORK command.
+     *
+     * @return Network ID of the new network or -1 on failure.
+     */
+    int add_network();
+
+    /**
+     * @brief Sets a network variable.
+     *
+     * Executes wpa_supplicant command SET_NETWORK to set a network variable value.
+     *
+     * This command uses the same variables and data formats as the wpa_supplicant configuration
+     * file. Examples:
+     * - ssid (network name, SSID)
+     * - psk (WPA passphrase or pre-shared key)
+     * - key_mgmt (key management protocol)
+     * - identity (EAP identity)
+     * - password (EAP password)
+     * - ...
+     *
+     * @param network_id Network ID of the network whose variable to set.
+     * @param param Name of the variable.
+     * @param value Value to set.
+     * @return True on success and false otherwise.
+     */
+    bool set_network(int network_id, const std::string &param, const std::string &value);
+
+    /**
+     * @brief Configures a network.
+     *
+     * Configures a newly added network before it can be enabled.
+     *
+     * @param network_id Network ID of the network to be configured.
+     * @param ssid Service Set Identifier (network name).
+     * @param bssid Basic Service Set Identifier.
+     * @param sec  WLAN Security Type (converted to a key management protocol).
+     * @param mem_only_psk Keep PSK/passphrase only in memory.
+     * @param pass WPA ASCII passphrase. If this is set, PSK will be generated using the SSID and
+     * passphrase configured for the network. ASCII passphrase must be between 8 and 63 characters
+     * (inclusive).
+     * @param hidden_ssid Scan this SSID with Probe Requests (scan_ssid can be used to scan for
+     * APs using hidden SSIDs).
+     * @param freq Frequency in MHz to allow for selecting the BSS. If 0, all frequencies are
+     * considered when selecting a BSS.
+     * @return True on success and false otherwise.
+     */
+    bool set_network_params(int network_id, const std::string &ssid, const std::string &bssid,
+                            WiFiSec sec, bool mem_only_psk, const std::string &pass,
+                            bool hidden_ssid, int freq = 0);
+
+    /**
+     * @brief Reads connection status information.
+     *
+     * Executes wpa_supplicant command STATUS to read current WPA/EAPOL/EAP status information and
+     * fills in given structure.
+     *
+     * @param[in,out] connection_status Connection status structure to fill in.
+     * @return True on success and false otherwise.
+     */
+    bool read_status(ConnectionStatus &connection_status);
+
+    /**
+     * @brief Updates connection status information.
+     *
+     * Sets class members containing active connection information with given connection status
+     * information (i.e.: network ID, BSSID, channel and SSID).
+     *
+     * @param connection_status Connection status information.
+     */
+    void update_status(const ConnectionStatus &connection_status);
+
+    /**
+     * @brief Enables a network.
+     *
+     * Executes wpa_supplicant command ENABLE_NETWORK to enable a network.
+     *
+     * @param network_id Network ID of the network to be enabled.
+     * @return True on success and false otherwise.
+     */
+    bool enable_network(int network_id);
+
+    /**
+     * @brief Checks if all authentication completed.
+     *
+     * Checks if wpa_supplicant has completed its processing for the association phase and that
+     * data connection is fully configured.
+     *
+     * @param wpa_state wpa_supplicant state (obtained with STATUS wpa_supplicant command).
+     * @return True if given wpa_supplicant state is "COMPLETED" and false otherwise.
+     */
+    bool is_connected(const std::string &wpa_state);
+
     // Active network parameters
     std::string m_active_ssid;
     std::string m_active_bssid;
