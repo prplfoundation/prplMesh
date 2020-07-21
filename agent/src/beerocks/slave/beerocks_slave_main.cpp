d21e46a3c7fc01bebfbdd9138e4aa4906004b8a7
Vladyslav Tupikin <v.tupikin@inango-systems.com>
2020-07-21 10:57:13 +0300

    cpmmon: bcl: Remove unused variable
    Need to remove deprecated variable
    enable_keep_alive.
    
    Also remove that variable from agent
    source code.
    
    https://jira.prplfoundation.org/browse/PPM-286
    
    Signed-off-by: Vladyslav Tupikin <v.tupikin@inango-systems.com>

--- agent/src/beerocks/slave/beerocks_slave_main.cpp
+++ agent/src/beerocks/slave/beerocks_slave_main.cpp
@@ -141,7 +141,6 @@ static void fill_son_slave_config(const beerocks::config_file::sConfigSlave &bee
     son_slave_conf.model     = beerocks_slave_conf.model;
     son_slave_conf.ucc_listener_port =
         beerocks::string_utils::stoi(beerocks_slave_conf.ucc_listener_port);
-    son_slave_conf.enable_keep_alive        = beerocks_slave_conf.enable_keep_alive == "1";
     son_slave_conf.bridge_iface             = beerocks_slave_conf.bridge_iface;
     son_slave_conf.backhaul_preferred_bssid = beerocks_slave_conf.backhaul_preferred_bssid;
     son_slave_conf.backhaul_wire_iface      = beerocks_slave_conf.backhaul_wire_iface;
