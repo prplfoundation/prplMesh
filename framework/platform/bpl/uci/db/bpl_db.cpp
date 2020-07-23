a3c94d386b7caec975d4a4da7810f61eb813363c
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-07-23 11:21:14 +0000

    framework: bpl: use the UCI APIs in the BPL DB.
    For the UCI implemented the persistent DB used the BPL UCI APIs.
    
    Add calls to the BPL UCI APIs from the BPL DB functions.
    
    Relates to JIRA PPM-5.
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- framework/platform/bpl/uci/db/bpl_db.cpp
+++ framework/platform/bpl/uci/db/bpl_db.cpp
@@ -6,7 +6,9 @@
  * See LICENSE file for more details.
  */
 
-#include "bpl/bpl_db.h"
+#include <bpl/bpl_db.h>
+
+#include "bpl_db_uci.h"
 
 #include <mapf/common/logger.h>
 
@@ -17,30 +19,63 @@
 namespace beerocks {
 namespace bpl {
 
+constexpr char DB_FILE[] = "prplmesh_db";
+
 bool db_has_entry(const std::string &entry_type, const std::string &entry_name)
 {
-    LOG(TRACE) << " - NOT IMPLEMENTED!";
-    return false;
+    LOG(TRACE) << entry_type << ":" << entry_name;
+    return db::uci_entry_exists(DB_FILE, entry_type, entry_name);
 }
 
 bool db_add_entry(const std::string &entry_type, const std::string &entry_name,
                   const std::unordered_map<std::string, std::string> &params)
 {
-    LOG(TRACE) << " - NOT IMPLEMENTED!";
-    return true;
+    LOG(TRACE) << entry_type << ":" << entry_name;
+
+    // Check if entry of the same name and type exist.
+    if (!db::uci_entry_exists(DB_FILE, entry_type, entry_name)) {
+        LOG(DEBUG) << "Entry " << entry_name << " does not exist! Create new one.";
+        if (!db::uci_add_entry(DB_FILE, entry_type, entry_name)) {
+            LOG(ERROR) << "Failed to create entry " << entry_name << "!";
+            return false;
+        }
+    }
+    // Update the existing entry
+    LOG(DEBUG) << "Update entry " << entry_name << " with new values.";
+    return db::uci_set_entry(DB_FILE, entry_type, entry_name, params);
 }
 
 bool db_set_entry(const std::string &entry_type, const std::string &entry_name,
                   const std::unordered_map<std::string, std::string> &params)
 {
-    LOG(TRACE) << " - NOT IMPLEMENTED!";
-    return true;
+    LOG(TRACE) << entry_type << ":" << entry_name;
+
+    if (!db::uci_entry_exists(DB_FILE, entry_type, entry_name)) {
+        LOG(ERROR) << "Entry " << entry_name << " not found, or exists under a diffrent type!";
+        return false;
+    }
+    // Update the new/existing entry
+    LOG(DEBUG) << "Update entry " << entry_name << " with new values.";
+    return db::uci_set_entry(DB_FILE, entry_type, entry_name, params);
 }
 
 bool db_get_entry(const std::string &entry_type, const std::string &entry_name,
                   std::unordered_map<std::string, std::string> &params)
 {
-    LOG(TRACE) << " - NOT IMPLEMENTED!";
+    LOG(TRACE) << entry_type << ":" << entry_name;
+
+    if (params.size() == 0) {
+        // params map is empty, getting all parameters
+        return db::uci_get_entry(DB_FILE, entry_type, entry_name, params);
+    }
+    for (auto &param : params) {
+        // params map is not empty, getting selected parameters
+        if (!db::uci_get_param(DB_FILE, entry_type, entry_name, param.first, param.second)) {
+            LOG(ERROR) << "Failed to get " << param.first;
+            // TODO:
+            return false;
+        }
+    }
     return true;
 }
 
@@ -48,14 +83,36 @@ bool db_get_entries_by_type(
     const std::string &entry_type,
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> &nested_params)
 {
-    LOG(TRACE) << " - NOT IMPLEMENTED!";
+    LOG(TRACE) << entry_type;
+
+    std::vector<std::string> entries;
+
+    if (!db::uci_get_all_entries(DB_FILE, entry_type, entries)) {
+        LOG(ERROR) << "Failed to get all entries";
+        return false;
+    }
+    for (auto &entry_name : entries) {
+        std::unordered_map<std::string, std::string> params;
+        if (!db::uci_get_entry(DB_FILE, entry_type, entry_name, params)) {
+            LOG(ERROR) << "Failed to get entry " << entry_name;
+            return false;
+        }
+        nested_params[entry_name] = params;
+    }
+
+    LOG(DEBUG) << "Found " << nested_params.size() << " entries!";
     return true;
 }
 
 bool db_remove_entry(const std::string &entry_type, const std::string &entry_name)
 {
-    LOG(TRACE) << " - NOT IMPLEMENTED!";
-    return true;
+    LOG(TRACE) << entry_type << ":" << entry_name;
+
+    if (!db::uci_entry_exists(DB_FILE, entry_type, entry_name)) {
+        LOG(DEBUG) << "Entry " << entry_name << " not found!";
+        return true;
+    }
+    return db::uci_delete_entry(DB_FILE, entry_type, entry_name);
 }
 
 } // namespace bpl
