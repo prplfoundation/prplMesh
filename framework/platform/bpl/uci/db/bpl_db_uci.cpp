694687b5048b53b8893333bb688fd8346e863722
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-07-23 11:20:32 +0000

    framework: bpl: uci: Implement delete_section function.
    The implementation of bpl_db_uci function utilizes the `libuci`
    functions that provides persistency of the configured data over UCI.
    
    Implemented the delete_section API, the function searches and removes
    the found section.
    
    Relates to JIRA PPM-5.
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- framework/platform/bpl/uci/db/bpl_db_uci.cpp
+++ framework/platform/bpl/uci/db/bpl_db_uci.cpp
@@ -356,6 +356,67 @@ bool uci_delete_section(const std::string &package_name, const std::string &sect
     LOG(TRACE) << "uci_delete_section() " << package_name << ".(" << section_type << ")"
                << section_name;
 
+    // Verify requested section exists
+    if (!uci_section_exists(package_name, section_type, section_name)) {
+        LOG(ERROR) << "section " << section_name << " of type " << section_type
+                   << " was not found!";
+        return false;
+    }
+
+    auto ctx = alloc_context();
+    if (!ctx) {
+        return false;
+    }
+
+    char sec_path[MAX_UCI_BUF_LEN] = {0};
+    // Generate a uci path to the section we wish to lookup
+    if (snprintf(sec_path, MAX_UCI_BUF_LEN, section_path, package_name.c_str(),
+                 section_name.c_str()) <= 0) {
+        LOG(ERROR) << "Failed to compose path";
+        return false;
+    }
+
+    uci_ptr sec_ptr;
+    // Initialize section pointer from path & validate section existace
+    if (uci_lookup_ptr(ctx.get(), &sec_ptr, sec_path, true) != UCI_OK || !sec_ptr.s) {
+        LOG(ERROR) << "UCI failed to lookup ptr for path: " << sec_path << std::endl
+                   << uci_get_error(ctx.get());
+        return false;
+    }
+
+    if (uci_delete(ctx.get(), &sec_ptr) != UCI_OK) {
+        LOG(ERROR) << "UCI failed to delete ptr: " << sec_path << std::endl
+                   << uci_get_error(ctx.get());
+        return false;
+    }
+
+    char pkg_path[MAX_UCI_BUF_LEN] = {0};
+    // Generate a uci path to the package we wish to lookup
+    if (snprintf(pkg_path, MAX_UCI_BUF_LEN, package_path, package_name.c_str()) <= 0) {
+        LOG(ERROR) << "Failed to compose path";
+        return false;
+    }
+
+    uci_ptr pkg_ptr;
+    // Initialize package pointer from path & validate package existace
+    if (uci_lookup_ptr(ctx.get(), &pkg_ptr, pkg_path, true) != UCI_OK || !pkg_ptr.p) {
+        LOG(ERROR) << "UCI failed to lookup ptr for path: " << pkg_path << std::endl
+                   << uci_get_error(ctx.get());
+        return false;
+    }
+
+    // Create delta of changes, this does not change the persistent file.
+    if (uci_save(ctx.get(), pkg_ptr.p) != UCI_OK) {
+        LOG(ERROR) << "Failed to save changes!" << std::endl << uci_get_error(ctx.get());
+        return false;
+    }
+
+    // Commit changes to file
+    if (uci_commit(ctx.get(), &pkg_ptr.p, false) != UCI_OK) {
+        LOG(ERROR) << "Failed to commit changes!" << std::endl << uci_get_error(ctx.get());
+        return false;
+    }
+
     return true;
 }
 
