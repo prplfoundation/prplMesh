221e96e51108eb55fda1914be4a0b28ed5fa4622
Itay Elenzweig <itayx.elenzweig@intel.com>
2020-07-23 11:19:16 +0000

    framework: bpl: uci: Implement section_exists function.
    The implementation of bpl_db_uci function utilizes the `libuci`
    functions that provides persistency of the configured data over UCI.
    
    Implemented the section_exists API, the functions checks whether a
    section exists in the given configuration.
    Existence is determined by matching section name & type (if provided).
    
    Relates to JIRA PPM-5.
    Signed-off-by: Itay Elenzweig <itayx.elenzweig@intel.com>

--- framework/platform/bpl/uci/db/bpl_db_uci.cpp
+++ framework/platform/bpl/uci/db/bpl_db_uci.cpp
@@ -44,6 +44,55 @@ bool uci_section_exists(const std::string &package_name, const std::string &sect
     LOG(TRACE) << "uci_section_exists() " << package_name << ".(" << section_type << ")"
                << section_name;
 
+    auto ctx = alloc_context();
+    if (!ctx) {
+        return false;
+    }
+
+    char path[MAX_UCI_BUF_LEN] = {0};
+    // Generate a uci path to the package we wish to lookup
+    if (snprintf(path, MAX_UCI_BUF_LEN, package_path, package_name.c_str()) <= 0) {
+        LOG(ERROR) << "Failed to compose path";
+        return false;
+    }
+
+    uci_package *pkg = uci_lookup_package(ctx.get(), path);
+    if (!pkg) {
+        // Try and load the package file, in case it wasn't loaded before
+        uci_load(ctx.get(), path, &pkg);
+        if (!pkg) {
+            LOG(ERROR) << "uci lookup package failed!" << std::endl << uci_get_error(ctx.get());
+            return false;
+        }
+    }
+
+    bool section_found = false;
+    // Loop through all sections
+    uci_section *sec = nullptr;
+    uci_element *elm = nullptr;
+    uci_foreach_element(&pkg->sections, elm)
+    {
+        sec = uci_to_section(elm);
+        // uci enforces sections must have unique name
+        if (section_name.compare(sec->e.name) == 0) {
+            LOG(TRACE) << "Found section: " << sec->type << "(" << sec->e.name << ")";
+            section_found = true;
+            break;
+        }
+    }
+
+    if (!section_found) {
+        LOG(DEBUG) << "No matching section found for " << section_name;
+        return false;
+    }
+
+    // If the section_type is empty all matching sections are a valid match
+    if (!section_type.empty() && section_type.compare(sec->type) != 0) {
+        LOG(DEBUG) << "Section with matching name found, but types don't match" << std::endl
+                   << "Searched type: " << section_type << ", Found type: " << sec->type;
+        return false;
+    }
+
     return true;
 }
 
