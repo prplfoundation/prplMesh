84eceead2d58c9432d61ce791cb5f60fe7c401ab
Anton Bilohai <a.bilohai@inango-systems.com>
2020-07-24 23:35:49 +0300

    cmake: create separate HLAPI library
    Create new Hight Level API library, which should handle interactions
    with different system buses.
    
    Jira link: https://jira.prplfoundation.org/browse/PPM-300
    
    Signed-off-by: Anton Bilohai <a.bilohai@inango-systems.com>

---
+++ framework/platform/hlapi/hlapi.cpp
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+extern "C" {
+#include <amxc/amxc.h>
+}
