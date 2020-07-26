bad377bbe494a284fc2991f8b607368c8ee92723
Adam Dov <adamx.dov@intel.com>
2020-07-26 07:30:28 +0000

    controller: read list of clients from DB
    Changed the ACTION_BML_CLIENT_GET_CLIENT_LIST_REQUEST handling to read
    the clients from persistent DB using the
    get_clients_with_persistent_data_configured() DB API.
    
    Removed the related cpp-check issue from cppcheck_existing_issues.txt.
    
    PPM-5.
    
    Signed-off-by: Adam Dov <adamx.dov@intel.com>

--- controller/src/beerocks/master/son_management.cpp
+++ controller/src/beerocks/master/son_management.cpp
@@ -2060,9 +2060,8 @@ void son_management::handle_bml_message(Socket *sd,
             }
         };
 
-        // TODO: replace with a list of configured clients read from controller-db
-        std::vector<sMacAddr> client_list;
-        if (!client_list.size()) {
+        auto client_list = database.get_clients_with_persistent_data_configured();
+        if (client_list.empty()) {
             LOG(DEBUG) << "client list is empty!";
             // Send a valid response with an empty list
             send_response(true);
