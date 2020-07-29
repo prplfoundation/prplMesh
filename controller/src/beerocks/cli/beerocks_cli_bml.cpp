b340078e4a4a10164f3cfb7ce21d0657b7c52d09
Adam Dov <adamx.dov@intel.com>
2020-07-29 15:01:59 +0000

    bml: cli: fix selected_bands debug prints
    Removed excess spaces from selected-bands debug print.
    
    PPM-5.
    
    Signed-off-by: Adam Dov <adamx.dov@intel.com>

--- controller/src/beerocks/cli/beerocks_cli_bml.cpp
+++ controller/src/beerocks/cli/beerocks_cli_bml.cpp
@@ -2270,13 +2270,13 @@ int cli_bml::client_get_client(const std::string &sta_mac)
             if (val == BML_CLIENT_SELECTED_BANDS_DISABLED)
                 return "Disabled";
             if (val & BML_CLIENT_SELECTED_BANDS_24G)
-                ret += "2.4 Ghz,";
+                ret += "2.4GHz,";
             if (val & BML_CLIENT_SELECTED_BANDS_5G)
-                ret += "5 Ghz,";
+                ret += "5GHz,";
             if (val & BML_CLIENT_SELECTED_BANDS_6G)
-                ret += "6 Ghz,";
+                ret += "6GHz,";
             if (val & BML_CLIENT_SELECTED_BANDS_60G)
-                ret += "60 Ghz,";
+                ret += "60GHz,";
 
             // remove ending comma
             if (!ret.empty() && (ret.back() == ',')) {
