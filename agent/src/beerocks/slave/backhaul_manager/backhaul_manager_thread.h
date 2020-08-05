9ef9819a02730fd947b40aa8e321800fde2eb8ec
Oren Vormaser <oren.vormaser@devalore.com>
2020-08-05 15:19:16 +0000

    agent: move 1905_higher_layer_data handling from backhaul to son_slave
    currently the backhaul manager handles 1905_higher_layer_data messages,
    while they should be handled by the agent. This PR moves the handling
    to son_slave thread as preparation for future unified agent class.
    
    This commit breaks higher_layer_data_payload_trigger test in
    test_flows.py. The test will be fixed in the next commit.
    
    Move handle_1905_higher_layer_data_message() from the backhaul manager
    to the son_slave thread.
    
    PPM-334
    
    Signed-off-by: Oren Vormaser <oren.vormaser@devalore.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.h
@@ -129,8 +129,6 @@ private:
     // 1905 messages handlers
     bool handle_1905_autoconfiguration_response(ieee1905_1::CmduMessageRx &cmdu_rx,
                                                 const std::string &src_mac);
-    bool handle_1905_higher_layer_data_message(ieee1905_1::CmduMessageRx &cmdu_rx,
-                                               const std::string &src_mac);
     bool handle_1905_link_metric_query(ieee1905_1::CmduMessageRx &cmdu_rx,
                                        const std::string &src_mac);
     bool handle_1905_combined_infrastructure_metrics(ieee1905_1::CmduMessageRx &cmdu_rx,
