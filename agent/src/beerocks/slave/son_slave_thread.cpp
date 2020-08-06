6d15f7e6d53c875e2ff6d34afd8b1b28f654eb9d
Oren Vormaser <oren.vormaser@devalore.com>
2020-08-06 08:51:46 +0000

    agent: Move backhaul_preferred_radio_band configuration to AgentDB
    Move "backhaul_preferred_radio_band" configuration of back radio from
    "sPlatformSettings" struct to the AgentDB.
    Replace the use of "backhaul_preferred_radio_band" settings on the Agent
    thread to the one on the AgentDB.
    
    Removing of the config from the tlvf will take place on future commit.
    
    PPM-338
    
    Signed-off-by: Oren Vormaser <oren.vormaser@devalore.com>

--- agent/src/beerocks/slave/son_slave_thread.cpp
+++ agent/src/beerocks/slave/son_slave_thread.cpp
@@ -3114,7 +3114,7 @@ bool slave_thread::slave_fsm(bool &call_slave_select)
                 platform_to_bwl_security(db->device_conf.back_radio.security_type));
             bh_enable->mem_only_psk() = db->device_conf.back_radio.mem_only_psk;
             bh_enable->backhaul_preferred_radio_band() =
-                platform_settings.backhaul_preferred_radio_band;
+                db->device_conf.back_radio.backhaul_preferred_radio_band;
 
             string_utils::copy_string(bh_enable->wire_iface(message::IFACE_NAME_LENGTH),
                                       db->ethernet.iface_name.c_str(), message::IFACE_NAME_LENGTH);
