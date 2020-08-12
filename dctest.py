9355d53f44e2a0cb33d681d89488e85fbe40fcf9
pablo <pablo@odkq.com>
2020-08-12 13:46:30 +0200

    boardfarm-ci: Removed more unused code
    Signed-off-by: pablo <pablo@odkq.com>

--- dctest.py
+++ dctest.py
@@ -145,30 +145,6 @@ class Services:
         return return_code
 
 
-def vararg_callback(option, opt_str, value, parser):
-    assert value is None
-    value = []
-
-    def floatable(str):
-        try:
-            float(str)
-            return True
-        except ValueError:
-            return False
-
-    for arg in parser.rargs:
-        # stop on --foo like options
-        if arg[:2] == "--" and len(arg) > 2:
-            break
-        # stop on -a, but not on -3 or -3.0
-        if arg[:1] == "-" and len(arg) > 1 and not floatable(arg):
-            break
-        value.append(arg)
-
-    del parser.rargs[:len(value)]
-    setattr(parser.values, option.dest, value)
-
-
 def cleanup(rc):
     if rc != 0:
         print('Return code !=0 -> {}'.format(rc))
