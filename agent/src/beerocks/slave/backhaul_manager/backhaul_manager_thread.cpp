f843fe41749d566ac8c74cb47a9f09880811c6cc
Moran Shoeg <moranx.shoeg@intel.com>
2020-08-12 19:47:20 +0000

    agent: ApAutoConfiguration Task skeleton and preparations
    Add a skeleton for the ApAutoConfiguration Task, which includes tasks state machine and
    functions that will be overridden from the Task base class.
    Add the new task to eTaskType.
    Add the task to the task pool on the backhaul manager.
    
    PPM-345
    
    Signed-off-by: Moran Shoeg <moranx.shoeg@intel.com>

--- agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
+++ agent/src/beerocks/slave/backhaul_manager/backhaul_manager_thread.cpp
@@ -34,6 +34,8 @@
 #include "../helpers/media_type.h"
 #include "../link_metrics/ieee802_11_link_metrics_collector.h"
 #include "../link_metrics/ieee802_3_link_metrics_collector.h"
+
+#include "../tasks/ap_autoconfiguration_task.h"
 #include "../tasks/topology_task.h"
 #include "../tlvf_utils.h"
 
@@ -186,12 +188,6 @@ backhaul_manager::backhaul_manager(const config_file::sConfigSlave &config,
 
     m_eFSMState = EState::INIT;
     set_select_timeout(SELECT_TIMEOUT_MSC);
-
-    auto topology_task = std::make_shared<TopologyTask>(*this, cmdu_tx);
-    if (!topology_task) {
-        LOG(ERROR) << "failed to allocate Topology Task!";
-    }
-    m_task_pool.add_task(topology_task);
 }
 
 backhaul_manager::~backhaul_manager() { backhaul_manager::on_thread_stop(); }
@@ -231,6 +227,20 @@ bool backhaul_manager::init()
         LOG(ERROR) << "Failed to init mapf_bus";
         return false;
     }
+
+    auto topology_task = std::make_shared<TopologyTask>(*this, cmdu_tx);
+    if (!topology_task) {
+        LOG(ERROR) << "failed to allocate Topology Task!";
+        return false;
+    }
+    m_task_pool.add_task(topology_task);
+
+    auto ap_auto_configuration_task = std::make_shared<ApAutoConfigurationTask>(*this, cmdu_tx);
+    if (!ap_auto_configuration_task) {
+        LOG(ERROR) << "failed to allocate ap_auto_configuration_task Task!";
+        return false;
+    }
+    m_task_pool.add_task(ap_auto_configuration_task);
 
     return true;
 }
